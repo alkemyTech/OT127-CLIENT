d5559377686320760eba63eb20fd3db5ff4edc6c
danieldamian09 <humbertorivero9@gmail.com>
2022-01-19 20:50:15 -0300

    add input file

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -4,15 +4,24 @@ import '../FormStyles.css';
 const CategoriesForm = () => {
     const [initialValues, setInitialValues] = useState({
         name: '',
-        description: ''
+        description: '',
+        image: ''
     })
 
     const handleChange = (e) => {
         if(e.target.name === 'name'){
             setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
+        } 
+        
+        if(e.target.name === 'description'){
             setInitialValues({...initialValues, description: e.target.value})
         }
+
+        if(e.target.name === 'image'){
+            setInitialValues({...initialValues, image: e.target.value})
+        }
+
+
     }
 
     const handleSubmit = (e) => {
@@ -24,7 +33,8 @@ const CategoriesForm = () => {
         <form className="form-container" onSubmit={handleSubmit}>
             <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Title"></input>
             <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some description"></input>
-            <button className="submit-btn" type="submit">Send</button>
+            <input className="input-field" type="file" name='image' value={initialValues.image} onChange={handleChange}></input>
+            <button className="submit-btn" type="submit" >Send</button>
         </form>
     );
 }
