e5862508db80b93b11586afaa90de2f6e144e42a
danieldamian09 <humbertorivero9@gmail.com>
2022-01-20 19:26:41 -0300

    fix form params

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -1,40 +1,58 @@
 import React, { useState } from 'react';
+import { useParams } from 'react-router-dom'
+import axios from 'axios';
 import '../FormStyles.css';
 
 const CategoriesForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        description: '',
-        image: ''
-    })
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } 
-        
-        if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        }
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [image, setImage] = useState("");
+    const [mesagge, setMesagge] = useState("");
+    
+    const params = useParams()
+    //console.log(Object.keys(params).length)
+    
 
-        if(e.target.name === 'image'){
-            setInitialValues({...initialValues, image: e.target.value})
-        }
-
-
+    const handleImage = e => {
+        setImage(e.target.files[0])
     }
+
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(initialValues);
+        
+        if([name, description].includes("") || (!image)){
+            setMesagge('Todos los campos son obligatorio')
+
+            setTimeout(() => {
+                setMesagge('')
+            }, 3000);
+
+            return
+        }
+
+        setName(name)
+        setDescription(description)
+
+        if(Object.keys(params).length){
+            // Modifico
+            console.log("Actualizando")
+
+        }else{
+            // Creo
+            console.log("Creando")
+        }
+
     }
 
     return (
         <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some description"></input>
-            <input className="input-field" type="file" name='image' value={initialValues.image} onChange={handleChange}></input>
-            <button className="submit-btn" type="submit" >Send</button>
+            {mesagge && <p>{mesagge}</p>}
+            <input className="input-field" type="text" name="name" value={name} onChange={e => setName(e.target.value)} placeholder="Title"></input>
+            <input className="input-field" type="text" name="description" value={description} onChange={e => setDescription(e.target.value)} placeholder="Write some description"></input>
+            <input className="input-field" type="file" name='image'  onChange={handleImage}></input>
+            <input className="submit-btn" type="submit" value={(Object.keys(params).length) ? "Edit" : "Add"}></input>
         </form>
     );
 }
