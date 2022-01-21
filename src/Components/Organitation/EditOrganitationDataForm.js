d73ca0107502990b3d7afa28e40054f71a8ac579
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-01-20 23:17:08 -0300

    Configurado onchange para recibir datos de inputs

--- src/Components/Organitation/EditOrganitationDataForm.js
+++ src/Components/Organitation/EditOrganitationDataForm.js
@@ -7,33 +7,42 @@ import FormLabel from '@mui/material/FormLabel';
 const EditOrganitationDataForm = () => {
 
     const [organitationData, setOrganitationData] = useState(organitationDataStructure())
-    
+    console.log(organitationData)
+    const handleChange = (event) => {
+        event.target.id === 'name' && setOrganitationData({...organitationData, name:event.target.value})
+        event.target.id === 'longDescription' && setOrganitationData({...organitationData, longDescription:event.target.value})
+        event.target.id === 'facebook' && setOrganitationData({...organitationData, facebook:event.target.value})
+        event.target.id === 'linkedin' && setOrganitationData({...organitationData, linkedin:event.target.value})
+        event.target.id === 'instagram' && setOrganitationData({...organitationData, instagram:event.target.value})
+        event.target.id === 'twitter' && setOrganitationData({...organitationData, twitter:event.target.value})
+    }
+
     return (
         <div>
             <FormControl>
             <InputLabel htmlFor="my-input" required={true}>Name</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input id="name" aria-describedby="my-helper-text" onChange={handleChange}/>
             </FormControl>
             <FormControl>
-            <InputLabel htmlFor="my-input" required={true}>Large description</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <InputLabel htmlFor="my-input" required={true}>Long description</InputLabel>
+            <Input id="longDescription" aria-describedby="my-helper-text" onChange={handleChange}/>
             </FormControl>
             <FormLabel>Redes sociales</FormLabel>
             <FormControl>
             <InputLabel htmlFor="my-input" required={true}>Facebook</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input id="facebook" aria-describedby="my-helper-text" onChange={handleChange}/>
             </FormControl>
             <FormControl>
             <InputLabel htmlFor="my-input" required={true}>Linkedin</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input id="linkedin" aria-describedby="my-helper-text" onChange={handleChange}/>
             </FormControl>
             <FormControl>
             <InputLabel htmlFor="my-input" required={true}>Instagram</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input id="instagram" aria-describedby="my-helper-text" onChange={handleChange}/>
             </FormControl>
             <FormControl>
             <InputLabel htmlFor="my-input" required={true}>Twitter</InputLabel>
-            <Input id="my-input" aria-describedby="my-helper-text" />
+            <Input id="twitter" aria-describedby="my-helper-text" onChange={handleChange}/>
             </FormControl>
             <FormControl>
             <Input id="my-input" type='submit' onClick={() => console.log('hola')}/>
