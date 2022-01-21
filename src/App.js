f25b5bef32be21151884cf35ce158245e9c0063d
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-01-20 21:03:45 -0300

    Fix/ ruta donar en una linea, pendiente determinar si usamos component o render

--- src/App.js
+++ src/App.js
@@ -32,12 +32,7 @@ function App() {
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
-          <Route
-            path='/donar'
-            render={(props) => (
-              <Donacion {...props} message={'Quieres donar?'} />
-            )}
-          />
+          <Route path='/donar' render={(props) => (<Donacion {...props} message={'Quieres donar?'}/>)}/>
           <Route path="/gracias" component={Gracias} />
         </Switch>
       </BrowserRouter>
