c2bb9c85fe40120b73ff759212c1015b7080d45c
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-01-22 10:15:29 -0300

    Hecho get con axios y renderizado de informacion

--- src/Components/Organization/OrganizationData.js
+++ src/Components/Organization/OrganizationData.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
 
 const OrganizationData = () => {
-  return <div></div>;
+
+    const [organizationData, setOrganizationData] = useState(null)
+
+    const getOrgData = () => {
+        axios.get(' http://ongapi.alkemy.org/api/organization')
+        .then(response => {
+            setOrganizationData(response.data)
+        })
+        .catch((error) => {
+            alert(error.message)
+        })
+    }
+
+    useEffect(() => {
+        getOrgData();
+        return () => {
+            setOrganizationData(null)
+        };
+    }, []);
+    
+    return (
+        <>
+        {organizationData &&
+        <div>
+            <h1>{organizationData.data.name}</h1>
+            <img src={organizationData.data.logo} alt='organization_logo'/>
+            <p>{organizationData.data.short_description}</p> 
+            <Link to='/backoffice/organization/edit'>Editar</Link>
+        </div>
+        }
+        </>
+    )
 }
 
 export default OrganizationData;
