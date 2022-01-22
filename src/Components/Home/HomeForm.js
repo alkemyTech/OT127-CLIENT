8956e08a6f3d1e61ada64e2e70201022d8ba2132
matias <solisma42@gmail.com>
2022-01-22 18:10:49 -0300

    estructura basica con formik

--- src/Components/Home/HomeForm.js
+++ src/Components/Home/HomeForm.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 const HomeForm = () => {
     return (
-       
+        <Formik
+            initialValues={{ welcome: '' }}
+            validationSchema={Yup.object({
+                welcome: Yup.string().min(20, 'Debe tener por lo menos 20 caracteres.').required('Este campo es obligatorio'),
+            })}
+            onSubmit={(values) => {
+                console.log(values)
+            }}
+        >
+            <Form>
+                <label htmlFor="welcome">
+                    Texto de Bienvenida
+                </label>
+                <Field name="welcome" />
+                <ErrorMessage name="welcome" />
+
+
+                <button type='submit'>Guardar Cambios</button>
+            </Form>
+        </Formik>
     )
 };
 
