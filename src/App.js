746a328e75db776cd8cbb4a33c7ebf69b118856f
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-01-21 22:12:06 -0300

    Creada ruta y componente organizationData

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import MembersForm from "./Components/Members/MembersForm";
 import ProjectsForm from "./Components/Projects/ProjectsForm";
 import RegisterForm from "./Components/Auth/RegisterForm";
 import LoginForm from "./Components/Auth/LoginForm";
+import OrganizationData from "./Components/Organization/OrganizationData";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
+          <Route path="/backoffice/organization" component={OrganizationData} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
---
+++ src/Components/Organization/OrganizationData.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const OrganizationData = () => {
+  return <div></div>;
+}
+
+export default OrganizationData;
