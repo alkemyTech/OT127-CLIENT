c075e78a1eff59086ee8a3b26ec77ab312a2477f
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-01-22 01:39:08 -0300

    Formulario creado

--- src/Components/Newsletter/NewsletterForm.js
+++ src/Components/Newsletter/NewsletterForm.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const NewsletterForm = () => {
-  return <div>
-      <h1>hola NewsletterForm</h1>
-  </div>;
+    const [email, setEmail] = useState({
+        email:'',
+    });
+
+    const handleChange = ({target}) =>{
+        setEmail({
+            [target.name] : target.value,
+        })
+    }
+
+    const handleSubmit = ( e ) =>{
+        e.preventDefault()
+        console.log('hola Submit');
+    }
+
+    return  <div>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="email">Newsletter</label>
+                    <input type="email" name="email" id="email" value={email} onChange={handleChange} placeholder="Ingresa tu email" />
+                    <button type='submit'> suscribirse </button>
+                </form>
+            </div>;
 };
 
 export default NewsletterForm;
