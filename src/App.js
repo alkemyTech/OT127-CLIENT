1e592566713aea8cc410857a29b9c7698f940e1a
matias <solisma42@gmail.com>
2022-01-22 17:49:57 -0300

    Agregado archivos iniciales para el formulario edicion home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import ActivitiesForm from './Components/Activities/ActivitiesForm';
 import CategoriesForm from './Components/Categories/CategoriesForm';
 import NewsForm from './Components/News/NewsForm';
@@ -15,6 +15,7 @@ import RegisterForm from "./Components/Auth/RegisterForm";
 import LoginForm from "./Components/Auth/LoginForm";
 import Donacion from './Donations/Donacion';
 import Gracias from './Donations/Gracias';
+import HomeForm from './Components/Home/HomeForm';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
+          <Route path="/backoffice/home" component={HomeForm} />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
@@ -34,7 +36,7 @@ function App() {
           <Route path="/toys-campaign" component={ToysCampaign} />
           <Route path="/login" component={LoginForm} />
           <Route path="/register" component={RegisterForm} />
-          <Route path='/donar' component={() => (<Donacion message={'Quieres donar?'}/>)}/> 
+          <Route path='/donar' component={() => (<Donacion message={'Quieres donar?'} />)} />
           <Route path="/gracias" component={Gracias} />
         </Switch>
       </BrowserRouter>
