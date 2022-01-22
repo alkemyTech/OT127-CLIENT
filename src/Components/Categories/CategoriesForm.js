954584c33287c7d134fd6556cf4dd06993b0d87c
danieldamian09 <humbertorivero9@gmail.com>
2022-01-22 15:39:40 -0300

    change masagge error and input

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -37,13 +37,35 @@ const CategoriesForm = () => {
 
     return (
         <form className="form-container" onSubmit={handleSubmit}>
-            {mesagge && <p>{mesagge}</p>}
-            <input className="input-field" type="text" name="name" value={name} onChange={e => setName(e.target.value)} placeholder="Title"></input>
-            <input className="input-field" type="text" name="description" value={description} onChange={e => setDescription(e.target.value)} placeholder="Write some description"></input>
-            <input className="input-field" type="file" name='image'  onChange={handleImage}></input>
-            <input className="submit-btn" type="submit" value={(Object.keys(params).length) ? "Edit" : "Add"}></input>
+
+                <input 
+                    className="input-field" 
+                    type="text" name="name" 
+                    value={name} 
+                    onChange={e => setName(e.target.value)} 
+                    placeholder="Title">
+                </input>
+
+                <input 
+                    className="input-field" 
+                    type="text" 
+                    name="description" 
+                    value={description} 
+                    onChange={e => setDescription(e.target.value)} 
+                    placeholder="Write some description">
+                </input>
+                <input className="input-field" type="file" name='image'  onChange={handleImage}></input>
+
+                <input 
+                className="submit-btn" 
+                type="submit" 
+                value={(Object.keys(location).length) ? "Edit" : "Add"}>
+                </input>
+
         </form>
+
+        
     );
 }
- 
+
 export default CategoriesForm;
