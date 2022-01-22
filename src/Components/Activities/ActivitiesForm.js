4ebb04db75acae423d8f0cbbf95772a1aee5eb7e
cardosonicolas <cardoso.leandro.nicolas@gmail.com>
2022-01-22 07:39:13 -0300

    replace description input with CKeditor, improve handleChange, add image preview

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -1,32 +1,68 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React, { useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import "../FormStyles.css";
 
 const ActivitiesForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        description: ''
-    });
+  const [activity, setActivity] = useState({
+    name: "",
+    description: "",
+    image: "",
+  });
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        }
+  const handleChange = (e) => {
+    if (e.target.name === "name") {
+      setActivity({ ...activity, name: e.target.value });
     }
+    if (e.target.name === "file") {
+      setActivity({
+        ...activity,
+        image: URL.createObjectURL(e.target.files[0]),
+      });
+    }
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
+  const handleChangeDescription = (e, editor) => {
+    if (e.name === "change:data") {
+      const data = editor.getData();
+      setActivity({ ...activity, description: data });
     }
-    
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Activity Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some activity description"></input>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
-    );
-}
- 
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(activity);
+  };
+
+  const { name, description, image } = activity;
+
+  return (
+    <form className="form-container" onSubmit={handleSubmit}>
+      <input
+        className="input-field"
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        placeholder="Activity Title"
+      ></input>
+      <CKEditor
+        editor={ClassicEditor}
+        data={description}
+        onChange={(event, editor) => handleChangeDescription(event, editor)}
+      />
+      <input
+        type="file"
+        name="file"
+        accept=".png, .jpg"
+        onChange={handleChange}
+      />
+      <img src={image} alt="" srcset="" />
+      <button className="submit-btn" type="submit">
+        Send
+      </button>
+    </form>
+  );
+};
+
 export default ActivitiesForm;
