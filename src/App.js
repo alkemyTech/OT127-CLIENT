b43910f36b965ca99545372a06e726d7e0c10486
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-01-22 13:58:38 -0300

    Fix/ Rutas login y register. pull de main

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import SchoolCampaign from './Campaigns/School/SchoolCampaign';
 import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
+import RegisterForm from "./Components/Auth/RegisterForm";
+import LoginForm from "./Components/Auth/LoginForm";
 import Donacion from './Donations/Donacion';
 import Gracias from './Donations/Gracias';
 
@@ -30,6 +32,8 @@ function App() {
           <Route path="/create-project" component={ProjectsForm} />
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
           <Route path='/donar' component={() => (<Donacion message={'Quieres donar?'}/>)}/> 
           <Route path="/gracias" component={Gracias} />
         </Switch>
