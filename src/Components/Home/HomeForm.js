98a44b984cad15c7a12ae51356d9d6120e0e068b
matias <solisma42@gmail.com>
2022-01-23 17:54:33 -0300

    Terminada estructura HTML

--- src/Components/Home/HomeForm.js
+++ src/Components/Home/HomeForm.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 
 const HomeForm = () => {
     const [slidesData, setSlidesData] = useState([])
-    const [welcomeText, setWelcomeText] = useState("")
+    const [welcomeText, setWelcomeText] = useState('')
 
     useEffect(() => {
         const getDataToEdit = async () => {
@@ -15,7 +15,7 @@ const HomeForm = () => {
 
                 const slides = slidesResponse.data.data
                 const welcomeText = welcomeResponse.data.data.welcome_text
-
+                console.log(welcomeResponse);
                 setSlidesData(slides);
                 setWelcomeText(welcomeText);
 
@@ -29,7 +29,10 @@ const HomeForm = () => {
 
     return (
         <Formik
-            initialValues={{ welcome: '' }}
+            enableReinitialize={true}
+            initialValues={{
+                welcome: welcomeText,
+            }}
             validationSchema={Yup.object({
                 welcome: Yup.string().min(20, 'Debe tener por lo menos 20 caracteres.').required('Este campo es obligatorio'),
             })}
@@ -37,13 +40,29 @@ const HomeForm = () => {
                 console.log(values)
             }}
         >
-            <Form>
+            <Form className='form'>
+                <p className='form__title'>Formulario de Edición Home</p>
                 <label htmlFor="welcome">
                     Texto de Bienvenida
                 </label>
-                <Field name="welcome" />
+                <Field name="welcome" className='form__input' />
                 <ErrorMessage name="welcome" />
-                <button type='submit'>Guardar Cambios</button>
+                <b>Sliders</b>
+                <hr />
+                <div className='form__sliders'>
+                    {slidesData.map((slide) => (
+                        <div className='form__slide'>
+                            <p>ID: {slide.id}</p>
+                            <p>Titulo</p>
+                            <Field name={`${slide.id}_title`} className='form__input' />
+                            <p>Descripción</p>
+                            <Field name={`${slide.id}_title`} className='form__input' />
+                            <p>URL de la imagen</p>
+                            <Field name={`${slide.id}_title`} className='form__input' />
+                        </div>
+                    ))}
+                </div>
+                <button type='submit' className='form__button'>Guardar Cambios</button>
             </Form>
         </Formik>
     )
