5c3ecd20fd7bab3ab142819c8865a8c6f88d01e6
danieldamian09 <humbertorivero9@gmail.com>
2022-01-23 13:41:58 -0300

    structure of the Form with Formik

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -1,9 +1,69 @@
 import React from 'react';
+import { Formik, Form, Field } from 'formik'
 
 function ContactForm() {
   return (
-    <div>
-      
+    <div className="form-container">
+      <Formik>
+        <Form>
+
+          <div>
+            <label
+              htmlFor='name'
+            >Nombre:</label>
+            <Field
+              className="input-field"
+              id="name"
+              type="text"
+              placeholder="Ingrese su nombre"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor='email'
+            >Email:</label>
+            <Field
+              className="input-field"
+              id="email"
+              type="text"
+              placeholder="Ingrese correo electronico"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor='phone'
+            >Teléfono:</label>
+            <Field
+              className="input-field"
+              id="phone"
+              type="tel"
+              placeholder="Ingrese su número de teléfono"
+            />
+          </div>
+
+          <div>
+            <label
+              htmlFor='mesagge'
+            >Mensaje:</label>
+            <Field
+              className="input-field"
+              as="textarea"
+              id="mesagge"
+              type="text"
+              placeholder="Mensaje"
+            />
+          </div>
+
+          <input
+            className="submit-btn"
+            type="submit"
+            value="Enviar"
+          />
+
+        </Form>
+      </Formik>
     </div>
   );
 }
