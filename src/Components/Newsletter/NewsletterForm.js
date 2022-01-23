bc3230fcc5bb9f6fcf9921577b65cc734fb112ea
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-01-23 10:48:10 -0300

    formik para gestionar el formulario

--- src/Components/Newsletter/NewsletterForm.js
+++ src/Components/Newsletter/NewsletterForm.js
@@ -1,47 +1,60 @@
 import React, { useState } from "react";
+import "./newsletter.scss";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 const NewsletterForm = () => {
   const [newsletter, setNewsletter] = useState({
     email: "",
-    subscribed: null,
   });
-
-  const { email } = newsletter;
-
-  const handleChange = ({ target }) => {
-    setNewsletter({
-      [target.name]: target.value,
-    });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("hola Submit");
-  };
-
   return (
-    <div className="container">
-      <div className="newsletter__main-content">
-        <form className="newsletter__subscription" onSubmit={handleSubmit}>
-          <label htmlFor="email">Newsletter</label>
-          <input
-            className="newsletter__add-email"
-            type="email"
-            name="email"
-            id="email"
-            value={email}
-            onChange={handleChange}
-            placeholder="Ingresa tu email"
-          />
-          <button className="newsletter__submit-button" type="submit">
-            <span className="newsletter__submit-before">Suscribirse</span>
-            <span className="newsletter__submit-after">
-              Gracias por suscribirse!
-            </span>
-          </button>
-        </form>
-      </div>
-    </div>
+    <>
+      <Formik
+        initialValues={{ email: "" }}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email("Ingresa un email valido")
+            .required("Ingrese un email"),
+        })}
+        onSubmit={(values, { resetForm }) => {
+          const userNewsletterSubscribed = "active";
+          setNewsletter({
+            email: values.email,
+          });
+
+          localStorage.setItem(
+            "newsletter",
+            JSON.stringify(userNewsletterSubscribed)
+          );
+          resetForm();
+        }}
+      >
+        <div className="container">
+          <div className="newsletter__wrapper">
+            <div>
+              <ErrorMessage
+                name="email"
+                render={(msg) => <div className="error">{msg}</div>}
+              />
+            </div>
+            <div className="newsletter__main-content">
+              <Form className="newsletter__subscription">
+                <Field
+                  className="newsletter__add-email"
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Ingresa tu email"
+                />
+                <button className="newsletter__submit-button" type="submit">
+                  Suscribirse
+                </button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </Formik>
+    </>
   );
 };
 
