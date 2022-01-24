1b2acb6e528bf25cadac35dcb8fd4fbe32676718
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-01-23 23:46:42 -0300

    Pull a main

--- src/App.js
+++ src/App.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import './App.css';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import ActivitiesForm from './Components/Activities/ActivitiesForm';
-import CategoriesForm from './Components/Categories/CategoriesForm';
-import NewsForm from './Components/News/NewsForm';
-import SlidesForm from './Components/Slides/SlidesForm';
-import TestimonialForm from './Components/Testimonials/TestimonialsForm';
-import UserForm from './Components/Users/UsersForm';
-import SchoolCampaign from './Campaigns/School/SchoolCampaign';
-import ToysCampaign from './Campaigns/Toys/ToysCampaign';
-import MembersForm from './Components/Members/MembersForm';
-import ProjectsForm from './Components/Projects/ProjectsForm';
-import EditOrganizationDataForm from './Components/Organization/EditOrganizationDataForm'
-=======
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
@@ -29,8 +13,7 @@ import MembersForm from "./Components/Members/MembersForm";
 import ProjectsForm from "./Components/Projects/ProjectsForm";
 import RegisterForm from "./Components/Auth/RegisterForm";
 import LoginForm from "./Components/Auth/LoginForm";
->>>>>>> 989b637a8efa54464e55a89f7e0f9850029baf1a
-
+import EditOrganizationDataForm from "./Components/Organization/EditOrganizationDataForm";
 function App() {
   return (
     <>
@@ -41,7 +24,10 @@ function App() {
           <Route path="/create-category" component={CategoriesForm} />
           <Route path="/create-news" component={NewsForm} />
           <Route path="/backoffice/create-slide" component={SlidesForm} />
-          <Route path="/backoffice/organization/edit" component={EditOrganizationDataForm} />
+          <Route
+            path="/backoffice/organization/edit"
+            component={EditOrganizationDataForm}
+          />
           <Route path="/create-testimonials" component={TestimonialForm} />
           <Route path="/create-user" component={UserForm} />
           <Route path="/create-member" component={MembersForm} />
@@ -52,10 +38,7 @@ function App() {
           <Route path="/register" component={RegisterForm} />
         </Switch>
       </BrowserRouter>
-<<<<<<< HEAD
-=======
       <div className="App"></div>
->>>>>>> 989b637a8efa54464e55a89f7e0f9850029baf1a
     </>
   );
 }
