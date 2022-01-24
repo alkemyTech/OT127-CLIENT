18e880a61bae14ad3c0398750ef2676df00505d5
Gabriel Suarez <67937686+silversurferbeats@users.noreply.github.com>
2022-01-24 01:48:54 -0300

    Update Actividad.js
    refactorizacion

--- src/actividad/Actividad.js
+++ src/actividad/Actividad.js
@@ -1,31 +1,16 @@
 import React, { Component } from 'react';
-//import { CKEditor } from '@ckeditor/ckeditor5-react';
-//import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 
-//antesd de  commit cambiar de branch
-
-const Actividad = () =>{
-    
-    
+const Actividad = () =>{    
     function enviarDatos(){
         let texto = document.getElementById('campo').value;
-        
         document.body.innerHTML = texto;
     }
-    
-
     return(
         <>
             <div>Hola Mundo</div>
-
             <textarea id="campo" type="text" name="campo" cols="40" rows="3" ><input/></textarea><br/>
-
-            <button className="submit-btn" type="submit" onClick={()=> enviarDatos()} >Render</button><br/>
-            
+            <button className="submit-btn" type="submit" onClick={()=> enviarDatos()} >Render</button><br/>       
         </>
     )
-    
 }
-
-
 export default Actividad;
