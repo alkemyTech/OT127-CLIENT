4cb1407b9ede550aadb4f7975caf5c0ff6f316c0
danieldamian09 <humbertorivero9@gmail.com>
2022-01-24 15:06:33 -0300

    Install prettier extension to format and perform corrections

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -3,129 +3,103 @@ import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
 function ContactForm() {
+	const phoneValidate = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
-  const phoneValidate = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+	const newContactSchema = Yup.object().shape({
+		name: Yup.string()
+			.min(3, 'El nombre es muy corto')
+			.max(30, 'El nombre es muy largo')
+			.required('El nombre es Obligatorio'),
+		email: Yup.string().email('El correo es obligatorio').required('El correo es obligatorio'),
+		phone: Yup.string()
+			.matches(phoneValidate, 'Numero no valido')
+			.required('El numero de telefono es obligatorio')
+			.min(8, 'Deben ser 8 caracteres minimo'),
+		message: Yup.string().max(50, 'El mensaje es muy largo').required('Debe colocar un mensaje')
+	});
 
-  const newContactSchema = Yup.object().shape({
-    nameContact: Yup.string()
-              .min(3, 'El nombre es muy corto')
-              .max(30, 'El nombre es muy largo')
-              .required('El nombre es Obligatorio'),
-    emailContact: Yup.string()
-              .email('El correo es obligatorio')
-              .required('El correo es obligatorio'),
-    phoneContact: Yup.string()
-              .matches(phoneValidate, 'Numero no valido')
-              .required('El numero de telefono es obligatorio')
-              .min(8, 'Deben ser 8 caracteres minimo'),
-    mesaggeContact: Yup.string()
-                .max(50, 'El mensaje es muy largo')
-                .required('Debe colocar un mensaje'),
-  })
+	const handleSubmit = (values) => {
+		// to do (logica pendiente)
+	};
 
-  const handleSubmit = (values) => {
-    console.log(values)
-  } 
+	return (
+		<div className="form-container">
+			<Formik
+				initialValues={{
+					name: '',
+					email: '',
+					phone: '',
+					message: ''
+				}}
+				onSubmit={(values) => {
+					handleSubmit(values);
+				}}
+				validationSchema={newContactSchema}
+			>
+				{({ errors, touched }) => {
+					return (
+						<Form>
+							<div>
+								<label htmlFor="name">Nombre:</label>
+								<Field
+									className="input-field"
+									name="name"
+									id="name"
+									type="text"
+									placeholder="Ingrese su nombre"
+								/>
+								{/* Cambiar el div del mensaje de error por el componente creado para form category */}
+								{errors.name && touched.name ? <div>{errors.name}</div> : null}
+							</div>
 
-  return (
-    <div className="form-container">
-      <Formik
-        initialValues={{
-          nameContact: '',
-          emailContact: '',
-          phoneContact: '',
-          mesaggeContact:''
-        }}
-        onSubmit={ (values) => {
-          handleSubmit(values)
-        }}
-        validationSchema={newContactSchema}
-      >
-        {({errors, touched}) => {
+							<div>
+								<label htmlFor="email">Email:</label>
+								<Field
+									className="input-field"
+									name="email"
+									id="email"
+									type="text"
+									placeholder="Ingrese correo electronico"
+								/>
+								{/* Cambiar el div del mensaje de error por el componente creado para form category */}
+								{errors.email && touched.email ? <div>{errors.email}</div> : null}
+							</div>
 
-          return (
-          <Form>
+							<div>
+								F<label htmlFor="phone">Teléfono:</label>
+								<Field
+									className="input-field"
+									name="phone"
+									id="phone"
+									type="text"
+									placeholder="Ingrese su número de teléfono"
+								/>
+								{/* Cambiar el div del mensaje de error por el componente creado para form category */}
+								{errors.phone && touched.phone ? <div>{errors.phone}</div> : null}
+							</div>
 
-          <div>
-            <label
-              htmlFor='nameContact'
-            >Nombre:</label>
-            <Field
-              className="input-field"
-              name="nameContact"
-              id="nameContact"
-              type="text"
-              placeholder="Ingrese su nombre"
-            />
-            {/* Cambiar el div del mensaje de error por el componente creado para form category */}
-            {errors.nameContact && touched.nameContact ? (
-              <div>{errors.nameContact}</div>
-            ) : null}
-          </div>
+							<div>
+								<label htmlFor="message">Mensaje:</label>
+								<Field
+									className="input-field"
+									name="message"
+									type="text"
+									id="message"
+									placeholder="Mensaje"
+								/>
+								{/* Cambiar el div del mensaje de error por el componente creado para form category */}
+								{errors.message && touched.message ? <div>{errors.message}</div> : null}
+							</div>
 
-          <div>
-            <label
-              htmlFor='emailContact'
-            >Email:</label>
-            <Field
-              className="input-field"
-              name="emailContact"
-              id="emailContact"
-              type="text"
-              placeholder="Ingrese correo electronico"
-            />
-            {/* Cambiar el div del mensaje de error por el componente creado para form category */}
-            {errors.emailContact && touched.emailContact ? (
-              <div>{errors.emailContact}</div>
-            ) : null}
-          </div>
-
-          <div>
-            <label
-              htmlFor='phoneContact'
-            >Teléfono:</label>
-            <Field
-              className="input-field"
-              name="phoneContact"
-              id="phoneContact"
-              type="text"
-              placeholder="Ingrese su número de teléfono"
-            />
-            {/* Cambiar el div del mensaje de error por el componente creado para form category */}
-            {errors.phoneContact && touched.phoneContact ? (
-              <div>{errors.phoneContact}</div>
-            ) : null}
-          </div>
-
-          <div>
-            <label
-              htmlFor='mesaggeContact'
-            >Mensaje:</label>
-            <Field
-              className="input-field"
-              name="mesaggeContact"
-              type="text"
-              id="mesaggeContact"
-              placeholder="Mensaje"
-            />
-            {/* Cambiar el div del mensaje de error por el componente creado para form category */}
-            {errors.mesaggeContact && touched.mesaggeContact? (
-              <div>{errors.mesaggeContact}</div>
-            ) : null}
-          </div>
-
-          <input
-            className="submit-btn"
-            type="submit"
-            value="Enviar"
-          />
-
-        </Form>
-        )}}
-        
-      </Formik>
-    </div>
-  );
+							<button className="submit-btn" type="submit">
+								Enviar
+							</button>
+						</Form>
+					);
+				}}
+			</Formik>
+		</div>
+	);
 }
 
 export default ContactForm;
