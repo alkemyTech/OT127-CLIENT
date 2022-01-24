0d97a10f20ef417e3995366f9c977322e06fb6d1
cardosonicolas <cardoso.leandro.nicolas@gmail.com>
2022-01-24 06:44:04 -0300

    create handleChangeImage

--- src/Components/Projects/ProjectsForm.js
+++ src/Components/Projects/ProjectsForm.js
@@ -17,6 +17,13 @@ const ProjectsForm = () => {
     }));
   };
 
+  const handleChangeImage = (e) => {
+    setProject((prevProject) => ({
+      ...prevProject,
+      image: e.target.files[0],
+    }));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
   };
@@ -40,7 +47,13 @@ const ProjectsForm = () => {
         onChange={handleChangeDescription}
         placeholder="Write some description"
       ></input>
-
+      <input
+        type="file"
+        name="file"
+        accept=".png, .jpg"
+        onChange={handleChangeImage}
+      ></input>
+      <img src={image} alt="" />
       <button className="submit-btn" type="submit">
         Send
       </button>
