e281a4b16c141a385f0de1dd2919978d33015e2e
danieldamian09 <humbertorivero9@gmail.com>
2022-01-24 18:23:15 -0300

    clear F and format code

--- src/Components/Contact/ContactForm.js
+++ src/Components/Contact/ContactForm.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import { Formik, Form, Field } from 'formik';
-import * as Yup from 'yup';
+import React from "react";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 function ContactForm() {
-	const phoneValidate = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+	const phoneValidate =
+		/^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
 	const newContactSchema = Yup.object().shape({
 		name: Yup.string()
-			.min(3, 'El nombre es muy corto')
-			.max(30, 'El nombre es muy largo')
-			.required('El nombre es Obligatorio'),
-		email: Yup.string().email('El correo es obligatorio').required('El correo es obligatorio'),
+			.min(3, "El nombre es muy corto")
+			.max(30, "El nombre es muy largo")
+			.required("El nombre es Obligatorio"),
+		email: Yup.string()
+			.email("El correo es obligatorio")
+			.required("El correo es obligatorio"),
 		phone: Yup.string()
-			.matches(phoneValidate, 'Numero no valido')
-			.required('El numero de telefono es obligatorio')
-			.min(8, 'Deben ser 8 caracteres minimo'),
-		message: Yup.string().max(50, 'El mensaje es muy largo').required('Debe colocar un mensaje')
+			.matches(phoneValidate, "Numero no valido")
+			.required("El numero de telefono es obligatorio")
+			.min(8, "Deben ser 8 caracteres minimo"),
+		message: Yup.string()
+			.max(50, "El mensaje es muy largo")
+			.required("Debe colocar un mensaje"),
 	});
 
 	const handleSubmit = (values) => {
@@ -26,10 +31,10 @@ function ContactForm() {
 		<div className="form-container">
 			<Formik
 				initialValues={{
-					name: '',
-					email: '',
-					phone: '',
-					message: ''
+					name: "",
+					email: "",
+					phone: "",
+					message: "",
 				}}
 				onSubmit={(values) => {
 					handleSubmit(values);
@@ -49,7 +54,9 @@ function ContactForm() {
 									placeholder="Ingrese su nombre"
 								/>
 								{/* Cambiar el div del mensaje de error por el componente creado para form category */}
-								{errors.name && touched.name ? <div>{errors.name}</div> : null}
+								{errors.name && touched.name ? (
+									<div>{errors.name}</div>
+								) : null}
 							</div>
 
 							<div>
@@ -62,11 +69,13 @@ function ContactForm() {
 									placeholder="Ingrese correo electronico"
 								/>
 								{/* Cambiar el div del mensaje de error por el componente creado para form category */}
-								{errors.email && touched.email ? <div>{errors.email}</div> : null}
+								{errors.email && touched.email ? (
+									<div>{errors.email}</div>
+								) : null}
 							</div>
 
 							<div>
-								F<label htmlFor="phone">Teléfono:</label>
+								<label htmlFor="phone">Teléfono:</label>
 								<Field
 									className="input-field"
 									name="phone"
@@ -75,7 +84,9 @@ function ContactForm() {
 									placeholder="Ingrese su número de teléfono"
 								/>
 								{/* Cambiar el div del mensaje de error por el componente creado para form category */}
-								{errors.phone && touched.phone ? <div>{errors.phone}</div> : null}
+								{errors.phone && touched.phone ? (
+									<div>{errors.phone}</div>
+								) : null}
 							</div>
 
 							<div>
@@ -88,7 +99,9 @@ function ContactForm() {
 									placeholder="Mensaje"
 								/>
 								{/* Cambiar el div del mensaje de error por el componente creado para form category */}
-								{errors.message && touched.message ? <div>{errors.message}</div> : null}
+								{errors.message && touched.message ? (
+									<div>{errors.message}</div>
+								) : null}
 							</div>
 
 							<button className="submit-btn" type="submit">
