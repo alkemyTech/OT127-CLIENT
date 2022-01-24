16d312f76ba7e11ace787545312baf2989cbbe3f
Nel Rodriguez <nel.rodriguez22@gmail.com>
2022-01-24 19:29:13 +0000

    ckeditor agregado

--- src/Components/Testimonials/TestimonialsForm.js
+++ src/Components/Testimonials/TestimonialsForm.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import '../FormStyles.css';
 
 const TestimonialForm = () => {
@@ -6,10 +8,8 @@ const TestimonialForm = () => {
        name: '',
        description: '' 
 	 });
-	const [ images, setImages ] = useState()
+	const [ image, setImage ] = useState()
 	
-
-
     const handleChange = (e) => {
         if(e.target.name === 'name'){
             setInitialValues({...initialValues, name: e.target.value})
@@ -17,25 +17,41 @@ const TestimonialForm = () => {
             setInitialValues({...initialValues, description: e.target.value})
         }
     }
-
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(initialValues);
     }
-
 	function onImageChange(e) {
-		setImages(e.target.files)
+		setImage(e.target.files)
 		console.log(e.target.files);
 	}
 
     return (
         <form className="form-container" onSubmit={handleSubmit}>
 			<input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Testimonial Title"></input>
-			<input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Testimonial description"></input>
-			<input type="file" name="image" onChange={onImageChange} />
-            <button className="submit-btn" type="submit">Send</button>
+			 <input type="file" name="image" onChange={onImageChange} />
+			 {/* <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Testimonial description"></input> */}
+			 <CKEditor
+				 editor={ClassicEditor}
+				 data="<p>Testimonial</p>"
+				 onReady={editor => {
+					 // You can store the "editor" and use when it is needed.
+					 console.log('Editor is ready to use!', editor);
+				 }}
+				 onChange={(event, editor) => {
+					 const data = editor.getData();
+					 console.log({ event, editor, data });
+				 }}
+				 onBlur={(event, editor) => {
+					 console.log('Blur.', editor);
+				 }}
+				 onFocus={(event, editor) => {
+					 console.log('Focus.', editor);
+				 }}
+			 />
+			<button className="submit-btn" type="submit">Send</button>
         </form>
     );
 }
- 
+
 export default TestimonialForm;
