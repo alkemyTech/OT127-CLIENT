120eb50e7507dcd22b47cd011a21fe1d57f9ca7b
silversurferbeats <gabrielsuarez89@gmail.com>
2022-01-24 20:34:03 -0300

    creacion componente de contenido de actividad

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -19,12 +19,20 @@ const ActivitiesForm = () => {
         e.preventDefault();
         console.log(initialValues);
     }
+
+    //renderHTML
+    function enviarDatos(){
+        let texto = document.getElementById('campo').value;    
+        document.body.innerHTML = texto;
+    }
+
     
     return (
         <form className="form-container" onSubmit={handleSubmit}>
             <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Activity Title"></input>
             <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some activity description"></input>
-            <button className="submit-btn" type="submit">Send</button>
+            <textarea id="campo" type="text" name="campo" cols="40" rows="3" ><input/></textarea><br/>
+            <button className="submit-btn" type="submit" onClick={()=> enviarDatos()} >Render</button><br/>
         </form>
     );
 }
