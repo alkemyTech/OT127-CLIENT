76b15ec1c46fb39e10697d4523e996c6170d0d00
chriscaracach <christiancaracach@gmail.com>
2022-01-25 11:05:10 -0300

    fix: add logic to newsList function

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -3,21 +3,23 @@ import React, { useState } from "react";
 const News = () => {
   const [news, setNews] = useState([]); //News va a ser sacado de la API en un futuro
 
-  const newsMap = news.map((element) => {
-    return (
-      <li className="card-info" key={element.id}>
-        <h3>{element.name}</h3>
-        <p>{element.description}</p>
-      </li>
+  const newsList = () => {
+    return news.length ? (
+      news.map((element) => (
+        <li className="card-info" key={element.id}>
+          <h3>{element.name}</h3>
+          <p>{element.description}</p>
+        </li>
+      ))
+    ) : (
+      <p>No hay novedades</p>
     );
-  });
+  };
 
   return (
     <>
       <h1>Novedades</h1>
-      <ul className="list-container">
-        {news.length > 0 ? newsMap : <p>No hay novedades</p>}
-      </ul>
+      <ul className="list-container">{newsList()}</ul>
     </>
   );
 };
