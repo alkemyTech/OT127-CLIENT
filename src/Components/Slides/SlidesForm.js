7ae5cda25dc911d441c4b351eebd0550d08e17b5
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-01-24 21:10:10 -0300

    feat  axios y put

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useParams } from "react-router-dom";
 
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
@@ -7,26 +8,98 @@ import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import "@ckeditor/ckeditor5-build-classic/build/translations/es";
 
+import axios from "axios";
+
 import "../FormStyles.css";
 
 const SlidesForm = () => {
-  const [formData, setFormData] = useState({
-    name: "",
-    description: "",
-    order: "",
-    image: "",
-  });
-  const [preview, setPreview] = useState(null);
+  const [initialValues, setInitialValues] = useState(null);
+  const [slidesData, setSlidesData] = useState([]); // para validar order
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
-  const imagePreview = (file) => {
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = () => {
-      setPreview(reader.result);
+  const { id } = useParams();
+
+  useEffect(() => {
+    const getSlidesData = async () => {
+      const url = "http://ongapi.alkemy.org/api/slides";
+
+      await axios
+        .get(url)
+        .then((res) => {
+          const {
+            data: { data },
+          } = res;
+          setSlidesData(data);
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
     };
+    getSlidesData();
+  }, []);
+  console.log(slidesData);
+
+  useEffect(() => {
+    const getSlidesById = async () => {
+      if (id) {
+        setLoading(true);
+        const url = `http://ongapi.alkemy.org/api/slides/${id}`;
+        await axios
+          .get(url)
+          .then((res) => {
+            const { data: status } = res;
+            if (status.success) {
+              const { data } = status;
+              setInitialValues({
+                name: data.name,
+                description: data.description,
+                order: data.order ? data.order : "",
+                image: data.image,
+              });
+            } else {
+              alert(status.message);
+            }
+          })
+          .catch((err) => {
+            alert(err.message);
+          });
+      }
+      setLoading(false);
+    };
+    getSlidesById();
+  }, [id]);
+
+  const handleSubmit = async (formValues) => {
+    if (id) {
+      const url = `http://ongapi.alkemy.org/api/slides/${id}`;
+      await axios
+        .put(url, {
+          name: formValues.name,
+          description: formValues.description,
+          order: formValues.order,
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    } else if (!id) {
+      const url = "http://ongapi.alkemy.org/api/slides";
+
+      await axios
+        .post(url, {
+          name: formValues.name,
+          description: formValues.description,
+          order: formValues.order,
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    }
   };
 
-  const formSchema = Yup.object({
+  const inputFileRef = useRef();
+
+  const validations = Yup.object({
     name: Yup.string()
       .min(4, "Debe tener al menos 4 caracteres")
       .required("Este campo es obligatorio"),
@@ -40,95 +113,103 @@ const SlidesForm = () => {
 
   return (
     <>
-      <Formik
-        initialValues={{
-          name: "",
-          description: "",
-          order: "",
-          image: "",
-        }}
-        validationSchema={formSchema}
-        onSubmit={(values, { resetForm }) => {
-          console.log(values);
-          let formValues = {
-            name: values.name,
-            description: values.description,
-            order: values.order,
-            image: values.image.name,
-          };
+      {loading ? (
+        <p>LOADING...</p>
+      ) : (
+        <Formik
+          initialValues={{
+            name: initialValues ? initialValues.name : "",
+            description: initialValues ? initialValues.description : "",
+            order: initialValues ? initialValues.order : "",
+            image: initialValues ? initialValues.image : "",
+          }}
+          validationSchema={validations}
+          onSubmit={(values, { resetForm }) => {
+            console.log(values);
+            let formValues = {
+              name: values.name,
+              description: values.description,
+              order: values.order,
+              image: values.image.name,
+            };
+            setInitialValues(null);
+            handleSubmit(formValues);
+            inputFileRef.current.value = "";
+            resetForm({
+              values: {
+                name: "",
+                description: "",
+                order: "",
+                image: "",
+              },
+            });
+          }}
+        >
+          {({ setFieldValue }) => (
+            <Form className="form-container">
+              <label htmlFor="name">Titulo</label>
+              <Field
+                id="name"
+                className="input-field"
+                type="text"
+                name="name"
+                placeholder="Slide Title"
+              />
 
-          setFormData({ ...formData, ...formValues });
+              <ErrorMessage name="name" render={(msg) => <div>{msg}</div>} />
+              <label htmlFor="description">Descripcion</label>
+              <Field name="description">
+                {({ field }) => (
+                  <>
+                    <CKEditor
+                      config={{
+                        language: "es",
+                      }}
+                      editor={ClassicEditor}
+                      data={field.value}
+                      onChange={(event, editor) => {
+                        setFieldValue(field.name, editor.getData());
+                      }}
+                    />
+                  </>
+                )}
+              </Field>
 
-          setPreview(null);
-          resetForm();
-        }}
-      >
-        {({ values, setFieldValue }) => (
-          <Form className="form-container">
-            <Field
-              className="input-field"
-              type="text"
-              name="name"
-              placeholder="Slide Title"
-            />
-
-            <ErrorMessage name="name" render={(msg) => <div>{msg}</div>} />
-
-            <Field name="description">
-              {({ field }) => (
-                <>
-                  <CKEditor
-                    config={{
-                      language: "es",
-                    }}
-                    editor={ClassicEditor}
-                    data={field.value}
-                    onChange={(event, editor) => {
-                      setFieldValue(field.name, editor.getData());
-                    }}
-                  />
-                </>
-              )}
-            </Field>
-
-            <ErrorMessage
-              name="description"
-              render={(msg) => <div>{msg}</div>}
-            />
-            <Field
-              className="input-field"
-              type="number"
-              name="order"
-              onChange={(e) => {
-                setFieldValue("order", e.currentTarget.value);
-              }}
-              placeholder="ingrese un numero"
-            />
-
-            <ErrorMessage name="order" render={(msg) => <div>{msg}</div>} />
-
-            <input
-              className="input-field"
-              type="file"
-              onChange={(e) => {
-                setFieldValue("image", e.currentTarget.files[0]);
-              }}
-              accept=".jpg, .png"
-            />
-            <ErrorMessage name="image" render={(msg) => <div>{msg}</div>} />
-            {values.image && imagePreview(values.image)}
-            <div>
-              {preview && (
-                <img src={preview} alt="preview" width="150px" height="auto" />
-              )}
-            </div>
-
-            <button type="submit" className="submit-btn">
-              Login
-            </button>
-          </Form>
-        )}
-      </Formik>
+              <ErrorMessage
+                name="description"
+                render={(msg) => <div>{msg}</div>}
+              />
+              <label htmlFor="order">Numero de orden</label>
+              <Field
+                id="order"
+                className="input-field"
+                type="number"
+                name="order"
+                onChange={(e) => {
+                  setFieldValue("order", parseInt(e.currentTarget.value));
+                }}
+                placeholder="ingrese un numero"
+              />
+              {error && <p>El numero de orden ya esta siendo usado</p>}
+              <ErrorMessage name="order" render={(msg) => <div>{msg}</div>} />
+              <label htmlFor="order">Cargar Imagen</label>
+              <input
+                ref={inputFileRef}
+                className="input-field"
+                type="file"
+                onChange={(e) => {
+                  setFieldValue("image", e.currentTarget.files[0]);
+                }}
+                accept=".jpg, .png"
+              />
+              <ErrorMessage name="image" render={(msg) => <div>{msg}</div>} />
+              <button type="submit" className="submit-btn">
+                Enviar
+              </button>
+            </Form>
+          )}
+        </Formik>
+      )}
     </>
   );
 };
