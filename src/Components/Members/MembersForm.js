bae67dbed4352d8b25f7073482be26d28d52121f
silversurferbeats <gabrielsuarez89@gmail.com>
2022-01-25 10:32:02 -0300

    FormularioMiembros

--- src/Components/Members/MembersForm.js
+++ src/Components/Members/MembersForm.js
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 import '../FormStyles.css';
+import { CKEditor } from '@ckeditor/ckeditor5-react';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { Formik, Field, Form } from 'formik';
+
+//import "../Members/Members.scss";
+
 
 const MembersForm = () => {
   const [initialValues, setInitialValues] = useState({
@@ -12,20 +18,70 @@ const MembersForm = () => {
       setInitialValues({...initialValues, name: e.target.value})
     } if(e.target.name === 'description'){
       setInitialValues({...initialValues, description: e.target.value})
-    }
+    } 
   }
-
   const handleSubmit = (e) => {
     e.preventDefault();
     console.log(initialValues);
   }
+  
+
+  let theEditor;
+
+    ClassicEditor
+      .create(document.querySelector('#agenda_mesyuarat'))
+      .then(editor => {
+        theEditor = editor;
+
+      })
+      .catch(error => {
+        console.error(error);
+      });
+
+
+    function getDataFromTheEditor() {
+      return theEditor.getData();
+    }
+    
+    function send_1() {
+      var testing = "<p><strong>Tilte</strong></p><p><i>123</i></p>";
+      theEditor.setData(testing);
+    }
 
   return (
-    <form className="form-container" onSubmit={handleSubmit}>
-      <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Name"></input>
-      <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some description"></input>
-      <button className="submit-btn" type="submit">Send</button>
-    </form>
+    <>
+      <Formik
+        id='formulario'
+        className='bgImage'
+        initialValues={{ name: "", description: ""}}
+        onSubmit={async values => {
+          await new Promise(resolve => setTimeout(resolve, 500));
+          alert(JSON.stringify(values, null, 2));
+        }}>
+        <div className='formWrap'>
+          <Form className='required_notification'>
+            <div>
+              <Field name="name" type="text" />
+            </div>
+            <div>
+              <CKEditor
+                editor={ ClassicEditor }
+                data="<p>Hello from CKEditor 5!</p>"
+                className="input-field"
+                name="description"
+                onReady={ editor => {
+                    // You can store the "editor" and use when it is needed.
+                    console.log( 'Editor is ready to use!', editor );
+                } }
+              />
+            </div>
+            <div className='submit'>
+              <button type='submit' className='submit' >Submit</button>
+            </div>
+          </Form>
+        </div>
+      </Formik>
+    </>
   );
 }
  
