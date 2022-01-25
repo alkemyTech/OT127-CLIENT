ede6cdf556c2499ca8b248ef14bc3f9f0ac81a9e
silversurferbeats <gabrielsuarez89@gmail.com>
2022-01-25 11:12:29 -0300

    delete  memberform

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import TestimonialForm from "./Components/Testimonials/TestimonialsForm";
 import UserForm from "./Components/Users/UsersForm";
 import SchoolCampaign from "./Campaigns/School/SchoolCampaign";
 import ToysCampaign from "./Campaigns/Toys/ToysCampaign";
-import MembersForm from "./Components/Members/MembersForm";
 import ProjectsForm from "./Components/Projects/ProjectsForm";
 import RegisterForm from "./Components/Auth/RegisterForm";
 import LoginForm from "./Components/Auth/LoginForm";
@@ -39,7 +38,6 @@ function App() {
 					/>
 					<Route path="/create-testimonials" component={TestimonialForm} />
 					<Route path="/create-user" component={UserForm} />
-					<Route path="/create-member" component={MembersForm} />
 					<Route path="/create-project" component={ProjectsForm} />
 					<Route path="/school-campaign" component={SchoolCampaign} />
 					<Route path="/toys-campaign" component={ToysCampaign} />
