785be238346178ddebd7dbfbde095ba485c67894
Nel Rodriguez <nel.rodriguez22@gmail.com>
2022-01-25 15:07:12 +0000

    fix:props&classes

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,21 +1,21 @@
-import { Title } from '../Titulosynovedades/Title'
-import "./contact.css";
-const Contact = (props) => {
+import Title  from '../Titulosynovedades/Title'
+import "./contact.scss";
+const Contact = ({ name, short_description, address, phone, cellphone }) => {
 	return (
 		<>
-			<div className="contact">
-				<div className="contact__title">
+			<div className="contact__container">
+				<div className="contact__title-text">
 					<Title title="Contacto" />
 				</div>
 				{/* las variables las saque de la api de alkemy/organization porque 
 				dice que las props van a venir mas adelante desde una api, supuse
 				que era de esa */}
 				<div className="contact__content">
-					<p>Nombre:{props.name}</p>
-					<p>Descripcion:{props.short_description}</p>
-					<p>Direccion:{props.address}</p>
-					<p>Telefono:{props.phone}</p>
-					<p>Celular:{props.cellphone}</p>
+					<p>Nombre:{name}</p>
+					<p>Descripcion:{short_description}</p>
+					<p>Direccion:{address}</p>
+					<p>Telefono:{phone}</p>
+					<p>Celular:{cellphone}</p>
 				</div>
 				<div className="contact__footer">
 					<h3>Redes Sociales</h3>
