06bd2b2ba58932079fa6606a0667d97f6a204a15
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-01-26 16:22:22 -0300

    Creada tabla de news y mostrando datos de mock

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -1,32 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../CardListStyles.css';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 const NewsList = () => {
     const newsMock = [
-        {id: 2, name: 'Titulo de prueba', description: 'Descripcion de prueba'},
-        {id: 1, name: 'Titulo de prueba', description: 'Descripcion de prueba'},
-        {id: 3, name: 'Titulo de prueba', description: 'Descripcion de prueba'}
+        {id: 2,  name: 'Titulo de prueba', image: '', description: 'Descripcion de prueba', createdAt:'26-01-2022'},
+        {id: 1,  name: 'Titulo de prueba', image: '', description: 'Descripcion de prueba', createdAt:'26-01-2022'},
+        {id: 3,  name: 'Titulo de prueba',image: '', description: 'Descripcion de prueba', createdAt:'26-01-2022'}
     ];
+
+    function createData(name, image, createdAt, actions) {
+        return { name, image, createdAt, actions };
+      }
+
+    const rows = newsMock.map(mock => createData(mock.name, mock.image,mock.createdAt))
 
     return (
         <div>
         <Link to='/backoffice/news/create'>Create news</Link>
-            <h1>Listado de Novedades</h1>
-            <ul className="list-container">
-                {newsMock.length > 0 ? 
-                    newsMock.map((element) => {
-                        return(
-                            <li className="card-info" key={element.id}>
-                                <h3>{element.name}</h3>
-                                <p>{element.description}</p>
-                            </li>
-                        )
-                    })
-                :
-                    <p>No hay novedades</p>
-                }
-            </ul>
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+                <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell >Image</TableCell>
+                <TableCell >Created At</TableCell>
+                <TableCell >Actions</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {rows.map((row) => (
+                <TableRow
+                    key={row.name}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                    <TableCell component="th" scope="row">
+                    {row.name}
+                    </TableCell>
+                    <TableCell ><img src={row.image} alt='News_image'/></TableCell>
+                    <TableCell >{row.createdAt}</TableCell>
+                </TableRow>
+                ))}
+            </TableBody>
+            </Table>
+        </TableContainer>
         </div>
     );
 }
