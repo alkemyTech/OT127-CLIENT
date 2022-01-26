8701fbd2949e679bf3ed4bece2b0ba53772c51ff
Nel Rodriguez <nel.rodriguez22@gmail.com>
2022-01-26 00:11:03 +0000

    atom-func-editupd

--- src/Components/Testimonials/TestimonialsForm.js
+++ src/Components/Testimonials/TestimonialsForm.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
-import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import '../FormStyles.css';
+import React, { useState } from 'react'
+import { CKEditor } from '@ckeditor/ckeditor5-react'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import {useParams} from 'react-router-dom'
+import axios from 'axios'
+import '../FormStyles.css'
 
-const TestimonialForm = (props) => {
+const TestimonialForm = () => {
+	const id = useParams().id
+	const url = "http://ongapi.alkemy.org/api/testimonials/"
 	const [ initialValues, setInitialValues ] = useState({
 		name: '',
 		description: ''
@@ -18,12 +22,27 @@ const TestimonialForm = (props) => {
 	}
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(initialValues);
+		console.log(initialValues)
 	}
 	const onImageChange = (e) => {
 		setImage(e.target.files)
-		console.log(e.target.files);
+		console.log(e.target.files)
 	}
+	const editUpdate =(id)=>{
+	if (id === undefined) {
+		//create--testimonial
+	}else{
+		//update--testimonial(id)-rellenar campos para editar y despues enviar
+		axios.get(url`/${id}`)
+		.then(res => {
+		setInitialValues({
+			name: res.data.name,
+			description: res.data.description
+		})
+	}	
+	}
+	}
+
 	return (
 		<form className="form-container" onSubmit={handleSubmit}>
 			<input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Testimonial Title"></input>
@@ -50,4 +69,4 @@ const TestimonialForm = (props) => {
 		</form>
 	);
 }
-export default TestimonialForm;
+export default TestimonialForm
