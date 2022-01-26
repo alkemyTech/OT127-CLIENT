9e8dd1677e78096f84e1faf9947c9fff545ee81b
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-01-26 18:42:56 -0300

    Creado metodo getAll y getById

--- src/Services/publicActivityService.js
+++ src/Services/publicActivityService.js
@@ -1,13 +1,25 @@
-const axios = require('axios')
+const axios = require("axios");
 
 class ActivitiesService {
-  getAll = async (req, res) => {
-    
+  constructor() {
+    this.API_URL = "http://ongapi.alkemy.org/api/activities";
+  }
+
+  getAll = async () => {
+    await axios.get(this.API_URL).then((response) => response.data);
+  };
+
+  getById = async (id) => {
+    if (id) {
+      await axios
+        .get(`${this.API_URL}/${id}`)
+        .then((response) => response.data);
+    }
   };
 }
 
 const activitiesController = new ActivitiesService();
 
 module.exports = {
-    activitiesController,
+  activitiesController,
 };
