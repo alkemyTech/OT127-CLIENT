26945012a0e7d673bb157de9317b4b911df9fdf4
Nel Rodriguez <nel.rodriguez22@gmail.com>
2022-01-27 13:27:21 +0000

    atom-agregado-campos

--- src/Components/Testimonials/TestimonialsForm.js
+++ src/Components/Testimonials/TestimonialsForm.js
@@ -35,40 +35,41 @@ const TestimonialForm = () => {
 		console.log(e.target.files)
 	}
 	//----------------------//
-	const formik = useFormik({
-		initialValues: {
-			name: "",
-			description: "",
-			imagen: ""
-		},
-		validate: (values) => {
-			const errors = {};
-			if (!values.email) {
-				errors.name = "El campo no puede estar vacio";
-			} else if (!values.password) {
-				errors.description = "El campo no puede estar vacio";
-			}
-			return errors;
-		},
-		onSubmit: () => {
-			if (!formik.isValid) {
-				return
-			}
-		},
-	})
+	// const formik = useFormik({
+	// 	initialValues: {
+	// 		name: "",
+	// 		description: "",
+	// 		imagen: ""
+	// 	},
+	// 	validate: (values) => {
+	// 		const errors = {};
+	// 		if (!values.email) {
+	// 			errors.name = "El campo no puede estar vacio";
+	// 		} else if (!values.password) {
+	// 			errors.description = "El campo no puede estar vacio";
+	// 		}
+	// 		return errors;
+	// 	},
+	// 	onSubmit: () => {
+	// 		if (!formik.isValid) {
+	// 			return
+	// 		}
+	// 	},
+	// })
 	//-----------------------//
 	useEffect(() => {
 		if (id){
 			axios
 				.get(`${url}${id}`)
 				.then((res) => {
-					setTestimonial(res.data)
-					console.log("asd data", res.data);
+					setTestimonial(res.data.data)
+					console.log("asd data", res.data.data);
 					setInitialValues({
-						name: res.data.name,
-						description: res.data.description,
-						image: res.data.image
+						name: res.data.data.name,
+						description: res.data.data.description,
 					})
+					setImage(res.data.data.image)
+
 				})
 				.catch((err) => {
 					console.log(err)
@@ -93,26 +94,33 @@ const TestimonialForm = () => {
 				name="name"
 				value={initialValues.name}
 				onChange={handleChange}
-				placeholder="Testimonial Title"
+				placeholder={initialValues.name}
 			></input>
 			<input type="file" name="image" onChange={onImageChange} />
+			<input
+				type="text"
+				name="imagefield"
+				value={image}
+				placeholder={image}
+				className="input-field"
+			></input>
 			<CKEditor
 				editor={ClassicEditor}
-				data="<p>Testimonial</p>"
+				data={initialValues.description}
 				onReady={(editor) => {
 					// You can store the "editor" and use when it is needed.
 					console.log("Editor is ready to use!", editor)
 				}}
-				onChange={(event, editor) => {
-					const data = editor.getData()
-					console.log({ event, editor, data })
-				}}
-				onBlur={(event, editor) => {
-					console.log("Blur.", editor)
-				}}
-				onFocus={(event, editor) => {
-					console.log("Focus.", editor)
-				}}
+				// 	onChange={(event, editor) => {
+				// 		const data = editor.getData()
+				// 		console.log({ event, editor, data })
+				// 	}}
+				// 	onBlur={(event, editor) => {
+				// 		console.log("Blur.", editor)
+				// 	}}
+				// 	onFocus={(event, editor) => {
+				// 		console.log("Focus.", editor)
+				// 	}}
 			/>
 			<button className="submit-btn" type="submit" onClick="">
 				Send
