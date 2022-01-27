a3b337eaac09f4613f5834ec02af5dbf9f41b448
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-01-27 16:05:07 -0300

    mock implementada

--- src/Components/Members/MembersList.js
+++ src/Components/Members/MembersList.js
@@ -3,6 +3,33 @@ import React from "react";
 import "./members-list.scss";
 
 const MembersList = () => {
+  const membersMock = [
+    {
+      id: 1,
+      name: "Nombre",
+      photo:
+        "https://images.unsplash.com/photo-1585670210693-e7fdd16b142e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    },
+    {
+      id: 2,
+      name: "Nombre",
+      photo:
+        "https://images.unsplash.com/photo-1585670210693-e7fdd16b142e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    },
+    {
+      id: 3,
+      name: "Nombre",
+      photo:
+        "https://images.unsplash.com/photo-1585670210693-e7fdd16b142e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    },
+    {
+      id: 4,
+      name: "Nombre",
+      photo:
+        "https://images.unsplash.com/photo-1585670210693-e7fdd16b142e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    },
+  ];
+
   return (
     <div className=" container datagrid">
       <table>
@@ -16,16 +43,20 @@ const MembersList = () => {
         </thead>
 
         <tbody>
-          <tr>
-            <td>nombre</td>
-            <td>data</td>
-            <td>
-              <button href="#">Editar</button>
-            </td>
-            <td>
-              <button>Eliminar</button>
-            </td>
-          </tr>
+          {membersMock.map((member) => (
+            <tr key={member.id}>
+              <td>{member.name}</td>
+              <td>
+                <img src={member.photo} alt={member.photo} width="50px" />
+              </td>
+              <td>
+                <button>Eliminar</button>
+              </td>
+              <td>
+                <button href="#">Editar</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </div>
