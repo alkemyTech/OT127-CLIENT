503e1434c5f65682fa82aa04aeb212dae2c219f4
matias <solisma42@gmail.com>
2022-01-27 15:21:18 -0300

    Definida estructura basica

--- src/Components/Users/UsersList.js
+++ src/Components/Users/UsersList.js
@@ -2,16 +2,36 @@ import React, { useEffect } from 'react';
 import axios from 'axios';
 
 const UsersList = () => {
+    const [users, setUsers] = React.useState([]);
     useEffect(() => {
         const getUsers = async () => {
             // Actualmente la api devuelve 300 usuarios, para trabajar mejor las limite a 5
             const response = await axios.get("http://ongapi.alkemy.org/api/users?limit=5")
-            console.log(response);
+            setUsers(response.data.data)
+            console.log(response.data.data);
         }
         getUsers()
     }, []);
 
-    return <div>Lista de usuarios</div>;
+    return (
+        <div className='UsersList'>
+            <p>Usuarios</p>
+            <table>
+                <tbody>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                        <th>Acción</th>
+                    </tr>
+                    <tr>
+                        <td>Alfreds Futterkiste</td>
+                        <td>solisma@gmail.com</td>
+                        <td>Editar Eliminar</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
 };
 
 export default UsersList;
