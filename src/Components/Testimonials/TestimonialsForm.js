a80489a93b18f87f121d10a8871628eb61d58e2f
Nel Rodriguez <nel.rodriguez22@gmail.com>
2022-01-27 02:39:52 +0000

    atom-formik

--- src/Components/Testimonials/TestimonialsForm.js
+++ src/Components/Testimonials/TestimonialsForm.js
@@ -1,70 +1,123 @@
-import React, { useState } from 'react'
-import { CKEditor } from '@ckeditor/ckeditor5-react'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
-import {useParams} from 'react-router-dom'
-import axios from 'axios'
-import '../FormStyles.css'
+import React, { useEffect, useState } from "react"
+import { CKEditor } from "@ckeditor/ckeditor5-react"
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic"
+import { useParams } from "react-router-dom"
+import axios from "axios"
+import { useFormik } from "formik"
+
+import "../FormStyles.css"
 
 const TestimonialForm = () => {
-	const id = useParams().id
+	// const id = useParams().id
+	const id = 78
 	const url = "http://ongapi.alkemy.org/api/testimonials/"
+	const [ testimonial, setTestimonial ] = useState({})
 	const [ initialValues, setInitialValues ] = useState({
-		name: '',
-		description: ''
-	});
+		name: "",
+		description: ""
+	})
 	const [ image, setImage ] = useState()
+
 	const handleChange = (e) => {
-		if (e.target.name === 'name') {
+		if (e.target.name === "name") {
 			setInitialValues({ ...initialValues, name: e.target.value })
-		} if (e.target.name === 'description') {
+		}
+		if (e.target.name === "description") {
 			setInitialValues({ ...initialValues, description: e.target.value })
 		}
 	}
 	const handleSubmit = (e) => {
-		e.preventDefault();
+		e.preventDefault()
 		console.log(initialValues)
 	}
 	const onImageChange = (e) => {
 		setImage(e.target.files)
 		console.log(e.target.files)
 	}
-	const editUpdate =(id)=>{
-	if (id === undefined) {
-		//crear--testimonial
-	}else{
-		//actualizar--testimonial(id)-rellenar campos para editar y despues enviar
-		axios
-		.get(`${url}/${id}`)
-		.then(res => {
-			log(res.data)
-	}	
-	}
+	//----------------------//
+	const formik = useFormik({
+		initialValues: {
+			name: "",
+			description: "",
+			imagen: ""
+		},
+		validate: (values) => {
+			const errors = {};
+			if (!values.email) {
+				errors.name = "El campo no puede estar vacio";
+			} else if (!values.password) {
+				errors.description = "El campo no puede estar vacio";
+			}
+			return errors;
+		},
+		onSubmit: () => {
+			if (!formik.isValid) {
+				return
+			}
+		},
+	})
+	//-----------------------//
+	useEffect(() => {
+		if (id){
+			axios
+				.get(`${url}${id}`)
+				.then((res) => {
+					setTestimonial(res.data)
+					console.log("asd data", res.data);
+					setInitialValues({
+						name: res.data.name,
+						description: res.data.description,
+						image: res.data.image
+					})
+				})
+				.catch((err) => {
+					console.log(err)
+				})
+		} else {
+		
+		}
+	}, [])
+	const createUpdateTestimonial = (id) => {
+		if (id === undefined) {
+			//crear--testimonial
+		} else {
+			//actualizar--testimonial(id)-rellenar campos para editar y despues enviar
+		}
 	}
 
 	return (
 		<form className="form-container" onSubmit={handleSubmit}>
-			<input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Testimonial Title"></input>
+			<input
+				className="input-field"
+				type="text"
+				name="name"
+				value={initialValues.name}
+				onChange={handleChange}
+				placeholder="Testimonial Title"
+			></input>
 			<input type="file" name="image" onChange={onImageChange} />
 			<CKEditor
 				editor={ClassicEditor}
 				data="<p>Testimonial</p>"
-				onReady={editor => {
+				onReady={(editor) => {
 					// You can store the "editor" and use when it is needed.
-					console.log('Editor is ready to use!', editor);
+					console.log("Editor is ready to use!", editor)
 				}}
 				onChange={(event, editor) => {
-					const data = editor.getData();
-					console.log({ event, editor, data });
+					const data = editor.getData()
+					console.log({ event, editor, data })
 				}}
 				onBlur={(event, editor) => {
-					console.log('Blur.', editor);
+					console.log("Blur.", editor)
 				}}
 				onFocus={(event, editor) => {
-					console.log('Focus.', editor);
+					console.log("Focus.", editor)
 				}}
 			/>
-			<button className="submit-btn" type="submit">Send</button>
+			<button className="submit-btn" type="submit" onClick="">
+				Send
+			</button>
 		</form>
-	);
+	)
 }
 export default TestimonialForm
