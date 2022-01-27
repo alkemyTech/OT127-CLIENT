4b01e2c5cd79b39a32900965edeaa7178915580b
danieldamian09 <humbertorivero9@gmail.com>
2022-01-27 13:42:32 -0300

    Creating a function to make put requests to private endpoints

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -1,21 +1,32 @@
 import axios from "axios";
 
 const config = {
-  headers: {
-    Group: 01, //Aqui va el ID del equipo!!
-  },
+	headers: {
+		Group: 01, //Aqui va el ID del equipo!!
+	},
 };
 
 const Get = () => {
-  axios
-    .get("https://jsonplaceholder.typicode.com/users", config)
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err));
+	axios
+		.get("https://jsonplaceholder.typicode.com/users", config)
+		.then((res) => console.log(res))
+		.catch((err) => console.log(err));
 };
 
 const getSecureHeader = () => {
-  const token = localStorage.getItem("token");
-  token ? { Authorization: "Bearer " + token } : { error: "No token found" };
+	const token = localStorage.getItem("token");
+	token ? {Authorization: "Bearer " + token} : {error: "No token found"};
 };
+
+export function Put() {
+	axios
+		.put(url, data, config)
+		.then((res) => {
+			//to do
+		})
+		.catch((err) => {
+			//to do
+		});
+}
 
 export default Get;
