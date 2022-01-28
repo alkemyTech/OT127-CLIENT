b9a3ccd8d4c7f4b2a4df5942f65b48c69adebf55
matias <solisma42@gmail.com>
2022-01-27 21:54:30 -0300

    Popular con informacion el componente

--- src/Components/News/Detail/NewsDetails.js
+++ src/Components/News/Detail/NewsDetails.js
@@ -1,15 +1,28 @@
 import React, { useEffect } from 'react';
+import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import Title from '../../Titulosynovedades/Title';
 
 
+
 const NewsDetails = ({ title }) => {
+    const [news, setNews] = React.useState({});
     const { id } = useParams()
+    useEffect(() => {
+        const getNews = async () => {
+            const response = await axios.get(`http://ongapi.alkemy.org/api/news/${id}`)
+            setNews(response.data.data);
+        }
+        getNews()
+    }, []);
+
 
     return (
         <div>
             <Title title={title} />
-            <p>Detalle de novedades ID: {id} </p>
+            <p>{news.name}</p>
+            <img src={news.image} />
+            <p>{news.content}</p>
         </div>
     )
 };
