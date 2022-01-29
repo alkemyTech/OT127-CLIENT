d2dae1a867153aee97f89e9ea5a8d051a3535c8f
matias <solisma42@gmail.com>
2022-01-29 19:03:37 -0300

    Agregada funcion del metodo GET

--- src/Services/slidesApiService.js
+++ src/Services/slidesApiService.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
-export const getSlidesData = () => {
-
+export const getSlidesData = async () => {
+    try {
+        const response = await axios.get('http://ongapi.alkemy.org/api/slides')
+        return response
+    } catch (error) {
+        return error
+    }
 }
 
 export const getSlidesDataById = () => {
