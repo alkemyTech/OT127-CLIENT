12f905380b4bbf0b8ed93061c758547b872d0016
matias <solisma42@gmail.com>
2022-01-29 18:51:39 -0300

    Creado archivo slidesApi y definida funciones de metodos principales

---
+++ src/Services/slidesApiService.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+export const getSlidesData = () => {
+
+}
+
+export const getSlidesDataById = () => {
+
+}
+
+export const createNewSlide = () => {
+
+}
+
+export const updateSlide = () => {
+
+}
+
+export const deleteSlide = () => {
+
+} 
