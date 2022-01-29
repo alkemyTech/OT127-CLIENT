4af841ec663081e67ca643798630c1ed1def2d72
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-01-29 00:50:51 -0300

    servicios http creados

---
+++ src/Services/Members/MembersService.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import { Get } from "../publicApiService";
+
+const baseURL = "http://ongapi.alkemy.org/api";
+
+const GetMembers = async () => {
+  const response = await Get(baseURL);
+  return response;
+};
+
+const GetMembersbyId = async (id) => {
+  const response = await Get(baseURL, id);
+  return response;
+};
+
+const PostMember = async (data) => {
+  const response = await axios.post(baseURL, data).catch((err) => err.message);
+  return response;
+};
+
+const PutMember = async (id, data) => {
+  const response = await axios
+    .post(`${baseURL}/${id}`, data)
+    .catch((err) => err.message);
+  return response;
+};
+
+const DeleteMember = async (id) => {
+  const response = await axios
+    .delete(`${baseURL}/${id}`, id)
+    .catch((err) => err.message);
+  return response;
+};
+
+export { GetMembers, GetMembersbyId, PostMember, PutMember, DeleteMember };
