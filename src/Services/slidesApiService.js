55d52b19b1bbaef77591bfaa9ec0f837598983f6
matias <solisma42@gmail.com>
2022-01-29 19:13:29 -0300

    Fixed: retornaba toda el objeto de axios en vez de solo la data

--- src/Services/slidesApiService.js
+++ src/Services/slidesApiService.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export const getSlidesData = async () => {
     try {
         const response = await axios.get('http://ongapi.alkemy.org/api/slides')
-        return response
+        return response.data.data
     } catch (error) {
         return error
     }
