608da7dd12e95ad6f210cbd586bd7c064c5a195e
cardosonicolas <cardoso.leandro.nicolas@gmail.com>
2022-01-29 20:16:56 -0300

    add simple  table

--- src/Pages/Slides/SlideList.js
+++ src/Pages/Slides/SlideList.js
@@ -1,52 +1,38 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
 const slides = {
   data: [
     {
-      id: 865,
+      id: 1,
       name: "Ignacio",
-      description: "<p>asdasdas</p>",
       image: "http://ongapi.alkemy.org/storage/j8Uo4skOTP.jpeg",
       order: 5454,
-      user_id: null,
-      created_at: "2022-01-27T04:31:35.000000Z",
-      updated_at: "2022-01-27T04:31:35.000000Z",
-      deleted_at: null,
-      group_id: null,
     },
     {
-      id: 866,
-      name: "Ignacio",
-      description: "<p>dfsfsd</p>",
+      id: 2,
+      name: "Roman",
       image: "http://ongapi.alkemy.org/storage/rEZJhWbxCx.jpeg",
       order: 8989,
-      user_id: null,
-      created_at: "2022-01-27T04:58:27.000000Z",
-      updated_at: "2022-01-27T04:58:27.000000Z",
-      deleted_at: null,
-      group_id: null,
     },
     {
-      id: 868,
-      name: "Titulo de prueba",
-      description: "<p>fdes</p>",
+      id: 3,
+      name: "Titulo de prueba asdsad  ad asd asd ",
       image: "http://ongapi.alkemy.org/storage/tRMcq6w2JV.jpeg",
       order: 1,
-      user_id: null,
-      created_at: "2022-01-27T20:29:02.000000Z",
-      updated_at: "2022-01-27T20:29:02.000000Z",
-      deleted_at: null,
-      group_id: null,
     },
     {
-      id: 869,
+      id: 4,
       name: "myslide",
-      description: "<p>myslide</p>",
       image: "http://ongapi.alkemy.org/storage/ae5LYQeuId.png",
       order: 4555,
-      user_id: null,
-      created_at: "2022-01-28T16:23:08.000000Z",
-      updated_at: "2022-01-28T16:23:08.000000Z",
-      deleted_at: null,
-      group_id: null,
     },
   ],
 };
@@ -54,9 +40,48 @@ const slides = {
 const getSlidesData = new Promise((resolved) => resolved(slides));
 
 const SlideList = () => {
-  getSlidesData.then((res) => console.log(res.data));
+  const [slides, setSlides] = useState([]);
 
-  return <div>hola</div>;
+  getSlidesData.then((res) => {
+    setSlides(res.data);
+  });
+
+  const rows = slides;
+
+  return (
+    <div style={{ width: "90%", padding: "1em", margin: "0 auto" }}>
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Titulo</TableCell>
+              <TableCell align="center">Imagen</TableCell>
+              <TableCell align="center">Orden</TableCell>
+              <TableCell align="center">Acciones</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows &&
+              rows.map((row) => (
+                <TableRow key={row.id}>
+                  <TableCell align="center">{row.name}</TableCell>
+                  <TableCell align="center">
+                    <img src={row.image} alt="" style={{ width: "30%" }} />
+                  </TableCell>
+                  <TableCell align="center">{row.order}</TableCell>
+                  <TableCell>
+                    <button>Editar</button>
+                  </TableCell>
+                  <TableCell align="center">
+                    <button>Eliminar</button>
+                  </TableCell>
+                </TableRow>
+              ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
 };
 
 export default SlideList;
