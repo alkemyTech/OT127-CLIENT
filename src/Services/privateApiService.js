12e6afd21b6c68f985f19702276820bbb96d72c5
matias <solisma42@gmail.com>
2022-01-30 16:34:50 -0300

    fix: no habia guardado, ahora si git pull solucionado conflicto

--- src/Services/privateApiService.js
+++ src/Services/privateApiService.js
@@ -2,52 +2,10 @@ import axios from "axios"
 
 let config = {
   headers: {
-<<<<<<< HEAD
-    Group: 127, //Aqui va el ID del equipo!!
-=======
     Group: "127",
->>>>>>> 7fbe783815868890a8ae26a76a355c201124c7fc
   },
 }
 
-<<<<<<< HEAD
-const Get = () => {
-  axios
-    .get("https://jsonplaceholder.typicode.com/users", config)
-    .then((res) => console.log(res))
-    .catch((err) => console.log(err))
-}
-
-const getSecureHeader = () => {
-  const token = localStorage.getItem("token")
-  return token
-    ? { Authorization: "Bearer " + token }
-    : { error: "No token found" }
-}
-
-export const privateServiceDelete = (route, id) => {
-  if (!route) return (alert("Error: debe proporcionar una ruta"))
-  if ((!id && id !== 0) || id < 0) return (alert("Error: debe proporcionar un id valido"))
-
-  const url = `${route}/${id}`
-  let token = getSecureHeader()
-  const { Authorization, error } = token
-
-  if (Authorization) {
-    axios.delete(url, {
-      header: {
-        ...config.headers,
-        Authorization,
-      }
-    })
-
-  } else {
-    return error
-  }
-}
-
-export default Get 
-=======
 const getSecureHeader = () => {
   const token = localStorage.getItem("token");
   return token
@@ -78,4 +36,3 @@ const getPrivate = async (route, id = null) => {
 };
 
 export default getPrivate;
->>>>>>> 7fbe783815868890a8ae26a76a355c201124c7fc
