8a893ad2970a0e7cc4407b4fc2923ad3b5003ae3
matias <solisma42@gmail.com>
2022-01-30 16:10:32 -0300

    Agregado metodo POST para agregar nuevos slides y funcion para pasar a base 64 los links de las imgs

--- src/Services/slidesApiService.js
+++ src/Services/slidesApiService.js
@@ -1,5 +1,14 @@
 import axios from 'axios'
 
+const toDataURL = (blob) =>
+    // Para convertir los links de imagenes a base64
+    new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onloadend = () => resolve(reader.result);
+        reader.onerror = reject;
+        reader.readAsDataURL(blob);
+    });
+
 export const getSlidesData = async () => {
     try {
         const response = await axios.get('http://ongapi.alkemy.org/api/slides')
@@ -18,8 +27,18 @@ export const getSlidesDataById = async (id) => {
     }
 }
 
-export const createNewSlide = () => {
-
+export const createNewSlide = async (data) => {
+    try {
+        // Primero hay que convertir el link de la imagen a base64
+        let imgBlob = await axios.get(data.image, { responseType: "blob" })
+        let encoded = await toDataURL(imgBlob.data)
+        // Guardamos el link ya transformado a base 64 y hacemos la peticion 
+        data.image = encoded
+        const response = await axios.post('http://ongapi.alkemy.org/api/slides', data)
+        return response
+    } catch (error) {
+        return error
+    }
 }
 
 export const updateSlide = () => {
