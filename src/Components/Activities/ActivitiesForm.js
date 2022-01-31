75b874e1ca040371a7e5b8ffb8e68b51373c1d71
silversurferbeats <gabrielsuarez89@gmail.com>
2022-01-31 14:35:21 -0300

    inserto dangerouslySetInnerHTML

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -4,7 +4,8 @@ import '../FormStyles.css';
 const ActivitiesForm = () => {
     const [initialValues, setInitialValues] = useState({
         name: '',
-        description: ''
+        description: '',
+        campo: ''
     });
 
     const handleChange = (e) => {
@@ -12,7 +13,9 @@ const ActivitiesForm = () => {
             setInitialValues({...initialValues, name: e.target.value})
         } if(e.target.name === 'description'){
             setInitialValues({...initialValues, description: e.target.value})
-        }
+        } if (e.target.campo === 'campo')(
+            setInitialValues({...initialValues, campo: e.target.value})
+        )
     }
 
     const handleSubmit = (e) => {
@@ -21,18 +24,26 @@ const ActivitiesForm = () => {
     }
 
     //renderHTML
-    function enviarDatos(){
-        let texto = document.getElementById('campo').value;    
-        document.body.innerHTML = texto;
-    }
+    
+    const formattedMessage = `
+        <h1> hola mensage!</h1>
+        <p>esto es un parrafo Lorem.</p>
+    `;
 
     
     return (
         <form className="form-container" onSubmit={handleSubmit}>
             <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Activity Title"></input>
             <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some activity description"></input>
-            <textarea id="campo" type="text" name="campo" cols="40" rows="3" ><input/></textarea><br/>
-            <button className="submit-btn" type="submit" onClick={()=> enviarDatos()} >Render</button><br/>
+            
+            <textarea type="text" name="campo"  defaultValue={ initialValues.campo } onChange={handleChange} ></textarea>
+            <div 
+                style={{ border: '1px solid', padding: '0.5rem' }}
+                dangerouslySetInnerHTML={{ __html: formattedMessage }} 
+            />
+
+
+            <button className="submit-btn" type="submit"  >Submit</button><br/>
         </form>
     );
 }
