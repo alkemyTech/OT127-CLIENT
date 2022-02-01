4245f9f93c701c402504c84cabc14a1c3f2c55e8
danieldamian09 <humbertorivero9@gmail.com>
2022-02-01 09:38:05 -0300

    Corrections in typos and taking logic out of the GET

--- src/Services/categoriesService.js
+++ src/Services/categoriesService.js
@@ -2,30 +2,24 @@ import Axios from "axios";
 
 const url = `http://ongapi.alkemy.org/api/categories`;
 
-export const getCategory = async (formValues, setFormValues, id) => {
+export const getCategory = async (id) => {
 	try {
 		const {data} = await Axios.get(url + "/" + id);
-		const {name, description, image} = data.data;
-		setFormValues({
-			...formValues,
-			name: name,
-			description: description,
-			image: image,
-		});
+		return data;
 	} catch (error) {
 		return error;
 	}
 };
 
-export const puttCategory = (id, name, description, image) => {
+export const putCategory = (id, name, description, image) => {
 	try {
-		Axios.put(url + "/" + id, {id, name, description, image});
+		Axios.put(url + "/" + id, {name, description, image});
 	} catch (error) {
 		return error;
 	}
 };
 
-export const posCategory = (name, description, image) => {
+export const postCategory = (name, description, image) => {
 	try {
 		Axios.post(url, {name, description, image});
 	} catch (error) {
