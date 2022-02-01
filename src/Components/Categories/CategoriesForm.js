15e9b4cbcb3b825e45cdc64e5b16adbcdb01654a
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-02-01 16:21:28 -0300

    Agregado estilos a categoriesForm

--- src/Components/Categories/CategoriesForm.js
+++ src/Components/Categories/CategoriesForm.js
@@ -1,32 +1,50 @@
-import React, { useState } from 'react';
-import '../FormStyles.css';
+import React, { useState } from "react";
+import "../FormStyles.css";
 
 const CategoriesForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        description: ''
-    })
+  const [initialValues, setInitialValues] = useState({
+    name: "",
+    description: "",
+  });
 
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        }
+  const handleChange = (e) => {
+    if (e.target.name === "name") {
+      setInitialValues({ ...initialValues, name: e.target.value });
     }
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(initialValues);
+    if (e.target.name === "description") {
+      setInitialValues({ ...initialValues, description: e.target.value });
     }
+  };
 
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write some description"></input>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
-    );
-}
- 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div className="body">
+      <form className="body__form" onSubmit={handleSubmit}>
+        <input
+          className="body__form--input"
+          type="text"
+          name="name"
+          value={initialValues.name}
+          onChange={handleChange}
+          placeholder="Title"
+        ></input>
+        <input
+          className="body__form--input"
+          type="text"
+          name="description"
+          value={initialValues.description}
+          onChange={handleChange}
+          placeholder="Write some description"
+        ></input>
+        <button className="body__form--submit" type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
+
 export default CategoriesForm;
