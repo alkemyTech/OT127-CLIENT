33615c1fc74563255379a4ec75f7fca49918103d
chriscaracach <christiancaracach@gmail.com>
2022-02-02 14:17:22 -0300

    Feat: Created File Spinner.js

---
+++ src/Components/Spinner/Spinner.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Rings } from "react-loader-spinner";
+
+const Spinner = () => {
+  //Cuando sea implementado, es importante revisar el color y el tamaño por posibles retoques
+  return <Rings color="#000" height={80} width={80} />;
+};
+
+export default Spinner;
