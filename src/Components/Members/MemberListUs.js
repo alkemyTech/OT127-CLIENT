c225fe1343ea97e433073f9272c64d178ed0eeb1
Nel Rodriguez <nel.rodriguez22@gmail.com>
2022-02-02 18:17:36 +0000

    1strev

--- src/Components/Members/MemberListUs.js
+++ src/Components/Members/MemberListUs.js
@@ -1,16 +1,19 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState, useEffect } from "react"
+import axios from "axios"
+import "./styles.scss"
 
 
 const MemberListUs = () => {
 	const url = "http://ongapi.alkemy.org/api/members"
 	const [ members, setMembers ] = useState([])
+	const [ loading, setLoading ] = useState(true)
+
 
 	useEffect(() => {
 		axios.get(url)
 			.then(res => {
 				setMembers(res.data.data)
-				console.log(members)
+				setLoading(false)
 			})
 			.catch(err => {
 				alert(err)
@@ -19,9 +22,32 @@ const MemberListUs = () => {
 
 	return (
 		<>
-			<h1>memberlistusa</h1>
+			{(loading)
+				? <h1>Cargando...</h1>
+				: <div className="members">
+					<h1>Miembros</h1>
+					<div className="members__container">
+						{members.map(member => (
+							<li className="members__item" key={member.id}>
+								<div className="members__item__img">
+									<img src={member.image} alt={member.name} />
+								</div>
+								<div className="members__item__body">
+									<div>
+										<h2>{member.name}</h2>
+										<p>{member.description}</p>
+									</div>
+									<div>
+										<p>{member.facebookUrl}</p>
+										<p>{member.linkedinUrl}</p>
+									</div>
+								</div>
+							</li>
+						))}
+					</div>
+				</div>}
 		</>
 	)
 }
 
-export default MemberListUs;
+export default MemberListUs
