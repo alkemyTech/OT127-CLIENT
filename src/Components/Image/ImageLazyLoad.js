718a9009e096b1b07407c2828342bd47c781170a
chriscaracach <christiancaracach@gmail.com>
2022-02-03 10:52:15 -0300

    Feat: Comments

--- src/Components/Image/ImageLazyLoad.js
+++ src/Components/Image/ImageLazyLoad.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
+// El componente acepta los atributos comunes de una <img> (src, alt)
+// También acepta width y height (number, no string)
+// y placeholder (componente que se va a mostrar mientras se carga la imagen)
+
 const ImageLazyLoad = ({ src, alt, height, width, placeholder }) => {
   return (
     <>
