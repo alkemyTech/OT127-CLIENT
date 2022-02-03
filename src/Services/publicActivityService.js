9f72e262c2549d4d610ac696b73df57741a23325
chriscaracach <christiancaracach@gmail.com>
2022-02-03 11:11:16 -0300

    Feat: Existant referencies to /activities endpoint modified

--- src/Services/publicActivityService.js
+++ src/Services/publicActivityService.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 
 class ActivitiesService {
   constructor() {
-    this.API_URL = "http://ongapi.alkemy.org/api/activities";
+    this.API_URL = process.env.REACT_APP_ACTIVITIES_ENDPOINT;
   }
 
   getAll = async () => {
