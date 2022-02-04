a54b7ecf2cc853d72721354fb63d525104015ad4
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-02-04 17:05:44 -0300

    Prueba recibiendo productos en estado de redux

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import newsReducer from '../features/news/newsSlice'
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
-    news: newsReducer
+    news: newsReducer,
   },
 });
