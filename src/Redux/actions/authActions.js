2522ac789830fbc29249c8302c44172253c4d16b
chriscaracach <christiancaracach@gmail.com>
2022-02-04 07:00:14 -0300

    Feat: Created set_token action

--- src/Redux/actions/authActions.js
+++ src/Redux/actions/authActions.js
@@ -1,15 +1,6 @@
-//loginUser y RegisterUser deben recibir un objeto como parámetro, con éste formato
-// {
-//   name: string,
-//   email: string,
-//   role_id: string,
-//   remember_token: string,
-//   address: string,
-//   profile_image: string,
-// }
-
 import { createAction } from "@reduxjs/toolkit";
 
 export const loginUser = createAction("LOGIN_USER");
 export const registerUser = createAction("REGISTER_USER");
 export const logoutUser = createAction("LOGOUT_USER");
+export const setToken = createAction("SET_TOKEN");
