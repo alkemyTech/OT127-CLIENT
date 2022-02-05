26e112819e1ea577ca689b809c080afdbb4a6c23
danieldamian09 <humbertorivero9@gmail.com>
2022-02-05 19:59:33 -0300

    adding reducer categories to store

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import {authReducer} from "../reducers/authReducer";
 import newsReducer from "../reducers/newsSlice"
+import categoriesReducer from "../reducers/categoriesReducer";
 
 export default configureStore({
   reducer: {
     authReducer: authReducer,
-    newsReducer: newsReducer
+    newsReducer: newsReducer,
+    categoriesReducer: categoriesReducer,
   },
 });
