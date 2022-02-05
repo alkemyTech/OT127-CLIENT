af6537219385f02fd76aaae1b3c99394b8fb6d0a
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-02-05 11:18:42 -0300

    reducer en store

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {authReducer} from "../reducers/authReducer";
-import newsReducer from "../reducers/newsSlice"
+import { authReducer } from "../reducers/authReducer";
+import membersReducer from "../reducers/membersSlice";
+import newsReducer from "../reducers/newsSlice";
 
 export default configureStore({
   reducer: {
     authReducer: authReducer,
-    newsReducer: newsReducer
+    newsReducer: newsReducer,
+    membersReducer: membersReducer,
   },
 });
