031ffa78d0c4250e5a1ce829de356f23e965d8f4
matias <solisma42@gmail.com>
2022-02-05 13:37:07 -0300

    agregado inicialState

--- src/Redux/reducers/usersSlice.js
+++ src/Redux/reducers/usersSlice.js
@@ -5,3 +5,10 @@ export const getUsersData = createAsyncThunk("get/getUsers", async () => {
     const response = await Get("http://ongapi.alkemy.org/api/testimonials");
     return response.data.data;
 })
+
+const initialState = {
+    users: [],
+    status: 'idle', // el status nos va a servir para el user feedback, como mostrar un spinner de carga
+    error: null
+}
+
