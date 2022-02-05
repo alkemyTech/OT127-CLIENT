a26202bfa0a8cb7cbb895b7d33a31894263c7703
matias <solisma42@gmail.com>
2022-02-05 14:30:37 -0300

    agregado esqueleto postsSlice

--- src/Redux/reducers/usersSlice.js
+++ src/Redux/reducers/usersSlice.js
@@ -1,14 +1,30 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import { Get } from "../../Services/publicApiService";
 
-export const getUsersData = createAsyncThunk("get/getUsers", async () => {
+export const getUsers = createAsyncThunk("get/getUsers", async () => {
     const response = await Get("http://ongapi.alkemy.org/api/testimonials");
     return response.data.data;
 })
 
 const initialState = {
     users: [],
-    status: 'idle', // el status nos va a servir para el user feedback, como mostrar un spinner de carga
+    status: 'idle',
     error: null
 }
+// el "status" puede ser "idle", "loading", "succeded", "failed", nos sirve para el userfeedback
+const postsSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {},
+    extraReducers: {
+        [getUsers.pending.type]: (state, action) => {
 
+        },
+        [getUsers.fulfilled.type]: (state, action) => {
+
+        },
+        [getUsers.rejected.type]: (state, action) => {
+
+        },
+    },
+});
