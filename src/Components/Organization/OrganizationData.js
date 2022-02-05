a3167e6c726abcfe596052d503725da20f2cab2a
Nel Rodriguez <nel.rodriguez22@gmail.com>
2022-02-05 15:48:54 +0000

    fixurl

--- src/Components/Organization/OrganizationData.js
+++ src/Components/Organization/OrganizationData.js
@@ -3,36 +3,35 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const OrganizationData = () => {
-  const [organizationData, setOrganizationData] = useState({});
-	const url = process.env.REACT_APP_API
+	const [ organizationData, setOrganizationData ] = useState({});
 
-  const getOrgData = () => {
-    axios
-		 .get(`${url}/${process.env.REACT_APP_API_ORGANIZATION}`)
-      .then((response) => {
-        setOrganizationData(response.data.data);
-      })
-      .catch((error) => {
-        alert(error.message);
-      });
-  };
+	const getOrgData = () => {
+		axios
+			.get(`${process.env.REACT_APP_API}/${process.env.REACT_APP_API_ORGANIZATION}`)
+			.then((response) => {
+				setOrganizationData(response.data.data);
+			})
+			.catch((error) => {
+				alert(error.message);
+			});
+	};
 
-  useEffect(() => {
-    getOrgData();
-  }, []);
+	useEffect(() => {
+		getOrgData();
+	}, []);
 
-  return (
-    <>
-      {organizationData && (
-        <div>
-          <h1>{organizationData.name}</h1>
-          <img src={organizationData.logo} alt="organization_logo" />
-          <p>{organizationData.short_description}</p>
-          <Link to="/backoffice/organization/edit">Editar</Link>
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			{organizationData && (
+				<div>
+					<h1>{organizationData.name}</h1>
+					<img src={organizationData.logo} alt="organization_logo" />
+					<p>{organizationData.short_description}</p>
+					<Link to="/backoffice/organization/edit">Editar</Link>
+				</div>
+			)}
+		</>
+	);
 };
 
 export default OrganizationData;
