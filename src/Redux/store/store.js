96113c70f10b59c23a2fb4328544084134204b69
matias <solisma42@gmail.com>
2022-02-05 16:20:31 -0300

    agregado salto de linea para evitar posible conflicto

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "../reducers/authReducer";
 import newsReducer from "../reducers/newsSlice"
+
 import usersReducer from "../reducers/usersSlice"
 
 export default configureStore({
   reducer: {
     authReducer: authReducer,
     newsReducer: newsReducer,
-    usersReducer: usersReducer
+
+    usersReducer: usersReducer,
   },
 });
