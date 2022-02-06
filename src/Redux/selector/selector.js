149c0e7f86a49a40d951913723366a1b04d051e0
danieldamian09 <humbertorivero9@gmail.com>
2022-02-06 18:39:02 -0300

    creating the selectors for the flagas

--- src/Redux/selector/selector.js
+++
@@ -1,5 +0,0 @@
-export const isFetchingCategoriesSel = state => state.categoriesReducer.isFetchingCategories
-export const isEditingCategories = state => state.categoriesReducer.isEditingCategories
-export const isCreatingCategories = state => state.categoriesReducer.isCreatingCategories
-export const categoriesSel = state => state.categoriesReducer.categories
-export const errorCategoriesSel = state => state.categoriesReducer.error
