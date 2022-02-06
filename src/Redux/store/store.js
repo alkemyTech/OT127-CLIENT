ae848fc252065fdd194b6f337a0f26f7d32fa508
Cardoso, Leandro Nicolás <65054792+cardosonicolas@users.noreply.github.com>
2022-02-06 14:57:26 -0300

    Ot127-110/feat/create-slides-reducer (#90)
    * add slidesSlice file
    
    * create reducer
    
    * fix reducer
    
    * import reducer in store
    
    * refactor SlideList
    
    * refactor SliderHome
    
    * fix import order

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {authReducer} from "../reducers/authReducer";
-import newsReducer from "../reducers/newsSlice"
+import { authReducer } from "../reducers/authReducer";
+import newsReducer from "../reducers/newsSlice";
+import slidesReducer from "../reducers/slidesSlice";
 
 export default configureStore({
   reducer: {
     authReducer: authReducer,
-    newsReducer: newsReducer
+    newsReducer: newsReducer,
+    slidesReducer,
   },
 });
