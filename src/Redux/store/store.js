5a67382f7fcdf5f1cd5efaf2de2004d9821d9e77
Matias Solis <67497671+solismatias@users.noreply.github.com>
2022-02-07 10:50:50 -0300

    Ot127 111/feat/reducer usuarios (#87)
    * creado archivo userReducer
    
    * renombrado userReducer a usersSlice
    
    * añadidos imports necesarios y peticion de la api
    
    * agregado inicialState
    
    * agregado esqueleto postsSlice
    
    * fix: nombre del slice
    
    * terminado usersSlice
    
    * fix: arreglada la ruta de la api, traia los testimonios en vez de los usuarios
    
    * movida logica de la peticion de la api, a el thunk asincronico
    
    * agregado salto de linea para evitar posible conflicto
    
    Co-authored-by: matias <solisma42@gmail.com>

--- src/Redux/store/store.js
+++ src/Redux/store/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import { authReducer } from "../reducers/authReducer";
 import newsReducer from "../reducers/newsSlice";
 import slidesReducer from "../reducers/slidesSlice";
+import usersReducer from "../reducers/usersSlice"
 
 export default configureStore({
   reducer: {
     authReducer: authReducer,
     newsReducer: newsReducer,
     slidesReducer,
+    usersReducer: usersReducer,
   },
 });
