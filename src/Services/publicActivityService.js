29384d7811fdbef89858ce8bca295dbd7932bf2a
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-02-07 11:32:27 -0300

    alerta de errores en activityService post

--- src/Services/publicActivityService.js
+++ src/Services/publicActivityService.js
@@ -1,8 +1,9 @@
 const axios = require("axios");
+const { sweetAlertError } = require("./sweetAlertServices");
 
 class ActivitiesService {
   constructor() {
-    this.API_URL = "http://ongapi.alkemy.org/api/activities";
+    this.API_URL = "http://ongapi.alkemy.org/api/activites";
   }
 
   getAll = async () => {
@@ -10,7 +11,7 @@ class ActivitiesService {
     await axios
       .get(this.API_URL)
       .then((response) => (data = response.data.data))
-      .catch((error) => error.message);
+      .catch((error) => sweetAlertError(error.message));
     return data;
   };
 
@@ -20,7 +21,7 @@ class ActivitiesService {
       await axios
         .get(`${this.API_URL}/${id}`)
         .then((response) => (data = response.data.data))
-        .catch((error) => error.message);
+        .catch((error) => sweetAlertError(error.message));
       return data;
     }
   };
@@ -33,7 +34,7 @@ class ActivitiesService {
         image,
       })
       .then((response) => response.data)
-      .catch((error) => error.message);
+      .catch((error) => sweetAlertError(error.message));
   };
 
   put = async (id, name, description, image) => {
@@ -45,7 +46,7 @@ class ActivitiesService {
           image,
         })
         .then((response) => response.data)
-        .catch((error) => error.message);
+        .catch((error) => sweetAlertError(error.message));
     }
   };
 
