137639422eee9229cf46c11456c4ee2a3f961fea
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-02-07 18:37:47 -0300

    Agregado componenteNewsList a componente padre News

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -1,35 +1,12 @@
-import React, {useEffect} from "react";
-import { useSelector, useDispatch } from 'react-redux'
-import {getNews} from '../../Redux/reducers/newsSlice'
+import React from "react";
+
+import NewsList from "./NewsList";
 
 const News = () => {
-  const dispatch = useDispatch()
-  
-  useEffect(() => {
-    dispatch(getNews())
-  }, []); //eslint-disable-line
-
-  const news = useSelector(state => state.newsReducer.news.data)
-  
-  const newsList = () => {
-    return news.length ? (
-      news.map((element) => (
-        <li className="card-info" key={element.id}>
-          <h3>{element.name}</h3>
-          <p>{element.description}</p>
-        </li>
-      ))
-    ) : (
-      <div>
-      <p>No hay novedades</p>
-      </div>
-    );
-  };
-
   return (
     <>
       <h1>Novedades</h1>
-      <ul className="list-container">{newsList()}</ul>
+      <NewsList/>
     </>
   );
 };
