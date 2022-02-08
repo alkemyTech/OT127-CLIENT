0d3638fe032bb10c34ae785c4460621fbd0744e3
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-02-07 22:52:25 -0300

    Agregado comentarios y skeleton a componente newsDetails

--- src/Components/News/Comments.jsx
+++ src/Components/News/Comments.jsx
@@ -1,31 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import Skeleton from '../../features/skeleton/Skeleton';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Skeleton from "../../features/skeleton/Skeleton";
 
 const Commentaries = () => {
-    const [comments, setComments] = useState([])
-    useEffect(() => {
-      axios.get('http://ongapi.alkemy.org/api/comments')
-      .then(res => {
-        setTimeout(function(){
-          setComments(res.data.data)
-      }, 3000);
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://ongapi.alkemy.org/api/comments")
+      .then((res) => {
+        setTimeout(function () {
+          setComments(res.data.data);
+        }, 3000); //Seteado 3 segundos para poder ver el skeleton
       })
       .catch((error) => {
-          return error
-      })
-    }, []);
-    
-  return <div>
-      {comments.length?
-        comments.map(comment => {return (
-        <p key={comment.id}>{comment.text}</p>
-      )
-      })
-      :
-      <Skeleton mode='list'/>
-      }
-  </div>;
-}
+        return error;
+      });
+  }, []);
+
+  return (
+    <div>
+    <h1>Comentarios</h1>
+      {comments.length ? (
+        comments.map((comment) => {
+          return <p key={comment.id}>{comment.text}</p>;
+        })
+      ) : (
+        <Skeleton mode="list" />
+      )}
+    </div>
+  );
+};
 
 export default Commentaries;
