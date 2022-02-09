eeb4cf7f900db7795a130b1b950fb253c5748757
David Zapata <55600188+davidz1995@users.noreply.github.com>
2022-02-09 11:14:30 -0300

    Ot127 100/feat/cargar comentarios novedades (#102)
    * Organizado app temporalmente
    
    * Agregado componenteNewsList a componente padre News
    
    * Agregado listener scroll a componente News
    
    * Agregado componente Comments y skeleton mientras carga peticion
    
    * Agregado comentarios y skeleton a componente newsDetails
    
    * Seteado app a original de esta version
    
    * Eliminado useEffect
    
    * Cambio de nombre a comments
    
    * Ejecuta get en useEffect
    
    * Fix/ cambio de componente newsList de backoffice a lista de novedades de cliente
    
    * Fix, cambio de renderizado, element.description por element.content

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -1,17 +1,41 @@
-import React, { useState } from "react";
-import NewsList from "./NewsList";
-import Comments from "./Comments";
+import React, {useEffect, useState} from "react";
+import { useSelector, useDispatch } from 'react-redux'
+import {getNews} from '../../Redux/reducers/newsSlice'
 import { useBottomScrollListener } from "react-bottom-scroll-listener";
+import Comments from "./Comments";
 
 const News = () => {
+  const dispatch = useDispatch()
   const [showComments, setShowComments] = useState(false);
   useBottomScrollListener(() => setShowComments(true));
+  
+  useEffect(() => {
+    dispatch(getNews())
+  }, []); //eslint-disable-line
+
+  const news = useSelector(state => state.newsReducer.news.data)
+  
+  const newsList = () => {
+    return news.length ? (
+      news.map((element) => (
+        <li className="card-info" key={element.id}>
+          <h3>{element.name}</h3>
+          <p>{element.content}</p>
+        </li>
+      ))
+    ) : (
+      <div>
+      <p>No hay novedades</p>
+      </div>
+    );
+  };
+
   return (
-    <div>
+    <>
       <h1>Novedades</h1>
-      <NewsList />
+      <ul className="list-container">{newsList()}</ul>
       {showComments && <Comments />}
-    </div>
+    </>
   );
 };
 
