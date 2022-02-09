e57b408caa78cac971b78815472f941579153b7c
chriscaracach <christiancaracach@gmail.com>
2022-02-09 19:33:34 -0300

    Feat: Created file, installed library react-burger-menu

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,15 +1,17 @@
 import React, { useState } from "react";
 import SliderHome from "../../Components/Slides/SliderHome";
 import News from "../../Components/News/News";
+import BackofficeHeader from "../../Components/Backoffice/BackofficeHeader";
 
 const Home = () => {
   const [welcomeText, setWelcomeText] = useState("Bienvenido");
 
   return (
     <>
-      <h2>{welcomeText}</h2>
+      <BackofficeHeader></BackofficeHeader>
+      {/* <h2>{welcomeText}</h2>
       <SliderHome />
-      <News />
+      <News /> */}
     </>
   );
 };
