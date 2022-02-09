bb5825e41355e8d3a701a7c7533b8f151b3ac649
chriscaracach <christiancaracach@gmail.com>
2022-02-09 20:47:36 -0300

    Feat: Add styles /Fix: deleted buttons to show sidebar (unnecesary)

--- src/Components/Backoffice/BackofficeHeader.js
+++ src/Components/Backoffice/BackofficeHeader.js
@@ -1,25 +1,60 @@
 import React, { useState } from "react";
 import { bubble as Menu } from "react-burger-menu";
-import ViewListIcon from "@material-ui/icons/ViewList";
+
+//TODO Desinstalar MUI ICONS, no fue necesario
+//TODO Tengo que llevar todo éste CSS al Sass
+const styles = {
+  bmBurgerButton: {
+    position: "fixed",
+    width: "36px",
+    height: "30px",
+    left: "36px",
+    top: "36px",
+  },
+  bmBurgerBars: {
+    background: "#373a47",
+  },
+  bmBurgerBarsHover: {
+    background: "#a90000",
+  },
+  bmCrossButton: {
+    height: "24px",
+    width: "24px",
+  },
+  bmCross: {
+    background: "#bdc3c7",
+  },
+  bmMenuWrap: {
+    position: "fixed",
+    height: "100%",
+  },
+  bmMenu: {
+    background: "#373a47",
+    padding: "2.5em 1.5em 0",
+    fontSize: "1.15em",
+  },
+  bmMorphShape: {
+    fill: "#373a47",
+  },
+  bmItemList: {
+    color: "#b8b7ad",
+    padding: "0.8em",
+  },
+  bmItem: {
+    display: "block",
+  },
+  bmOverlay: {
+    background: "rgba(0, 0, 0, 0.3)",
+  },
+};
 
 const BackofficeHeader = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const showHideMenu = (event) => {
-    event.preventDefault();
-    setIsMenuOpen(!isMenuOpen);
-  };
   return (
     <>
-      <Menu isOpen={isMenuOpen} customBurgerIcon={false}>
+      <Menu isOpen={false} styles={styles}>
         <h1>ONG - Somos más</h1>
-        <a href="asd">Link 1</a>
-        <a href="asd">Link 2</a>
-        <a href="asd">Link 3</a>
-        <button onClick={showHideMenu}>Cerrar</button>
+        {/*LINKS DE REACT ROUTER ??*/}
       </Menu>
-      <button onClick={showHideMenu}>
-        <ViewListIcon />
-      </button>
     </>
   );
 };
