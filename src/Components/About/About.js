eb242b53a3de8fb98e2d882fe21a4d67d2399385
Nel Rodriguez <nel.rodriguez22@gmail.com>
2022-02-09 23:58:53 +0000

    1stRel

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Title from "../Titulosynovedades/Title";
+import { useDispatch, useSelector } from 'react-redux';
+import { getUs } from "../../Redux/reducers/usSlice";
+
 
 const About = ({
-  text = "Desde 1997 en Somos Más trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio La Cava ",
+	text = "Desde 1997 en Somos Más trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio La Cava ",
 }) => {
-  // El texto en un futuro se obtendra desde una API
-  return (
-    <>
-      <Title title="Nosotros" />
-      <main>
-        <p>{text}</p>
-      </main>
-    </>
-  );
+	const dispatch = useDispatch()
+	const info = useSelector(state => state.usReducer.info)
+
+	useEffect(() => {
+		dispatch(getUs())
+	}, []);
+	// El texto en un futuro se obtendra desde una API
+	return (
+		<>
+			<Title title="Nosotros" />
+			<main>
+				<p>{info.name}</p>
+				<p>{info.short_description}</p>
+			</main>
+		</>
+	);
 };
 
 export default About;
