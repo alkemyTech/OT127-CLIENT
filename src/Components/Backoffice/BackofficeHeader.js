8c2221721f0aba78ee0277da1e8ac5adabce99fc
chriscaracach <christiancaracach@gmail.com>
2022-02-09 19:57:24 -0300

    Feat: Main structure of component BackofficeHeader

--- src/Components/Backoffice/BackofficeHeader.js
+++ src/Components/Backoffice/BackofficeHeader.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { bubble as Menu } from "react-burger-menu";
 
 const BackofficeHeader = () => {
-  return <div>Header</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const showHideMenu = (event) => {
+    event.preventDefault();
+    setIsMenuOpen(!isMenuOpen);
+  };
+  return (
+    <>
+      <Menu isOpen={isMenuOpen} customBurgerIcon={false}>
+        <a href="asd">Link 1</a>
+        <a href="asd">Link 2</a>
+        <a href="asd">Link 3</a>
+        <button onClick={showHideMenu}>Cerrar</button>
+      </Menu>
+      <button onClick={showHideMenu}>Menu</button>
+    </>
+  );
 };
 
 export default BackofficeHeader;
