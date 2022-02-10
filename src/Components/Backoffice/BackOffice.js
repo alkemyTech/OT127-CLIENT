5c9eac0e918f5651d5d160b874bc8c67c83d2ee0
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-02-09 22:12:27 -0300

    Agregado accion para mostrar y ocultar menu lateral

--- src/Components/Backoffice/BackOffice.js
+++ src/Components/Backoffice/BackOffice.js
@@ -1,6 +1,5 @@
-import React, {useState} from "react";
-import Button from 'react-bootstrap/Button';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import React, { useState } from "react";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 const BackOffice = () => {
   const [show, setShow] = useState(false);
@@ -9,24 +8,20 @@ const BackOffice = () => {
   const handleShow = () => setShow(true);
 
   return (
-    <>
+    <div>
+      <div className="offcanvasButton" onMouseOver={handleShow}></div>
       <h1>Bienvenido!</h1>
-      <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch
-      </Button>
 
       <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+          <Offcanvas.Title>Backoffice Routes</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body>
           Some text as placeholder. In real life you can have the elements you
           have chosen. Like, text, images, lists, etc.
         </Offcanvas.Body>
       </Offcanvas>
-    </>
-    </>
+    </div>
   );
 };
 
