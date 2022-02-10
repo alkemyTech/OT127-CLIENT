84bbb3832cf0f8806519c9845b514965137312e3
silversurferbeats <gabrielsuarez89@gmail.com>
2022-02-10 17:11:44 -0300

    creacion reducer actividades

--- src/Redux/reducers/activitySlice.js
+++ src/Redux/reducers/activitySlice.js
@@ -1,37 +1,36 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";  
 import { Get } from "../../Services/publicApiService";
 
-export const getActivity = createAsyncThunk("get/getActivity", async () => {
-  const response = await Get("http://ongapi.alkemy.org/api//activities", null);
+export const getActivities = createAsyncThunk("get/getActivities", async () => {
+  const response = await Get("http://ongapi.alkemy.org/api/activities", null);
   return response.data.data;
 });
 
-export const activitySlice = createSlice({
-  name: "activity",
-  initialState: {
-    activity = {
-        id: "",
-        name: "",
-        description: "", 
-    }
-  },
-  reducers: {},
-  extraReducers: {
-    [getActivity.fulfilled.type]: (state, action) => {
-        state.activity = {
-            id: 1, 
-            name: 'Titulo de prueba', 
-            description: 'Descripcion de prueba'
-        }
+export const activitiesSlice = createSlice({ 
+  name: "activities", 
+  initialState: { 
+    activities: {
+      status: "idle",
+      data: {}, 
+      error: {}, 
     },
-    [getActivity.rejected.type]: (state, action) => {
-        state.activity = {
-            id: 1, 
-            name: 'Titulo de prueba', 
-            description: 'Descripcion de prueba'
-        }
+  },
+  reducers: {}, 
+  extraReducers: { 
+    [getActivities.fulfilled.type]: (state, action) => { 
+      state.activities = { 
+        status: "idle",
+        data: action.payload,
+        error: {},
+      };
+    },
+    [getActivities.rejected.type]: (state, action) => {
+      state.activities = {
+        status: "idle",
+        data: {},
+        error: action.payload,
+      };
     },
   },
 });
-
-export default activitySlice.reducer;
+export default activitiesSlice.reducer; 
