41facd0c245ad9488ca434447d749a9fc1fe59c1
cardosonicolas <cardoso.leandro.nicolas@gmail.com>
2022-02-10 20:34:27 -0300

    add state and useEffect, set state

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Get } from "../../Services/publicApiService";
 
 const API_URL = process.env.REACT_APP_API_URL;
 const ORGANIZATION_ENDPOINT = process.env.REACT_APP_API_ORGANIZATION;
 
 const Footer = () => {
+  const [organization, setOrganization] = useState({});
+
+  useEffect(() => {
+    Get(API_URL + ORGANIZATION_ENDPOINT).then((res) =>
+      setOrganization(res.data.data)
+    );
+  }, []);
+
   return <></>;
 };
 
