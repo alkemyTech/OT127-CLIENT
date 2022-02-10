b27352fff387f88b18d36d16d5c95c21d05aac2f
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-02-09 21:48:24 -0300

    Agregado offcanvas a backoffice

--- src/Components/Backoffice/BackOffice.js
+++ src/Components/Backoffice/BackOffice.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 const BackOffice = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <>
       <h1>Bienvenido!</h1>
+      <>
+      <Button variant="primary" onClick={handleShow}>
+        Launch
+      </Button>
+
+      <Offcanvas show={show} onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          Some text as placeholder. In real life you can have the elements you
+          have chosen. Like, text, images, lists, etc.
+        </Offcanvas.Body>
+      </Offcanvas>
+    </>
     </>
   );
 };
