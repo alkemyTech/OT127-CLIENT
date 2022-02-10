14d74f3d3353eb860b457b2a2bb3727dac6b1537
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-02-09 22:58:42 -0300

    Instaladas librerias y creado componente de leaftlet

---
+++ src/features/leaflet/Leaflet.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+
+const Leaflet = () => {
+  return (
+    <div>Leaflet</div>
+  )
+}
+
+export default Leaflet
