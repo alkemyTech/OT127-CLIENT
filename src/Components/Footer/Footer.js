bf07b54b48cd28b39d6249da66c257b9749a9865
cardosonicolas <cardoso.leandro.nicolas@gmail.com>
2022-02-10 20:32:13 -0300

    add environment variables, import get

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Get } from "../../Services/publicApiService";
+
+const API_URL = process.env.REACT_APP_API_URL;
+const ORGANIZATION_ENDPOINT = process.env.REACT_APP_API_ORGANIZATION;
 
 const Footer = () => {
   return <></>;
