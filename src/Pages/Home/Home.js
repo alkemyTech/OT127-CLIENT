9c8f06a8f674baca6b7ca85a3de3c50baad32c6b
chriscaracach <christiancaracach@gmail.com>
2022-02-10 19:22:20 -0300

    Fix: Deleted older imports and component

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,14 +1,12 @@
 import React, { useState } from "react";
 import SliderHome from "../../Components/Slides/SliderHome";
 import News from "../../Components/News/News";
-import BackofficeHeader from "../../Components/Backoffice/BackofficeHeader";
 
 const Home = () => {
   const [welcomeText, setWelcomeText] = useState("Bienvenido");
 
   return (
     <>
-      <BackofficeHeader></BackofficeHeader>
       <h2>{welcomeText}</h2>
       <SliderHome />
       <News />
