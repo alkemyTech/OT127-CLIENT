2e9ef6ed65b69e83653c0dab38970c0a8d26700c
chriscaracach <christiancaracach@gmail.com>
2022-02-10 09:06:56 -0300

    Feat: Deleted css in file

--- src/Components/Backoffice/BackofficeHeader.js
+++ src/Components/Backoffice/BackofficeHeader.js
@@ -1,23 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import { bubble as Menu } from "react-burger-menu";
 
 //TODO Desinstalar MUI ICONS, no fue necesario
-//TODO Tengo que llevar todo éste CSS al Sass
 
 const BackofficeHeader = () => {
   return (
-    <Menu
-      isOpen={false}
-      className="sidebar"
-      burguerButtonClassName={"sidebar__burgerButton"}
-      burgerBarClassName={"sidebar__bars"}
-      crossButtonClassName={"sidebar__crossButton"}
-      crossClassName={"sidebar__cross"}
-      menuClassName={"sidebar__menu"}
-      morphShapeClassName={"sidebar__morphShape"}
-      itemListClassName={"sidebar__itemList"}
-      overlayClassName={"sidebar__overlay"}
-    >
+    <Menu isOpen={false} className={"sidebar"}>
       <h1>ONG - Somos más</h1>
       {/*LINKS DE REACT ROUTER ??*/}
     </Menu>
