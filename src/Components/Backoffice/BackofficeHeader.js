590f6f968627f6a1dffdea42ff014c61b8973aec
chriscaracach <christiancaracach@gmail.com>
2022-02-10 17:44:35 -0300

    Feat: main structure of component

--- src/Components/Backoffice/BackofficeHeader.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const BackofficeHeader = () => {
-  return <></>;
-};
-
-export default BackofficeHeader;
---
+++ src/Components/Backoffice/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <button>Menu</button>
+      <h1>Somos más</h1>
+    </div>
+  );
+};
+
+export default Header;
