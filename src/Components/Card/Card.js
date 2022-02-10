b42688af03c2c433fbda7298ecc09761620a16af
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-02-10 13:03:37 -0300

    container provisorio eliminado

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -6,13 +6,11 @@ const Card = ({
   description,
 }) => {
   return (
-    <div className="container">
-      <div className="card__wrapper">
-        <img className="card__image" src={imgSrc} alt={title} />
-        <div className="card__content">
-          <h3 className="card__title">{title}</h3>
-          <p className="card__text">{description}</p>
-        </div>
+    <div className="card__wrapper">
+      <img className="card__image" src={imgSrc} alt={title} />
+      <div className="card__content">
+        <h3 className="card__title">{title}</h3>
+        <p className="card__text">{description}</p>
       </div>
     </div>
   );
