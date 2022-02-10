60c50e0736d03db8999e8f1f559d169c6412e618
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-02-09 22:28:08 -0300

    Agregado rutas al menu

--- src/Components/Backoffice/BackOffice.js
+++ src/Components/Backoffice/BackOffice.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import { Link } from "react-router-dom";
 
 const BackOffice = () => {
   const [show, setShow] = useState(false);
@@ -7,18 +8,32 @@ const BackOffice = () => {
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
+  const backofficeRoutes = [
+    { name: "Home", router: "/backoffice/home" },
+    { name: "Backoffice", router: "/backoffice" },
+    { name: "Users", router: "/backoffice/users" },
+    { name: "Slides", router: "/backoffice/slides" },
+    { name: "Members", router: "/backoffice/members" },
+    { name: "News", router: "/backoffice/news" },
+    { name: "Organization", router: "/backoffice/organization" },
+    { name: "Create Project", router: "/backoffice/create-project" },
+    { name: "Create Slides", router: "/backoffice/create-slide" },
+    { name: "Create User", router: "/backoffice/create-user" },
+  ];
+
   return (
     <div>
-      <div className="offcanvasButton" onMouseOver={handleShow}></div>
+      <div className="offcanvas__controller" onMouseOver={handleShow}></div>
       <h1>Bienvenido!</h1>
 
       <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Backoffice Routes</Offcanvas.Title>
         </Offcanvas.Header>
-        <Offcanvas.Body>
-          Some text as placeholder. In real life you can have the elements you
-          have chosen. Like, text, images, lists, etc.
+        <Offcanvas.Body className="offcanvas__body">
+          {backofficeRoutes.map((element) => {
+            return <Link to={element.router}>{element.name}</Link>;
+          })}
         </Offcanvas.Body>
       </Offcanvas>
     </div>
