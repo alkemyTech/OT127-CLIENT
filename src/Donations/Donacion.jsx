0946a66880b60508a608361de395bcf70e17784a
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-02-10 13:01:49 -0300

    boton Mercado Pago creado

--- src/Donations/Donacion.jsx
+++ src/Donations/Donacion.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const Donacion = ({ message }) => {
+  const mercadoPagoCheckout = "https://mpago.la/1NKGewb";
+
   return (
     <div>
       {message && <p>{message}</p>}
-      <button>MercadoPago</button>
+      <a className="button-lg" href={mercadoPagoCheckout}>
+        Mercado Pago
+      </a>
     </div>
   );
 };
