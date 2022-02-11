d80f34fefd20b2110294fe7c01250bc6ad56388d
cardosonicolas <cardoso.leandro.nicolas@gmail.com>
2022-02-11 16:00:04 -0300

    fix main code

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -1,68 +1,47 @@
-import React, {useEffect, useState} from "react";
-import { useSelector, useDispatch } from 'react-redux'
-import {getNews} from '../../Redux/reducers/newsSlice'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getNews } from "../../Redux/reducers/newsSlice";
 import { useBottomScrollListener } from "react-bottom-scroll-listener";
+import Spinner from "../Spinner/Spinner";
 import Comments from "./Comments";
 
 const News = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [showComments, setShowComments] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   useBottomScrollListener(() => setShowComments(true));
-  
+
   useEffect(() => {
-    dispatch(getNews())
+    dispatch(getNews());
+    setIsLoading(false);
   }, []); //eslint-disable-line
 
-  const news = useSelector(state => state.newsReducer.news.data)
-  
+  const news = useSelector((state) => state.newsReducer.news.data);
+
   const newsList = () => {
-    return news.length ? (
-      news.map((element) => (
-        <li className="card-info" key={element.id}>
-          <h3>{element.name}</h3>
-          <p>{element.content}</p>
-        </li>
-      ))
-    ) : (
-      <div>
-      <p>No hay novedades</p>
-      </div>
-    );
+    return news.length
+      ? news.map((element) => (
+          <li className="card-info" key={element.id}>
+            <h3>{element.name}</h3>
+            <p>{element.description}</p>
+          </li>
+        ))
+      : null;
   };
 
   return (
     <>
-      <h1>Novedades</h1>
-      <ul className="list-container">{newsList()}</ul>
-      {showComments && <Comments />}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div>
+          <h1>Novedades</h1>
+          <ul className="list-container">{newsList()}</ul>
+          {showComments && <Comments />}
+        </div>
+      )}
     </>
   );
 };
 
-	const newsList = () => {
-		return news.length ? (
-			news.map((element) => (
-				<li className="card-info" key={element.id}>
-					<h3>{element.name}</h3>
-					<p>{element.description}</p>
-				</li>
-			))
-		) : (
-			null
-		);
-	};
-
-	return (
-		<>
-			{isLoading
-				? <Spinner />
-				: (<div>
-					<h1>Novedades</h1>
-					<ul className="list-container">{newsList()}</ul>
-				</div>
-				)}
-		</>
-	)
-}
-
-export default News
+export default News;
