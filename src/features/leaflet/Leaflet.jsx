b73826169c49e0dc27758a120648219e7099ea17
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-02-11 09:13:58 -0300

    Agregado texto de error

--- src/features/leaflet/Leaflet.jsx
+++ src/features/leaflet/Leaflet.jsx
@@ -1,30 +1,51 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import L from 'leaflet';
-import logo from '../../assets/images/logo.jpg'
+import L from "leaflet";
+import logo from "../../assets/images/logo.jpg";
+import axios from "axios";
 
 const Leaflet = () => {
+  const [orgData, setOrgData] = useState({});
   const position = [4.6685073, -74.0622645];
-
   const myIcon = new L.Icon({
     iconUrl: logo,
     iconRetinaUrl: logo,
-    popupAnchor:  [-0, -0],
-    iconSize: [32,45],
-});
-  
+    popupAnchor: [-0, -0],
+    iconSize: [32, 45],
+  });
+
+  const getOrganizationData = () => {
+    let organizationApi = process.env.REACT_APP_ENDPOINT_ORGANIZATION;
+    axios
+      .get(`${organizationApi}`)
+      .then((response) => setOrgData(response.data))
+      .catch((error) => error);
+  };
+
+  useEffect(() => {
+    getOrganizationData();
+  }, []);
+
   return (
     <div id="map">
       <MapContainer
         center={position}
         zoom={20}
-        style={{ height: "50vh", width:"50%" }}
+        style={{ height: "50vh", width: "50%" }}
       >
-        <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         <Marker position={position} icon={myIcon}>
-          <Popup><strong>Somos Mas</strong></Popup>
+          <Popup>
+            {orgData.data ? (
+              <>
+                <strong>Somos Mas</strong>
+                <br />
+                {orgData.data.address}
+              </>
+            ) : (
+              <>No se encontró la ubicación.</>
+            )}
+          </Popup>
         </Marker>
       </MapContainer>
     </div>
