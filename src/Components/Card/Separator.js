c8f6f0a1a707f36765952966802b95114ee532df
matias <solisma42@gmail.com>
2022-02-11 14:47:35 -0300

    creado archivos iniciales para un componente reutilizable de separador

---
+++ src/Components/Card/Separator.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../../sass/components/_separator.scss"
+
+const Separator = (props) => {
+    return (
+        <div>
+            {props.children}
+        </div>
+    )
+}
+
+export default Separator
