3c14afd89620c03d22abe8562f0a863cff3e9f17
chriscaracach <christiancaracach@gmail.com>
2022-02-11 18:22:35 -0300

    Fix: fixed problems coming from main

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -1,20 +1,20 @@
-import React, {useEffect, useState} from "react";
-import { useSelector, useDispatch } from 'react-redux'
-import {getNews} from '../../Redux/reducers/newsSlice'
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { getNews } from "../../Redux/reducers/newsSlice";
 import { useBottomScrollListener } from "react-bottom-scroll-listener";
 import Comments from "./Comments";
 
 const News = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [showComments, setShowComments] = useState(false);
   useBottomScrollListener(() => setShowComments(true));
-  
+
   useEffect(() => {
-    dispatch(getNews())
+    dispatch(getNews());
   }, []); //eslint-disable-line
 
-  const news = useSelector(state => state.newsReducer.news.data)
-  
+  const news = useSelector((state) => state.newsReducer.news.data);
+
   const newsList = () => {
     return news.length ? (
       news.map((element) => (
@@ -25,7 +25,7 @@ const News = () => {
       ))
     ) : (
       <div>
-      <p>No hay novedades</p>
+        <p>No hay novedades</p>
       </div>
     );
   };
@@ -39,30 +39,30 @@ const News = () => {
   );
 };
 
-	const newsList = () => {
-		return news.length ? (
-			news.map((element) => (
-				<li className="card-info" key={element.id}>
-					<h3>{element.name}</h3>
-					<p>{element.description}</p>
-				</li>
-			))
-		) : (
-			null
-		);
-	};
+// 	const newsList = () => {
+// 		return news.length ? (
+// 			news.map((element) => (
+// 				<li className="card-info" key={element.id}>
+// 					<h3>{element.name}</h3>
+// 					<p>{element.description}</p>
+// 				</li>
+// 			))
+// 		) : (
+// 			null
+// 		);
+// 	};
 
-	return (
-		<>
-			{isLoading
-				? <Spinner />
-				: (<div>
-					<h1>Novedades</h1>
-					<ul className="list-container">{newsList()}</ul>
-				</div>
-				)}
-		</>
-	)
-}
+// 	return (
+// 		<>
+// 			{isLoading
+// 				? <Spinner />
+// 				: (<div>
+// 					<h1>Novedades</h1>
+// 					<ul className="list-container">{newsList()}</ul>
+// 				</div>
+// 				)}
+// 		</>
+// 	)
+// }
 
-export default News
+export default News;
