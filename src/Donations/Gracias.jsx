6960f9dfa43aa81b120a68337515cac385562b91
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-02-12 18:12:22 -0300

    contenido gracias modificado

--- src/Donations/Gracias.jsx
+++ src/Donations/Gracias.jsx
@@ -2,8 +2,17 @@ import React from "react";
 
 const Gracias = () => {
   return (
-    <div>
-      <p>Texto de agradecimiento.</p>
+    <div className="tanks__container">
+      <h1 className="thanks_Xtitle">Muchas Gracias por tu Ayuda!!</h1>
+      <div className="thanks__content">
+        <img src={process.env.PUBLIC_URL + "/images/happy-kid.png"} alt="kid" />
+        <p className="thanks__text">
+          Mediante nuestros programas educativos, buscamos incrementar la
+          capacidad intelectual, moral y afectiva de las personas de acuerdo con
+          la cultura y las normas de convivencia de la sociedad a la que
+          pertenecen.
+        </p>
+      </div>
     </div>
   );
 };
