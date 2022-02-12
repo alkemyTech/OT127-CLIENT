479f4069ba33f7492df84af559266351846f0e1f
chriscaracach <christiancaracach@gmail.com>
2022-02-12 15:10:43 -0300

    Feat: Backoffice routes main structures

--- src/Components/Backoffice/BackOffice.js
+++ src/Components/Backoffice/BackOffice.js
@@ -1,6 +1,22 @@
 import React, { useState } from "react";
 import Offcanvas from "react-bootstrap/Offcanvas";
-import { Link } from "react-router-dom";
+import { Link, Switch, Route } from "react-router-dom";
+import SlideList from "../../Pages/Slides/SlideList";
+import ActivitiesForm from "../Activities/ActivitiesForm";
+import ActivitiesList from "../Activities/ActivitiesList";
+import CategoriesForm from "../Categories/CategoriesForm";
+import CategoriesList from "../Categories/CategoriesList";
+import MembersForm from "../Members/MembersForm";
+import MembersList from "../Members/MembersList";
+import SlidesForm from "../Slides/SlidesForm";
+import HomeForm from "./HomeForm";
+import UsersList from "../Users/UsersList";
+import UserForm from "../Users/UsersForm";
+import NewsForm from "../News/NewsForm";
+import NewsList from "../News/NewsList";
+import OrganizationData from "../Organization/OrganizationData";
+import OrganizationDataForm from "../Organization/EditOrganizationDataForm";
+import ProjectsForm from "../Projects/ProjectsForm";
 
 const BackOffice = () => {
   const [show, setShow] = useState(false);
@@ -26,6 +42,7 @@ const BackOffice = () => {
       <div className="offcanvas__controller" onMouseOver={handleShow}></div>
       <h1>Bienvenido!</h1>
 
+      {/* TODO MODULARIZAR ESTO */}
       <Offcanvas show={show} onHide={handleClose}>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Backoffice Routes</Offcanvas.Title>
@@ -36,6 +53,41 @@ const BackOffice = () => {
           })}
         </Offcanvas.Body>
       </Offcanvas>
+
+      <Switch>
+        <Route path="/backoffice/home" component={HomeForm} />
+        <Route path="/backoffice/create-activity" component={ActivitiesForm} />
+        <Route
+          path="/backoffice/create-activity/:id"
+          component={ActivitiesForm}
+        />
+        <Route path="/backoffice/activities" component={ActivitiesList} />
+        <Route path="/backoffice/categories" component={CategoriesList} />
+        <Route
+          path="/backoffice/categories/create"
+          component={CategoriesForm}
+        />
+        <Route path="/backoffice/categories" component={CategoriesList} />
+        <Route path="/backoffice/members" component={MembersList} />
+        <Route path="/backoffice/members/create" component={MembersForm} />
+        <Route path="/backoffice/members/edit/:id" component={MembersForm} />
+        <Route path="/backoffice/slides" component={SlideList} />
+        <Route path="/backoffice/slides/create" component={SlidesForm} />
+        <Route path="/backoffice/slides/edit/:id" component={SlidesForm} />
+        <Route path="/backoffice/users" component={UsersList} />
+        <Route path="/backoffice/users/create" component={UserForm} />
+        <Route path="/backoffice/users/edit/:id" component={UserForm} />
+        <Route path="/backoffice/news" component={NewsList} />
+        <Route path="/backoffice/news/create" component={NewsForm} />
+        <Route path="/backoffice/news/edit/:id" component={NewsForm} />
+        <Route path="/backoffice/organization" component={OrganizationData} />
+        <Route
+          path="/backoffice/organization/edit"
+          component={OrganizationDataForm}
+        />
+        <Route path="/backoffice/projects/create" component={ProjectsForm} />
+        <Route path="/backoffice/projects/edit/:id" component={ProjectsForm} />
+      </Switch>
     </div>
   );
 };
