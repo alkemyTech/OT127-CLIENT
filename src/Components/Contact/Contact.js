e57a7dedc3e9f712b6623af6348c45fddb0d3541
Gabriel Suarez <67937686+silversurferbeats@users.noreply.github.com>
2022-02-12 19:01:00 -0300

    OT127-35 Feat Formulario creación/edición miembro (#59)
    * creacion formulario miembro
    
    * correccion: implementacion useState en inivialValues
    
    * agrego CKEditor y estilos sass
    
    * console.logs
    
    * coneccion put patch
    
    * validacion id en las peticiones http
    
    * correccion logica getDataById y estilos SASS
    
    * correccion logica handleSubmit
    
    * cambio de nombre a handleImag
    
    * solucion peticion put
    
    * correcciones SASS y cambio a formValues
    
    * quito el destructurin en habdleSubmit
    
    * solucion de conflictos

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,7 +1,6 @@
 import {useState, useEffect} from "react";
 import axios from "axios";
 import Title from "../Titulosynovedades/Title";
-import "./contact.scss";
 import {sweetAlertError} from "../../Services/sweetAlertServices";
 
 const Contact = () => {
