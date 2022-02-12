5de777fd175bc32ef035dc2c4c013e2f1fcc5a67
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-02-12 00:23:28 -0300

    estrutura content

--- src/Campaigns/Toys/Content.js
+++ src/Campaigns/Toys/Content.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const Content = () => {
   return (
-    <div>
-
+    <div className="content__container">
+      <h2 className="content__date"></h2>
+      <p className="content__description">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
+        similique consectetur, necessitatibus eaque maxime architecto earum
+        neque. Sapiente perspiciatis eligendi, non a minima odio nihil velit
+        vero incidunt iure eius.
+      </p>
+      <h3 className="content__countdown"></h3>
+      <img src="" alt="" className="content__img" />
+      <img src="" alt="" className="content__img" />
+      <img src="" alt="" className="content__img" />
+      <img src="" alt="" className="content__img" />
     </div>
   );
-}
- 
+};
+
 export default Content;
