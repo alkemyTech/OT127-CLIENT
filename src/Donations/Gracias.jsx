983fb49fca95e288d08f8e52436e3e70cce2f912
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-02-12 19:20:10 -0300

    estructura Gracias modificada

--- src/Donations/Gracias.jsx
+++ src/Donations/Gracias.jsx
@@ -2,10 +2,9 @@ import React from "react";
 
 const Gracias = () => {
   return (
-    <div className="tanks__container">
-      <h1 className="thanks_Xtitle">Muchas Gracias por tu Ayuda!!</h1>
+    <div className="thanks__container">
       <div className="thanks__content">
-        <img src={process.env.PUBLIC_URL + "/images/happy-kid.png"} alt="kid" />
+        <h1 className="thanks__title">Muchas Gracias por tu Ayuda!!</h1>
         <p className="thanks__text">
           Mediante nuestros programas educativos, buscamos incrementar la
           capacidad intelectual, moral y afectiva de las personas de acuerdo con
