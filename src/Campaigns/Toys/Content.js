95fc1238be02aec817a3fa5fbae669b93f9a38a8
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-02-13 15:26:28 -0300

    estructura content + images

--- src/Campaigns/Toys/Content.js
+++ src/Campaigns/Toys/Content.js
@@ -3,7 +3,24 @@ import React from "react";
 const Content = () => {
   return (
     <div className="content__container">
-      <h2 className="content__date"></h2>
+      <h2 className="content__date">
+        25/02/2022 calle 123, barrio La Cava, Buenos Aires
+      </h2>
+      <img
+        src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-1.jpg"}
+        alt=""
+        className="content__img"
+      />
+      <img
+        src={process.env.PUBLIC_URL + "/images/toys-campaign/niños-3.jpg"}
+        alt=""
+        className="content__img"
+      />
+      <img
+        src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-2.jpg"}
+        alt=""
+        className="content__img"
+      />
       <p className="content__description">
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
         similique consectetur, necessitatibus eaque maxime architecto earum
@@ -11,10 +28,16 @@ const Content = () => {
         vero incidunt iure eius.
       </p>
       <h3 className="content__countdown"></h3>
-      <img src="" alt="" className="content__img" />
-      <img src="" alt="" className="content__img" />
-      <img src="" alt="" className="content__img" />
-      <img src="" alt="" className="content__img" />
+      <img
+        src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-4.jpg"}
+        alt=""
+        className="content__img"
+      />
+      <img
+        src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-5.jpg"}
+        alt=""
+        className="content__img"
+      />
     </div>
   );
 };
