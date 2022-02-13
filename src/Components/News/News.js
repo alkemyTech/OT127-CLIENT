2e5f86968c4f144c251cda4e51a9224a007226ff
chriscaracach <christiancaracach@gmail.com>
2022-02-13 15:04:55 -0300

    Fix: Conflicts solved

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -1,68 +1,47 @@
 import React, { useEffect, useState } from "react";
-import { useSelector, useDispatch } from 'react-redux'
-import { getNews } from '../../Redux/reducers/newsSlice'
+import { useSelector, useDispatch } from "react-redux";
+import { getNews } from "../../Redux/reducers/newsSlice";
 import { useBottomScrollListener } from "react-bottom-scroll-listener";
-import Spinner from '../Spinner/Spinner';
+import Spinner from "../Spinner/Spinner";
 import Comments from "./Comments";
 
 const News = () => {
-	const dispatch = useDispatch()
-	const [ showComments, setShowComments ] = useState(false);
-	const [ isLoading, setIsLoading ] = useState(true);
-	useBottomScrollListener(() => setShowComments(true));
+  const dispatch = useDispatch();
+  const [showComments, setShowComments] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  useBottomScrollListener(() => setShowComments(true));
 
-<<<<<<< HEAD
-  const news = useSelector(state => state.newsReducer.news.data)
-  
+  useEffect(() => {
+    dispatch(getNews());
+    setIsLoading(false);
+  }, []); //eslint-disable-line
+
+  const news = useSelector((state) => state.newsReducer.news.data);
+
   const newsList = () => {
-    return news.length ? (
-      news.map((element) => (
-        <li key={element.id}>
-          <h3>{element.name}</h3>
-          <p>{element.content}</p>
-        </li>
-      ))
-    ) : (
-      <div>
-      <p>No hay novedades</p>
-      </div>
-    );
+    return news.length
+      ? news.map((element) => (
+          <li className="card-info" key={element.id}>
+            <h3>{element.name}</h3>
+            <p>{element.description}</p>
+          </li>
+        ))
+      : null;
   };
-=======
->>>>>>> 03ed2e218d3cb49b7dfc00d802e2e4412a317482
 
-	useEffect(() => {
-		dispatch(getNews())
-		setIsLoading(false)
-	}, []); //eslint-disable-line
+  return (
+    <>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div>
+          <h1>Novedades</h1>
+          <ul className="list-container">{newsList()}</ul>
+          {showComments && <Comments />}
+        </div>
+      )}
+    </>
+  );
+};
 
-	const news = useSelector(state => state.newsReducer.news.data)
-
-	const newsList = () => {
-		return news.length ? (
-			news.map((element) => (
-				<li className="card-info" key={element.id}>
-					<h3>{element.name}</h3>
-					<p>{element.description}</p>
-				</li>
-			))
-		) : (
-			null
-		);
-	};
-
-	return (
-		<>
-			{isLoading
-				? <Spinner />
-				: (<div>
-					<h1>Novedades</h1>
-					<ul className="list-container">{newsList()}</ul>
-					{showComments && <Comments />}
-				</div>
-				)}
-		</>
-	)
-}
-
-export default News
+export default News;
