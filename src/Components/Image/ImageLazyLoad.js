233e25dc90fa733d08cca7d63a34273333e09f06
chriscaracach <christiancaracach@gmail.com>
2022-02-14 17:49:22 -0300

    Fix: Added classname prop and placeholder

--- src/Components/Image/ImageLazyLoad.js
+++ src/Components/Image/ImageLazyLoad.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 // El componente acepta los atributos comunes de una <img> (src, alt)
 // También acepta width y height (number, no string)
 // y placeholder (componente que se va a mostrar mientras se carga la imagen)
 
-const ImageLazyLoad = ({ src, alt, height, width, placeholder, className }) => {
+const ImageLazyLoad = ({
+  src,
+  alt,
+  height,
+  width,
+  placeholder = "https://via.placeholder.com/150",
+  className,
+}) => {
   return (
     <>
       <LazyLoadImage
@@ -14,7 +22,7 @@ const ImageLazyLoad = ({ src, alt, height, width, placeholder, className }) => {
         alt={alt}
         height={height}
         width={width}
-        placeholder={placeholder}
+        placeholderSrc={placeholder}
         effect="opacity"
       />
     </>
