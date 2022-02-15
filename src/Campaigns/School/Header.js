d0e595804b12bee45b907d38a9a9561e2d8b89f5
chriscaracach <christiancaracach@gmail.com>
2022-02-14 21:35:32 -0300

    Feat: Details in css and real images (no placeholder)

--- src/Campaigns/School/Header.js
+++ src/Campaigns/School/Header.js
@@ -1,20 +1,22 @@
 import React from "react";
 import ImageLazyLoad from "../../Components/Image/ImageLazyLoad";
+import ongLogo from "../../images/LOGO-SOMOS_MAS.png";
+import campaignLogo from "../../images/campaña-escolar.jpeg";
 
 const Header = () => {
   return (
     <header className="schoolcampaignheader">
       <ImageLazyLoad
-        src="https://placeimg.com/200/200/tech"
+        src={campaignLogo}
         alt="Texto alt 1"
         className="schoolcampaignheader__campaignlogo"
       />
+      <h1 className="schoolcampaignheader__slogan">Seamos útiles</h1>
       <ImageLazyLoad
-        src="https://placeimg.com/200/200/any"
+        src={ongLogo}
         alt="Texto alt 1"
         className="schoolcampaignheader__onglogo"
       />
-      <h1 className="schoolcampaignheader__slogan">Slogan de campaña</h1>
     </header>
   );
 };
