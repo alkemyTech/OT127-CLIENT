a8fd296e1fe755575cadab00f1395df2092bca55
danieldamian09 <humbertorivero9@gmail.com>
2022-02-15 20:09:39 -0300

    loading and error message in activity component

--- src/Components/Activities/ActivitiesList.js
+++ src/Components/Activities/ActivitiesList.js
@@ -1,32 +1,54 @@
-import React from 'react';
-import '../../sass/components/_card.scss';
+import Title from "../Titulosynovedades/Title";
+import {useState, useEffect} from "react";
+import axios from "axios";
+import Spinner from "../Spinner/Spinner";
+import {sweetAlertError} from "../../Services/sweetAlertServices"
 
 const ActivitiesList = () => {
-    const activitiesMock = [
-        { id: 2, name: 'Titulo de prueba', description: 'Descripcion de prueba' },
-        { id: 1, name: 'Titulo de prueba', description: 'Descripcion de prueba' },
-        { id: 3, name: 'Titulo de prueba', description: 'Descripcion de prueba' }
-    ];
+	const [activities, setActivities] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const url = "http://ongapi.alkemy.org/api/activities";
 
-    return (
-        <div>
-            <h1>Listado Actividades</h1>
-            <ul className="list-container">
-                {activitiesMock.length > 0 ?
-                    activitiesMock.map((activity) => {
-                        return (
-                            <li className="card-info" key={activity.id}>
-                                <h3>{activity.name}</h3>
-                                <p>{activity.description}</p>
-                            </li>
-                        )
-                    })
-                    :
-                    <p>No hay actividades</p>
-                }
-            </ul>
-        </div>
-    );
-}
+	const getActivities = () => {
+		setLoading(true);
+		axios.get(url).then(({data}) => {
+			setActivities(data);
+			setLoading(false);
+		}).catch((err) => {
+      sweetAlertError()
+      return err
+    })
+	};
 
+	useEffect(() => {
+		getActivities();
+	}, []);
+
+	return (
+		<div className="activitites">
+			<Title title="Actividades" />
+			<ul className="activities__ul">
+				{loading ? (
+					<Spinner />
+				) : (
+					activities.data.map(({id, name, description, image}) => (
+						<li key={id}>
+							<div className="activities__container">
+								<div className="activities__body">
+									<div className="activities__body-title">
+										<h2>{name}</h2>
+									</div>
+									<div className="activities__body-p">{description}</div>
+								</div>
+								<div className="activities__image">
+									<img src={image} alt={name} />
+								</div>
+							</div>
+						</li>
+					))
+				)}
+			</ul>
+		</div>
+	);
+};
 export default ActivitiesList;
