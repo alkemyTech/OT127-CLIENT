9aeba6b79c0a9875ad8ccc2f0b0a9faacdc0dcca
cardosonicolas <cardoso.leandro.nicolas@gmail.com>
2022-02-15 20:47:44 -0300

    structure header

--- src/Campaigns/Toys/Header.js
+++ src/Campaigns/Toys/Header.js
@@ -4,7 +4,13 @@ import campaignLogo from "../../images/Logotipo-campaña-juguetes.png";
 import ongLogo from "../../images/LOGO-SOMOS_MAS.png";
 
 const Header = () => {
-  return <header></header>;
+  return (
+    <header>
+      <ImageLazyLoad src={campaignLogo} />
+      <h1>Juguetes por más sonrisas</h1>
+      <ImageLazyLoad src={ongLogo} />
+    </header>
+  );
 };
 
 export default Header;
