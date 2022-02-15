a84daca1e5bd6c5d6b815e3a04402c0f7686bcff
Chris Caracach <69370931+Chriscaracach@users.noreply.github.com>
2022-02-14 23:37:25 -0300

    Ot127 131/feat/landing header (#128)
    * Feat: Main structure of component
    
    * Fix: Added classname as prop
    
    * Feat: Added breakpoints variables / import component in main.scss
    
    * Feat: Main structure of Schoolcampaignheader style
    
    * Fix: Added classname prop and placeholder
    
    * Feat: Added slogan and improve img
    
    * Feat: Details in css and real images (no placeholder)

--- src/Components/Image/ImageLazyLoad.js
+++ src/Components/Image/ImageLazyLoad.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 // El componente acepta los atributos comunes de una <img> (src, alt)
 // También acepta width y height (number, no string)
 // y placeholder (componente que se va a mostrar mientras se carga la imagen)
 
-const ImageLazyLoad = ({ src, alt, height, width, placeholder }) => {
+const ImageLazyLoad = ({
+  src,
+  alt,
+  height,
+  width,
+  placeholder = "https://via.placeholder.com/150",
+  className,
+}) => {
   return (
     <>
       <LazyLoadImage
+        className={className}
         src={src}
         alt={alt}
         height={height}
         width={width}
-        placeholder={placeholder}
+        placeholderSrc={placeholder}
         effect="opacity"
       />
     </>
