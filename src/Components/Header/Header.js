ae5e4cc895a27f7ec43aec75797186257813f774
Nel Rodriguez <nel.rodriguez22@gmail.com>
2022-02-16 13:11:46 -0300

    OT127-124/Feat/comp-header (#112)
    * 1stRel
    
    * 1stRel
    
    * Cambio en el menu, paso de objeto a array de objetos
    
    * Cambios en los nombres de las clases de sass y mejor anidacion

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+
+import '../../sass/layout/_header.scss'
 
 const Header = () => {
-    return (
-        <p>Header</p>
-    )
-}
+	const isLogged = useSelector(state => state.authReducer.userIsLogged)
 
+	const menuItems = [
+		{ link: '/school-campaign', name: 'Campaña escolar' },
+		{ link: '/toys-campaign', name: 'Campaña de juguetes' },
+		{ link: '/nosotros', name: 'Nosotros' },
+		{ link: '/contacto', name: 'Contacto' }
+	]
+
+	return (
+		<>
+			<header className="header">
+				<nav className="header__nav">
+					<div className="header__nav-left">
+						<NavLink
+                            className="header__nav-links"
+                            activeClassName="header__nav-links-active"
+							to="/">Inicio
+						</NavLink>
+					</div>
+					<div className="header__nav-right">
+						{!isLogged
+							? (
+								<ul className="header__nav-list">
+									{menuItems.map(item => (
+										<li key={item.name} className="header__nav-item">
+											<NavLink
+                                                className="header__nav-links"
+                                                activeClassName="header__nav-links-active"
+												to={item.link}>
+												{item.name}
+											</NavLink>
+										</li>
+									))}
+								</ul>
+							)
+							: null //Login/Register ?
+						}
+					</div>
+				</nav>
+			</header>
+		</>
+	)
+}
 export default Header
