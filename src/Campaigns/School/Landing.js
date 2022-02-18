735f0741eaa81cb17f95e529747fc00c097e3df3
silversurferbeats <gabrielsuarez89@gmail.com>
2022-02-18 18:24:25 -0300

    Landing Responsive

--- src/Campaigns/School/Landing.js
+++ src/Campaigns/School/Landing.js
@@ -12,52 +12,56 @@ const Landing = () => {
     );
   };
   return (
-    
     <div className="landing__container">
-      <label>descripcion:</label>
-      <input type='text' name='descripcion' className="landing__input" />
+        <div className="login-container">
+            <div className="login-info-container">
+                <h1 className="title">Log in with</h1>
+                
+                <form className="inputs-container">
+                    <input className="input" type="text" placeholder="Descripcion" />
+                    <input className="input" type="text" placeholder="Fecha" />
+                    <input className="input" type="text" placeholder="Hora" />
+                    <input className="input" type="text" placeholder="Lugar" />
+                    
+                    <button className="btn">login</button>
+                    <p>Don't have an account? <span className="span">Sign Up</span></p>
+                    <h3 className="content__countdown">
+                    <Countdown date={new Date(2022, 1, 25, 0, 0, 0)} renderer={renderer} />
+                    </h3>
+                </form>
+            </div>
+        </div>
 
-      <label>Fecha:</label>
-      <input type='date' name='fecha' className="landing__input" />
 
-      <label>Hora:</label>
-      <input type='time' name='hora' className="landing__input" />
-
-      <label>Lugar:</label>
-      <input type='text' name='lugar' className="landing__input" />
-
-     
-
-      
-      <img
+        <img
         src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-1.jpg"}
         alt=""
         className="content__img"
-      />
-      <h3 className="content__countdown">
+        />
+        <h3 className="content__countdown">
         <Countdown date={new Date(2022, 1, 25, 0, 0, 0)} renderer={renderer} />
-      </h3>
-      <img
+        </h3>
+        <img
         src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-2.jpg"}
         alt=""
         className="content__img"
-      />
-      
-      <img
+        />
+        
+        <img
         src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-4.jpg"}
         alt=""
         className="content__img"
-      />
-      <img
+        />
+        <img
         src={process.env.PUBLIC_URL + "/images/toys-campaign/niños-3.jpg"}
         alt=""
         className="content__img"
-      />
-      <img
+        />
+        <img
         src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-5.jpg"}
         alt=""
         className="content__img"
-      />
+        />
     </div>
   );
 };
