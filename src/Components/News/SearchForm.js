6ea41e9d7ec68e145b1dce63aaed9ff4b2504b5a
chriscaracach <christiancaracach@gmail.com>
2022-02-18 10:15:16 -0300

    Feat: Add classname and search Icon

--- src/Components/News/SearchForm.js
+++ src/Components/News/SearchForm.js
@@ -1,9 +1,11 @@
 import React from "react";
+import SearchIcon from "@material-ui/icons/Search";
 
 const SearchForm = ({ searchNews }) => {
   return (
     <>
-      <input type="text" onChange={(e) => searchNews(e)} />
+      <SearchIcon></SearchIcon>
+      <input type="text" onChange={(e) => searchNews(e)} className="input" />
     </>
   );
 };
