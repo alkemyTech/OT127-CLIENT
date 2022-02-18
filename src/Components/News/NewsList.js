40765ca5b76669e1d1c7a4937c8dd3db1879042a
chriscaracach <christiancaracach@gmail.com>
2022-02-18 10:34:46 -0300

    Feat: Added list and filteredList map / conditional rendering

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
@@ -8,25 +8,84 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
-import { getNews } from '../../Redux/reducers/newsSlice'
-import { useSelector, useDispatch } from 'react-redux'
+import { getNews } from "../../Redux/reducers/newsSlice";
+import { useSelector, useDispatch } from "react-redux";
 
 const NewsList = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+  const [filteredNews, setFilteredNews] = useState([]);
 
   useEffect(() => {
-    dispatch(getNews())
+    dispatch(getNews());
   }, []); //eslint-disable-line
 
-  const news = useSelector(state => state.newsReducer.news.data)
+  const news = useSelector((state) => state.newsReducer.news.data);
 
   const handleClickEdit = () => {
-    //TODO, acciones editar novedades 
-  }
+    //TODO, acciones editar novedades
+  };
 
   const handleClickDelete = () => {
     //TODO, acciones para borrar novedades
-  }
+  };
+
+  const newsMap = () => {
+    return news.map((element) => (
+      <TableRow
+        key={element.id}
+        sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+      >
+        <TableCell component="th" scope="row">
+          {element.name}
+        </TableCell>
+        <TableCell align="right">
+          <img src={element.image} alt="News_image" width="150em" />
+        </TableCell>
+        <TableCell align="right">{element.created_at}</TableCell>
+        <TableCell align="right">
+          <Button
+            variant="outlined"
+            style={{ marginRight: "1em" }}
+            onClick={handleClickEdit}
+          >
+            Edit
+          </Button>
+          <Button variant="outlined" color="error" onClick={handleClickDelete}>
+            Delete
+          </Button>
+        </TableCell>
+      </TableRow>
+    ));
+  };
+
+  const filteredNewsMap = () => {
+    return filteredNews.map((element) => (
+      <TableRow
+        key={element.id}
+        sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+      >
+        <TableCell component="th" scope="row">
+          {element.name}
+        </TableCell>
+        <TableCell align="right">
+          <img src={element.image} alt="News_image" width="150em" />
+        </TableCell>
+        <TableCell align="right">{element.created_at}</TableCell>
+        <TableCell align="right">
+          <Button
+            variant="outlined"
+            style={{ marginRight: "1em" }}
+            onClick={handleClickEdit}
+          >
+            Edit
+          </Button>
+          <Button variant="outlined" color="error" onClick={handleClickDelete}>
+            Delete
+          </Button>
+        </TableCell>
+      </TableRow>
+    ));
+  };
 
   return (
     <div>
@@ -42,29 +101,7 @@ const NewsList = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {news.length &&
-              news.map((element) => (
-                <TableRow
-                  key={element.id}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {element.name}
-                  </TableCell>
-                  <TableCell align="right">
-                    <img src={element.image} alt="News_image" width="150em" />
-                  </TableCell>
-                  <TableCell align="right">{element.created_at}</TableCell>
-                  <TableCell align="right">
-                    <Button variant="outlined" style={{ marginRight: "1em" }} onClick={handleClickEdit}>
-                      Edit
-                    </Button>
-                    <Button variant="outlined" color="error" onClick={handleClickDelete}>
-                      Delete
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
+            {filteredNews.length !== 0 ? filteredNewsMap() : newsMap()}
           </TableBody>
         </Table>
       </TableContainer>
