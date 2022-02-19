a460c1d09a06970e7bef60b8be4fc3e3a330a873
danieldamian09 <humbertorivero9@gmail.com>
2022-02-19 16:09:35 -0300

    Updating component HTML structure, removing console.log and adding component styles

--- src/Components/Activities/Activities.js
+++ src/Components/Activities/Activities.js
@@ -10,19 +10,6 @@ import Spinner from "../Spinner/Spinner";
 
 // ! Sacar Mock y hacer logica de Get y agregar logica de Edit, Delete y renderizar en Backoffice
 
-import {
-	Container,
-	Table,
-	TableBody,
-	TableContainer,
-	TableHead,
-	Paper,
-	Button,
-} from "@mui/material";
-import {styled} from "@mui/material/styles";
-import TableCell, {tableCellClasses} from "@mui/material/TableCell";
-import TableRow, {tableRowClasses} from "@mui/material/TableRow";
-
 const Activities = () => {
 	const [activities, setActivities] = useState([]);
 	const [loading, setLoading] = useState(true);
@@ -37,7 +24,7 @@ const Activities = () => {
 			const {data} = await Axios.get(url);
 			const activitiesData = data.data;
 			setActivities(activitiesData);
-      setLoading(false);
+			setLoading(false);
 		} catch (error) {
 			sweetAlertError();
 			return error;
@@ -67,86 +54,73 @@ const Activities = () => {
 			);
 			setActivities(activitiesUpDate);
 			sweetAlertInfo("Registro Eliminado con Exito");
-			console.log(respuesta);
+			return respuesta;
 		} catch (error) {
 			return error;
 		}
 	};
 
-	// estilos
-	const StyledTableCell = styled(TableCell)(({theme}) => ({
-		[`&.${tableCellClasses.head}`]: {
-			backgroundColor: theme.palette.common.black,
-			color: theme.palette.common.white,
-		},
-		[`&.${tableCellClasses.body}`]: {
-			fontSize: 14,
-		},
-	}));
-
-	const StyledTableRow = styled(TableRow)(({theme}) => ({
-		[`&.${tableRowClasses.root}`]: {
-			height: "50px",
-			width: "70px",
-		},
-		"&:nth-of-type(odd)": {
-			backgroundColor: theme.palette.action.hover,
-		},
-		// hide last border
-		"&:last-child td, &:last-child th": {
-			border: 0,
-		},
-	}));
-	// fin de stilos
 	return (
-		<Container maxWidth="md">
-			<Link to="/backoffice/create-activity">Create Activity</Link>
-			{loading ? (
-				<Spinner />
-			) : (
-				<TableContainer component={Paper}>
-					<Table sx={{minWidth: 600}} stickyHeader>
-						<TableHead>
-							<TableRow>
-								<StyledTableCell>Nombre</StyledTableCell>
-								<StyledTableCell>Imagen</StyledTableCell>
-								<StyledTableCell></StyledTableCell>
-							</TableRow>
-						</TableHead>
-						<TableBody>
+		<div className="table">
+			<div className="table__container">
+				<div className="table__actions">
+					<input type="search" />
+					<Link className="table__link" to="/backoffice/create-activity">
+						Crear Actividad
+					</Link>
+				</div>
+				{loading ? (
+					<Spinner />
+				) : (
+					<table className="table__data">
+						<thead className="table__head">
+							<tr className="table__row">
+								<th className="table__title">Nombre</th>
+								<th className="table__title">Imagen</th>
+								<th className="table__title-edit">Editar</th>
+								<th className="table__title-delete">Eliminar</th>
+							</tr>
+						</thead>
+						<tbody className="table__body">
 							{activities.map((activity) => (
-								<StyledTableRow key={activity.id}>
-									<StyledTableCell scope="row">{activity.name}</StyledTableCell>
-									<StyledTableCell>
+								<tr key={activity.id} className="table__row">
+									<td className="table__cell">{activity.name}</td>
+									<td className="table__cell">
 										<img
 											src={activity.image}
 											alt={activity.name}
 											width="50px"
 										/>
-									</StyledTableCell>
-									<StyledTableCell style={{width: "25%"}}>
-										<Button
-											color="success"
-											onClick={() => handleEdit(activity.id)}
+									</td>
+									<td className="table__cell-edit">
+										<button
+											className="table__edit"
+											onClick={() => handleEdit(activity?.id)}
 										>
 											Editar
-										</Button>{" "}
-										<Button
-											color="error"
+										</button>
+									</td>
+									<td className="table__cell-delete">
+										<button
+											className="table__delete"
 											onClick={() =>
-												handleDelete(activity.id, activity.name, activity.image)
+												handleDelete(
+													activity?.id,
+													activity.name,
+													activity.image
+												)
 											}
 										>
 											Eliminar
-										</Button>
-									</StyledTableCell>
-								</StyledTableRow>
+										</button>
+									</td>
+								</tr>
 							))}
-						</TableBody>
-					</Table>
-				</TableContainer>
-			)}
-		</Container>
+						</tbody>
+					</table>
+				)}
+			</div>
+		</div>
 	);
 };
 
