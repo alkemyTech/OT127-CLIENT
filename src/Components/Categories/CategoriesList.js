e71cfc94893b5b7d6e6b210b335dae51eff6c237
danieldamian09 <humbertorivero9@gmail.com>
2022-02-19 13:17:48 -0300

    add styles to CategoriesList

--- src/Components/Categories/CategoriesList.js
+++ src/Components/Categories/CategoriesList.js
@@ -12,7 +12,6 @@ import {
 } from "../../Redux/selector/selectorCategories";
 
 const CategoriesList = () => {
-
 	const dispatch = useDispatch();
 	const isFetchingCategories = useSelector(
 		isFetchingCategoriesSel,
@@ -47,34 +46,46 @@ const CategoriesList = () => {
 	};
 
 	return (
-		<>
-			<Link to="/create-category">Crear categoría</Link>
-			<h1>Categorías</h1>
-			<table>
-				<thead>
-					<tr>
-						<th>Nombre</th>
-						<th>Fecha de Creación</th>
-						<th>Editar</th>
-						<th>Eliminar</th>
-					</tr>
-				</thead>
-				<tbody>
-					{categories?.data.map((categori) => (
-						<tr key={categori.id}>
-							<td>{categori.name}</td>
-							<td>{handleDate(categori.created_at)}</td>
-							<td>
-								<button onClick={handleEdit}>Editar</button>
-							</td>
-							<td>
-								<button onClick={handleDelete}>Editar</button>
-							</td>
+		<div className="table">
+			<div className="table__container">
+				<div className="table__actions">
+					<input type="search" />
+					<Link className="table__link" to="/create-category">
+						Crear categoría
+					</Link>
+				</div>
+				<table className="table__data">
+					<thead className="table__head">
+						<tr className="table__row">
+							<th className="table__title">Nombre</th>
+							<th className="table__title">Fecha de Creación</th>
+							<th className="table__title-edit">Editar</th>
+							<th className="table__title-delete">Eliminar</th>
 						</tr>
-					))}
-				</tbody>
-			</table>
-		</>
+					</thead>
+					<tbody className="table__body">
+						{categories?.data.map((categori) => (
+							<tr key={categori.id} className="table__row">
+								<td className="table__cell">{categori.name}</td>
+								<td className="table__cell">
+									{handleDate(categori.created_at)}
+								</td>
+								<td className="table__cell-edit">
+									<button className="table__edit" onClick={handleEdit}>
+										Editar
+									</button>
+								</td>
+								<td className="table__cell-delete">
+									<button className="table__delete" onClick={handleDelete}>
+										Eliminar
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+		</div>
 	);
 };
 
