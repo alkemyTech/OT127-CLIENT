6a943884ed2af96a590f678ee748eb69a58e455a
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-02-19 19:25:39 -0300

    Agregado minHeight a layouts

--- src/Components/Layout/LayoutPublic.js
+++ src/Components/Layout/LayoutPublic.js
@@ -4,11 +4,11 @@ import Header from '../Header/Header'
 
 const LayoutPublic = (props) => {
     return (
-        <>
+        <div className='app_container'>
             <Header />
             {props.children}
             <PublicFooter />
-        </>
+        </div>
     )
 }
 
