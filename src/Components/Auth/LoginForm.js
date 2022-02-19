0bfc3e805b17a5c614adf0bb2333d90a4e8a57be
Chris Caracach <69370931+Chriscaracach@users.noreply.github.com>
2022-02-19 15:24:51 -0300

    Ot127 140/feat/logout logic (#148)
    * Feat: Added button to header
    
    * Feat: Added Redux logic for logout
    
    * Feat: Added set Token (email) in localStorage
    
    * Feat: Added login service
    
    * Feat: Added APILoginUser method for login in API
    
    * Feat: Added localStorage logic and work in auth method
    
    * Feat: create Register method for API
    
    * Feat: Add register method from services
    
    * Feat: add APIRegisterUser method
    
    * Feat: Add redirection to home when login is successfull
    
    * Feat: cleaning authToken in reducer / comments
    
    * Fix: Conflicts solved
    
    * Fix: conflicts solved

--- src/Components/Auth/LoginForm.js
+++ src/Components/Auth/LoginForm.js
@@ -1,15 +1,31 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import Spinner from "../Spinner/Spinner";
+import { useDispatch } from "react-redux";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import { loginUser, setToken } from "../../Redux/actions/authActions";
+import { APIloginUser } from "../../Services/userService";
+import { sweetAlertError } from "../../Services/sweetAlertServices";
 
 const LoginForm = () => {
-  const [userData, setUserData] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-  const handleSubmit = (values) => {
-    setUserData([
-      ...userData,
-      { email: values.email, password: values.password },
-    ]);
+  const handleSubmit = async (values) => {
+    setisLoading(true);
+    try {
+      let response = await APIloginUser(values);
+      let { token, user } = response.data.data;
+      dispatch(loginUser(user));
+      dispatch(setToken(token));
+      localStorage.setItem("TOKEN", token);
+      history.push("/");
+    } catch (error) {
+      sweetAlertError("Contraseña o usuario incorrectos");
+    }
+    setisLoading(false);
   };
 
   return (
@@ -33,15 +49,26 @@ const LoginForm = () => {
         }}
       >
         <Form className="form">
-          <label htmlFor="email" className="form__label">Email</label>
-          <Field name="email" type="email" className="form__input"/>
-          <ErrorMessage name="email" render={(msg) => <div className="form__error">{msg}</div>}/>
+          <label htmlFor="email" className="form__label">
+            Email
+          </label>
+          <Field name="email" type="email" className="form__input" />
+          <ErrorMessage
+            name="email"
+            render={(msg) => <div className="form__error">{msg}</div>}
+          />
 
-          <label htmlFor="password" className="form__label">Contraseña</label>
-          <Field name="password" type="password" className="form__input"/>
-          <ErrorMessage name="password" render={(msg) => <div className="form__error">{msg}</div>}/>
-
-          <button type="submit" className="form__button">Entrar</button>
+          <label htmlFor="password" className="form__label">
+            Contraseña
+          </label>
+          <Field name="password" type="password" className="form__input" />
+          <ErrorMessage
+            name="password"
+            render={(msg) => <div className="form__error">{msg}</div>}
+          />
+          <button type="submit" className="form__button">
+            {!isLoading ? "Entrar" : <Spinner size={30}></Spinner>}
+          </button>
         </Form>
       </Formik>
     </div>
