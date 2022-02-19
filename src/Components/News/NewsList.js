bc7e85157bc559419e8b26aca9e9390ad9b7eff4
danieldamian09 <humbertorivero9@gmail.com>
2022-02-19 17:25:56 -0300

    Updating HTML structure of the component, adding spinner and styles

--- src/Components/News/NewsList.js
+++ src/Components/News/NewsList.js
@@ -1,75 +1,76 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import Button from "@mui/material/Button";
-import { getNews } from '../../Redux/reducers/newsSlice'
-import { useSelector, useDispatch } from 'react-redux'
+import React, {useEffect} from "react";
+import {Link} from "react-router-dom";
+import {getNews} from "../../Redux/reducers/newsSlice";
+import {useSelector, useDispatch} from "react-redux";
+import Spinner from "../Spinner/Spinner";
 
 const NewsList = () => {
-  const dispatch = useDispatch()
+	const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getNews())
-  }, []); //eslint-disable-line
+	useEffect(() => {
+		dispatch(getNews());
+	}, []); //eslint-disable-line
 
-  const news = useSelector(state => state.newsReducer.news.data)
+	const news = useSelector((state) => state.newsReducer.news.data);
 
-  const handleClickEdit = () => {
-    //TODO, acciones editar novedades 
-  }
+	const handleClickEdit = () => {
+		//TODO, acciones editar novedades
+	};
 
-  const handleClickDelete = () => {
-    //TODO, acciones para borrar novedades
-  }
+	const handleClickDelete = () => {
+		//TODO, acciones para borrar novedades
+	};
 
-  return (
-    <div>
-      <Link to="/backoffice/news/create">Create news</Link>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Name</TableCell>
-              <TableCell align="right">Image</TableCell>
-              <TableCell align="right">Created At</TableCell>
-              <TableCell align="right"></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {news.length &&
-              news.map((element) => (
-                <TableRow
-                  key={element.id}
-                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                >
-                  <TableCell component="th" scope="row">
-                    {element.name}
-                  </TableCell>
-                  <TableCell align="right">
-                    <img src={element.image} alt="News_image" width="150em" />
-                  </TableCell>
-                  <TableCell align="right">{element.created_at}</TableCell>
-                  <TableCell align="right">
-                    <Button variant="outlined" style={{ marginRight: "1em" }} onClick={handleClickEdit}>
-                      Edit
-                    </Button>
-                    <Button variant="outlined" color="error" onClick={handleClickDelete}>
-                      Delete
-                    </Button>
-                  </TableCell>
-                </TableRow>
-              ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </div>
-  );
+	return (
+		<div className="table">
+			<div className="table__container">
+				<div className="table__actions">
+					<input type="search" />
+					<Link className="table__link" to="/backoffice/news/create">
+						Crear Novedad
+					</Link>
+				</div>
+				{!news.length ? (
+					<Spinner />
+				) : (
+					<table className="table__data">
+						<thead className="table__head">
+							<tr className="table__row">
+								<th className="table__title">Nombre</th>
+								<th className="table__title">Imagen</th>
+								<th className="table__title-edit">Editar</th>
+								<th className="table__title-delete">Eliminar</th>
+							</tr>
+						</thead>
+						<tbody className="table__body">
+							{news.length &&
+								news.map((element) => (
+									<tr key={element.id} className="table__row">
+										<td className="table__cell">{element.name}</td>
+										<td className="table__cell">
+											<img src={element.image} alt="News_image" width="100" />
+										</td>
+										<td className="table__cell-edit">
+											<button className="table__edit" onClick={handleClickEdit}>
+												Editar
+											</button>
+										</td>
+										<td className="table__cell-delete">
+											<button
+												className="table__delete"
+												onClick={handleClickDelete}
+											>
+												Eliminar
+											</button>
+										</td>
+									</tr>
+								))}
+						</tbody>
+					</table>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default NewsList;
