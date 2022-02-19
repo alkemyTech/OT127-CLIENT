fbd0bb80292b97b5be6f38c2f67aa50f76fba4f7
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-02-19 17:12:22 -0300

    Fix, cambio de datos que guardo en localStorage, guardo datos de usuario en lugar de solo token

--- src/Components/Backoffice/BackOffice.js
+++ src/Components/Backoffice/BackOffice.js
@@ -1,5 +1,5 @@
-import React, {lazy, Suspense} from "react";
-import {Switch, Route, Link} from "react-router-dom";
+import React, { lazy, Suspense } from "react";
+import { Switch, Route, Link } from "react-router-dom";
 import Progress from "../Progress/Porgress";
 import LayoutBackOffice from "./LayoutBackOffice";
 
@@ -18,92 +18,107 @@ const NewsForm = lazy(() => import("../News/NewsForm"));
 const NewsList = lazy(() => import("../News/NewsList"));
 const OrganizationData = lazy(() => import("../Organization/OrganizationData"));
 const OrganizationDataForm = lazy(() =>
-	import("../Organization/OrganizationDataForm")
+  import("../Organization/OrganizationDataForm")
 );
 const ProjectsForm = lazy(() => import("../Projects/ProjectsForm"));
 const BackOffice = () => {
-	const isAuthenticated = localStorage.getItem('token')
-	return (
-		<>
-		{isAuthenticated ? 
-			<div>
-				<LayoutBackOffice>
-					<Switch>
-						<Suspense fallback={<Progress height={7} />}>
-							<Route path="/backoffice/home" component={HomeForm} />
-							<Route
-								path="/backoffice/create-activity"
-								exact
-								component={ActivitiesForm}
-							/>
-							<Route
-								path="/backoffice/create-activity/:id"
-								component={ActivitiesForm}
-							/>
-							<Route path="/backoffice/activities" component={Activities} />
-							<Route
-								path="/backoffice/categories"
-								exact
-								component={CategoriesList}
-							/>
-							<Route
-								path="/backoffice/categories/create"
-								component={CategoriesForm}
-							/>
-							{/* PROBLEMA members no carga los miembros */}
-							<Route path="/backoffice/members" exact component={MembersList} />
-							<Route
-								path="/backoffice/members/create"
-								exact
-								component={MembersForm}
-							/>
-							{/* PROBLEMA MembersForm no detecta si le pasamos un id en la url */}
-							<Route
-								path="/backoffice/members/edit/:id"
-								component={MembersForm}
-							/>
-							<Route path="/backoffice/slides" exact component={SlideList} />
-							{/* PROBLEMA Res.data is undefined en create y edit slides */}
-							<Route
-								path="/backoffice/slides/create"
-								exact
-								component={SlidesForm}
-							/>
-							<Route path="/backoffice/slides/edit/:id" component={SlidesForm} />
-							<Route path="/backoffice/users" exact component={UsersList} />
-							<Route path="/backoffice/users/create" exact component={UserForm} />
-							<Route path="/backoffice/users/edit/:id" component={UserForm} />
-							<Route path="/backoffice/news" exact component={NewsList} />
-							<Route path="/backoffice/news/create" exact component={NewsForm} />
-							<Route path="/backoffice/news/edit/:id" component={NewsForm} />
-							{/* PROBLEMA Parsed is undefined (?) */}
-							<Route
-								path="/backoffice/organization"
-								exact
-								component={OrganizationData}
-							/>
-							{/* PROBLEMA se rompe todo el layout*/}
-							<Route
-								path="/backoffice/organization/edit"
-								component={OrganizationDataForm}
-							/>
-							<Route
-								path="/backoffice/projects/create"
-								component={ProjectsForm}
-							/>
-							<Route
-								path="/backoffice/projects/edit/:id"
-								component={ProjectsForm}
-							/>
-						</Suspense>
-					</Switch>
-				</LayoutBackOffice>
-			</div>
-			:
-			<Link to='/login'>Iniciá sesión</Link>
-		}
-		</>
-	);
+  const isAuthenticated = JSON.parse(localStorage.getItem("token"));
+  return (
+    <>
+      {isAuthenticated.user.role_id === 1 ? (
+        <div>
+          <LayoutBackOffice>
+            <Switch>
+              <Suspense fallback={<Progress height={7} />}>
+                <Route path="/backoffice/home" component={HomeForm} />
+                <Route
+                  path="/backoffice/create-activity"
+                  exact
+                  component={ActivitiesForm}
+                />
+                <Route
+                  path="/backoffice/create-activity/:id"
+                  component={ActivitiesForm}
+                />
+                <Route path="/backoffice/activities" component={Activities} />
+                <Route
+                  path="/backoffice/categories"
+                  exact
+                  component={CategoriesList}
+                />
+                <Route
+                  path="/backoffice/categories/create"
+                  component={CategoriesForm}
+                />
+                {/* PROBLEMA members no carga los miembros */}
+                <Route
+                  path="/backoffice/members"
+                  exact
+                  component={MembersList}
+                />
+                <Route
+                  path="/backoffice/members/create"
+                  exact
+                  component={MembersForm}
+                />
+                {/* PROBLEMA MembersForm no detecta si le pasamos un id en la url */}
+                <Route
+                  path="/backoffice/members/edit/:id"
+                  component={MembersForm}
+                />
+                <Route path="/backoffice/slides" exact component={SlideList} />
+                {/* PROBLEMA Res.data is undefined en create y edit slides */}
+                <Route
+                  path="/backoffice/slides/create"
+                  exact
+                  component={SlidesForm}
+                />
+                <Route
+                  path="/backoffice/slides/edit/:id"
+                  component={SlidesForm}
+                />
+                <Route path="/backoffice/users" exact component={UsersList} />
+                <Route
+                  path="/backoffice/users/create"
+                  exact
+                  component={UserForm}
+                />
+                <Route path="/backoffice/users/edit/:id" component={UserForm} />
+                <Route path="/backoffice/news" exact component={NewsList} />
+                <Route
+                  path="/backoffice/news/create"
+                  exact
+                  component={NewsForm}
+                />
+                <Route path="/backoffice/news/edit/:id" component={NewsForm} />
+                {/* PROBLEMA Parsed is undefined (?) */}
+                <Route
+                  path="/backoffice/organization"
+                  exact
+                  component={OrganizationData}
+                />
+                {/* PROBLEMA se rompe todo el layout*/}
+                <Route
+                  path="/backoffice/organization/edit"
+                  component={OrganizationDataForm}
+                />
+                <Route
+                  path="/backoffice/projects/create"
+                  component={ProjectsForm}
+                />
+                <Route
+                  path="/backoffice/projects/edit/:id"
+                  component={ProjectsForm}
+                />
+              </Suspense>
+            </Switch>
+          </LayoutBackOffice>
+        </div>
+      ) : (
+        <Link to="/login">Iniciá sesión</Link>
+      )}
+    </>
+  );
 };
 
 export default BackOffice;
--- src/Services/loginService.js
+++ src/Services/loginService.js
@@ -5,7 +5,7 @@ const ENDPOINT_LOGIN = process.env.REACT_APP_ENDPOINT_LOGIN;
 export const login = (data) => {
   axios.post(ENDPOINT_LOGIN, data)
   .then(response => {
-      localStorage.setItem('token', response.data.data.token)
+      localStorage.setItem('token', JSON.stringify(response.data.data))
       window.location.href = '/backoffice/organization'
       return response.data
   })
