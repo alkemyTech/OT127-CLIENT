e0569819016ce5077d0d0be90f7492673945846e
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-02-20 15:26:28 -0300

    link de inicio de sesion y registrarse

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useHistory } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { logoutUser } from "../../Redux/actions/authActions";
 
 const Header = () => {
-  const isLogged = useSelector((state) => state.authReducer.userIsLogged);
+  const isLogged = useSelector((state) => state.authReducer.authToken);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const menuItems = [
     { link: "/school-campaign", name: "Campaña escolar" },
@@ -14,8 +15,9 @@ const Header = () => {
   ];
 
   const logout = () => {
-    dispatch(logoutUser);
+    dispatch(logoutUser());
     localStorage.setItem("TOKEN", "");
+    history.push("/");
   };
 
   return (
@@ -32,7 +34,7 @@ const Header = () => {
             </NavLink>
           </div>
           <div className="header__nav-right">
-            {!isLogged ? (
+            {isLogged ? (
               <ul className="header__nav-list">
                 {menuItems.map((item) => (
                   <li key={item.name} className="header__nav-item">
@@ -45,10 +47,27 @@ const Header = () => {
                     </NavLink>
                   </li>
                 ))}
+                <button onClick={logout}>Cerrar sesión</button>
               </ul>
-            ) : null}
+            ) : (
+              <div>
+                <NavLink
+                  className="header__nav-links"
+                  activeClassName="header__nav-links-active"
+                  to="/login"
+                >
+                  Iniciar sesión
+                </NavLink>
+                <NavLink
+                  className="header__nav-links"
+                  activeClassName="header__nav-links-active"
+                  to="/register"
+                >
+                  Registrarse
+                </NavLink>
+              </div>
+            )}
           </div>
-          <button onClick={logout}>Cerrar sesión</button>
           {/* Atento cuando venga el pull de los estilos que hice, hay que borrar todo lo local, pero pasar el 
           metodo logout como prop del botton que viene */}
         </nav>
