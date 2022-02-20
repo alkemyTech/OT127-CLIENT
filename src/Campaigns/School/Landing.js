98e6684852b9768986680ca348b950f1a5935695
silversurferbeats <gabrielsuarez89@gmail.com>
2022-02-19 22:57:07 -0300

    Landing campaña escolar responsive

--- src/Campaigns/School/Landing.js
+++ src/Campaigns/School/Landing.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Countdown from "react-countdown";
 import '../../sass/components/_landing.scss';
-//import '../../sass/components/_landing'
 
 const Landing = () => {
   const renderer = ({ days, hours, minutes, seconds }) => {
@@ -15,53 +14,51 @@ const Landing = () => {
     <div className="landing__container">
         <div className="login-container">
             <div className="login-info-container">
-                <h1 className="title">Log in with</h1>
+                <h1 className="title">Campaña Escolar</h1>
                 
                 <form className="inputs-container">
                     <input className="input" type="text" placeholder="Descripcion" />
-                    <input className="input" type="text" placeholder="Fecha" />
-                    <input className="input" type="text" placeholder="Hora" />
+                    <input className="input" type="date" placeholder="Fecha" />
+                    <input className="input" type="time" placeholder="Hora" />
                     <input className="input" type="text" placeholder="Lugar" />
                     
-                    <button className="btn">login</button>
-                    <p>Don't have an account? <span className="span">Sign Up</span></p>
-                    <h3 className="content__countdown">
-                    <Countdown date={new Date(2022, 1, 25, 0, 0, 0)} renderer={renderer} />
-                    </h3>
+                    <button className="btn">enviar</button>
                 </form>
+                
+            </div>
+            <div className="conteinerGrid">
+                    <img
+                    src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-1.jpg"}
+                    alt=""
+                    className="content__img"
+                    />
+                    <h3 className="content__countdown">
+                    <Countdown date={new Date(2022, 1, 25, 0, 0, 0)} renderer={renderer} className='Countdown' />
+                    </h3>
+                    <img
+                    src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-2.jpg"}
+                    alt=""
+                    className="content__img"
+                    />
+                    
+                    <img
+                    src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-4.jpg"}
+                    alt=""
+                    className="content__img"
+                    />
+                    <img
+                    src={process.env.PUBLIC_URL + "/images/toys-campaign/niños-3.jpg"}
+                    alt=""
+                    className="content__img"
+                    />
+                    <img
+                    src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-5.jpg"}
+                    alt=""
+                    className="content__img"
+                    />
             </div>
         </div>
 
-
-        <img
-        src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-1.jpg"}
-        alt=""
-        className="content__img"
-        />
-        <h3 className="content__countdown">
-        <Countdown date={new Date(2022, 1, 25, 0, 0, 0)} renderer={renderer} />
-        </h3>
-        <img
-        src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-2.jpg"}
-        alt=""
-        className="content__img"
-        />
-        
-        <img
-        src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-4.jpg"}
-        alt=""
-        className="content__img"
-        />
-        <img
-        src={process.env.PUBLIC_URL + "/images/toys-campaign/niños-3.jpg"}
-        alt=""
-        className="content__img"
-        />
-        <img
-        src={process.env.PUBLIC_URL + "/images/toys-campaign/juguete-5.jpg"}
-        alt=""
-        className="content__img"
-        />
     </div>
   );
 };
