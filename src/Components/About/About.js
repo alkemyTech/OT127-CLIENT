c78f00924870dd81508457ca11a2ba33758c77ce
chriscaracach <christiancaracach@gmail.com>
2022-02-22 17:16:23 -0300

    Feat: Add protect routes to /about

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,30 +1,35 @@
 import React, { useEffect } from "react";
 import Title from "../Titulosynovedades/Title";
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { getUs } from "../../Redux/reducers/usSlice";
 import SocialMedia from "./SocialMedia";
 
-
 const About = ({
-	text = "Desde 1997 en Somos Más trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio La Cava ",
+  text = "Desde 1997 en Somos Más trabajamos con los chicos y chicas, mamás y papás, abuelos y vecinos del barrio La Cava ",
 }) => {
-	const dispatch = useDispatch()
-	const info = useSelector(state => state.usReducer.info)
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const isLogged = useSelector((state) => state.authReducer.userIsLogged);
+  const info = useSelector((state) => state.usReducer.info);
 
-	useEffect(() => {
-		dispatch(getUs())
-	}, []);
-	// El texto en un futuro se obtendra desde una API
-	return (
-		<>
-			<Title title="Nosotros" />
-			<main>
-				<p>{info.name}</p>
-				<p>{info.short_description}</p>
-			</main>
-			<SocialMedia />
-		</>
-	);
+  useEffect(() => {
+    if (!isLogged) {
+      history.push("/");
+    }
+    dispatch(getUs());
+  }, []);
+  // El texto en un futuro se obtendra desde una API
+  return (
+    <>
+      <Title title="Nosotros" />
+      <main>
+        <p>{info.name}</p>
+        <p>{info.short_description}</p>
+      </main>
+      <SocialMedia />
+    </>
+  );
 };
 
 export default About;
