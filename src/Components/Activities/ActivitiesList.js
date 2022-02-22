a6ee5a964ba4d477723fbf2f9ed4382efe4cad6a
chriscaracach <christiancaracach@gmail.com>
2022-02-22 17:18:02 -0300

    Feat: Added protected routes to /activities

--- src/Components/Activities/ActivitiesList.js
+++ src/Components/Activities/ActivitiesList.js
@@ -1,56 +1,66 @@
 import Title from "../Titulosynovedades/Title";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useSelector } from "react-redux";
 import axios from "axios";
 import Spinner from "../Spinner/Spinner";
-import {sweetAlertError} from "../../Services/sweetAlertServices"
+import { sweetAlertError } from "../../Services/sweetAlertServices";
 
 // ! Va en el Front si queda tiempo generar el detalle de activida(donde se muestra una sola actividad)
 
 const ActivitiesList = () => {
-	const [activities, setActivities] = useState([]);
-	const [loading, setLoading] = useState(true);
-	const url = "http://ongapi.alkemy.org/api/activities";
+  const [activities, setActivities] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const url = "http://ongapi.alkemy.org/api/activities";
+  const history = useHistory();
+  const isLogged = useSelector((state) => state.authReducer.userIsLogged);
 
-	const getActivities = () => {
-		setLoading(true);
-		axios.get(url).then(({data}) => {
-			setActivities(data);
-			setLoading(false);
-		}).catch((err) => {
-      sweetAlertError()
-      return err
-    })
-	};
+  const getActivities = () => {
+    setLoading(true);
+    axios
+      .get(url)
+      .then(({ data }) => {
+        setActivities(data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        sweetAlertError();
+        return err;
+      });
+  };
 
-	useEffect(() => {
-		getActivities();
-	}, []);
+  useEffect(() => {
+    if (!isLogged) {
+      history.push("/");
+    }
+    getActivities();
+  }, []);
 
-	return (
-		<div className="activitites">
-			<Title title="Actividades" />
-			<ul className="activities__ul">
-				{loading ? (
-					<Spinner />
-				) : (
-					activities.data.map(({id, name, description, image}) => (
-						<li key={id}>
-							<div className="activities__container">
-								<div className="activities__body">
-									<div className="activities__body-title">
-										<h2>{name}</h2>
-									</div>
-									<div className="activities__body-p">{description}</div>
-								</div>
-								<div className="activities__image">
-									<img src={image} alt={name} />
-								</div>
-							</div>
-						</li>
-					))
-				)}
-			</ul>
-		</div>
-	);
+  return (
+    <div className="activitites">
+      <Title title="Actividades" />
+      <ul className="activities__ul">
+        {loading ? (
+          <Spinner />
+        ) : (
+          activities.data.map(({ id, name, description, image }) => (
+            <li key={id}>
+              <div className="activities__container">
+                <div className="activities__body">
+                  <div className="activities__body-title">
+                    <h2>{name}</h2>
+                  </div>
+                  <div className="activities__body-p">{description}</div>
+                </div>
+                <div className="activities__image">
+                  <img src={image} alt={name} />
+                </div>
+              </div>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
 };
 export default ActivitiesList;
