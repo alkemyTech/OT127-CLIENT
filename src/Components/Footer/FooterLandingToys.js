6305d6914ed826b5ba75ed9191bc29d2faedefb8
Nel Rodriguez <nel.rodriguez22@gmail.com>
2022-02-22 23:29:29 +0000

    Creacion de componente

---
+++ src/Components/Footer/FooterLandingToys.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../../sass/layout/_footer.scss'
+
+const FooterLandingToys = () => {
+  return (
+      <>
+        <h1>asd</h1>
+      </>
+  )
+}
+
+export default FooterLandingToys
