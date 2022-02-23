fac1370c8a0192a4f63e010b141818b35715b4b0
chriscaracach <christiancaracach@gmail.com>
2022-02-23 17:06:37 -0300

    Feat: searchform styles

--- src/Components/News/SearchForm.js
+++ src/Components/News/SearchForm.js
@@ -3,10 +3,15 @@ import SearchIcon from "@material-ui/icons/Search";
 
 const SearchForm = ({ searchNews }) => {
   return (
-    <>
+    <div className="searchform">
       <SearchIcon />
-      <input type="text" onChange={(e) => searchNews(e)} className="input" />
-    </>
+      <input
+        type="text"
+        onChange={(e) => searchNews(e)}
+        className="searchform__input"
+        placeholder="Buscar novedades"
+      />
+    </div>
   );
 };
 
