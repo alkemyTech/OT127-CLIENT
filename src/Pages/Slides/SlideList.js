2ab041e8c955538fcfaeebe6c2b1c7c63ff44008
davidz1995 <55600188+davidz1995@users.noreply.github.com>
2022-02-23 15:10:53 -0300

    Funcionando ellimnar y editar en slidesList

--- src/Pages/Slides/SlideList.js
+++ src/Pages/Slides/SlideList.js
@@ -1,103 +1,103 @@
-import {useEffect} from "react";
-import {getSlides, getSlidesSearch} from "../../Redux/reducers/slidesSlice";
-import {useSelector, useDispatch} from "react-redux";
-import {Link} from "react-router-dom";
+import { useEffect } from "react";
+import { getSlides, getSlidesSearch } from "../../Redux/reducers/slidesSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { Link } from "react-router-dom";
 import Spinner from "../../Components/Spinner/Spinner";
+import { sweetAlertConfirm } from "../../Services/sweetAlertServices";
+import { deleteSlide } from "../../Services/slidesApiService";
 
 const SlideList = () => {
-	const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-	useEffect(() => {
-		dispatch(getSlides());
-	}, []);
+  useEffect(() => {
+    dispatch(getSlides());
+  }, []); //eslint-disable-line
 
-	const slides = useSelector((state) => state.slidesReducer.slides.data);
+  const slides = useSelector((state) => state.slidesReducer.slides.data);
 
-	const rows = slides;
+  const rows = slides;
 
-  const handleClickEdit = () => {
-		//TODO, acciones editar novedades
-	};
+  const handleClickDelete = (id) => {
+    sweetAlertConfirm(
+		"Eliminar slide",
+		"Seguro quieres eliminar el slide?"
+	  ).then((res) => {
+		res && deleteSlide(id);
+		setTimeout(() => {
+		  dispatch(getSlides());
+		}, 2000);
+	  });
+  };
 
-	const handleClickDelete = () => {
-		//TODO, acciones para borrar novedades
-	};
-
-
-	const handleSearchChange = (e) => {
-		let {value} = e.target;
-		if (value.length > 2) {
-			dispatch(getSlidesSearch(value));
-		} else {
-			dispatch(getSlides());
-		}
-	};
-	return (
-		<div className="table">
-			<div className="table__container">
-				<div className="table__actions">
-					<input
-						type="search"
-						name="search"
-						onChange={(e) => {
-							handleSearchChange(e);
-						}}
-						placeholder="Buscar Slide"
-					/>
-					<Link className="table__link" to="/backoffice/slides/create">
-						Crear Slide
-					</Link>
-				</div>
-				{!rows.length ? (
-					<Spinner />
-				) : (
-					<table className="table__data">
-						<thead className="table__head">
-							<tr className="table__row">
-								<th className="table__title">Título</th>
-								<th className="table__title">Imagen</th>
-								<th className="table__title">Orden</th>
-								<th className="table__title-edit">Editar</th>
-								<th className="table__title-delete">Eliminar</th>
-							</tr>
-						</thead>
-						<tbody className="table__body">
-							{rows.map(({id, name, image, order}) => (
-								<tr key={id} className="table__row">
-									<td className="table__cell">{name}</td>
-									<td className="table__cell">
-										<img
-											src={image}
-											alt=""
-											className="imageTable"
-											width="100"
-										/>
-									</td>
+  const handleSearchChange = (e) => {
+    let { value } = e.target;
+    if (value.length > 2) {
+      dispatch(getSlidesSearch(value));
+    } else {
+      dispatch(getSlides());
+    }
+  };
+  return (
+    <div className="table">
+      <div className="table__container">
+        <div className="table__actions">
+          <input
+            type="search"
+            name="search"
+            onChange={(e) => {
+              handleSearchChange(e);
+            }}
+            placeholder="Buscar Slide"
+          />
+          <Link className="table__link" to="/backoffice/slides/create">
+            Crear Slide
+          </Link>
+        </div>
+        {!rows.length ? (
+          <Spinner />
+        ) : (
+          <table className="table__data">
+            <thead className="table__head">
+              <tr className="table__row">
+                <th className="table__title">Título</th>
+                <th className="table__title">Imagen</th>
+                <th className="table__title">Orden</th>
+                <th className="table__title-edit">Editar</th>
+                <th className="table__title-delete">Eliminar</th>
+              </tr>
+            </thead>
+            <tbody className="table__body">
+              {rows.map(({ id, name, image, order }) => (
+                <tr key={id} className="table__row">
+                  <td className="table__cell">{name}</td>
+                  <td className="table__cell">
+                    <img
+                      src={image}
+                      alt=""
+                      className="imageTable"
+                      width="100"
+                    />
+                  </td>
                   <td className="table__cell">{order}</td>
-									<td className="table__cell-edit">
-										<button
-											className="table__edit"
-											onClick={handleClickEdit}
-										>
-											Editar
-										</button>
-									</td>
-									<td className="table__cell-delete">
-										<button
-											className="table__delete"
-											onClick={handleClickDelete}
-										>
-										Eliminar
-										</button>
-									</td>
-								</tr>
-							))}
-						</tbody>
-					</table>
-				)}
-			</div>
-		</div>
-	);
+                  <td className="table__cell-edit">
+                    <Link to={`/backoffice/slides/edit/${id}`}>Editar</Link>
+                  </td>
+                  <td className="table__cell-delete">
+                    <button
+                      className="table__delete"
+                      onClick={() => handleClickDelete(id)}
+                    >
+                      Eliminar
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default SlideList;
