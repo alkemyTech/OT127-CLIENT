6d66a54e874d83867d6102889d9041f0d97ca2eb
Chris Caracach <69370931+Chriscaracach@users.noreply.github.com>
2022-02-24 11:50:29 -0300

    Ot127 43/fix/styles (#145)
    * Fix: Main structure of header styles
    
    * fixed logout function
    
    * fix: modified header
    
    * Fix: comments and position fixed in navbar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,95 +1,90 @@
-import {NavLink} from "react-router-dom";
-import {useDispatch} from "react-redux";
-import {logoutUser} from "../../Redux/actions/authActions";
-import {Link} from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import logo from "../../images/LOGO-SOMOS_MAS.png";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../Redux/actions/authActions";
 
 const Header = () => {
-	const dispatch = useDispatch();
-	const isAuthenticated = JSON.parse(localStorage.getItem("authenticatedUser"));
-	const token = localStorage.getItem("TOKEN");
+  const dispatch = useDispatch();
+  const isAuthenticated = JSON.parse(localStorage.getItem("authenticatedUser"));
+  const token = localStorage.getItem("TOKEN");
 
-	const menuItems = [
-		{link: "/school-campaign", name: "Campaña escolar"},
-		{link: "/toys-campaign", name: "Campaña de juguetes"},
-		{link: "/nosotros", name: "Nosotros"},
-	];
+  const menuItems = [
+    { link: "/school-campaign", name: "Campaña escolar" },
+    { link: "/toys-campaign", name: "Campaña de juguetes" },
+    { link: "/nosotros", name: "Nosotros" },
+  ];
 
-	const logout = () => {
-		dispatch(logoutUser());
-		localStorage.removeItem("TOKEN");
-		localStorage.removeItem("authenticatedUser");
-		window.location.href = "/";
-	};
+  const logout = () => {
+    dispatch(logoutUser());
+    localStorage.removeItem("TOKEN");
+    localStorage.removeItem("authenticatedUser");
+    window.location.href = "/";
+  };
 
-	return (
-		<>
-			<header className="header">
-				<nav className="header__nav">
-					<div className="header__nav-left">
-						<NavLink
-							className="header__nav-links"
-							activeClassName="header__nav-links-active"
-							to="/"
-						>
-							Inicio
-						</NavLink>
-					</div>
-					<div className="header__nav-right">
-						{token ? (
-							<ul className="header__nav-list">
-								{menuItems.map((item) => (
-									<li key={item.name} className="header__nav-item">
-										<NavLink
-											className="header__nav-links"
-											activeClassName="header__nav-links-active"
-											to={item.link}
-										>
-											{item.name}
-										</NavLink>
-									</li>
-								))}
-								{isAuthenticated && isAuthenticated.role_id !== 1 && (
-									<li className="header__nav-item">
-										<NavLink
-											className="header__nav-links"
-											activeClassName="header__nav-links-active"
-											to="/contacto"
-										>
-											Contacto
-										</NavLink>
-									</li>
-								)}
-								<button onClick={logout}>Cerrar sesión</button>
-							</ul>
-						) : (
-							<div>
-								<NavLink
-									className="header__nav-links"
-									activeClassName="header__nav-links-active"
-									to="/login"
-								>
-									Iniciar sesión
-								</NavLink>
-								<NavLink
-									className="header__nav-links"
-									activeClassName="header__nav-links-active"
-									to="/register"
-								>
-									Registrarse
-								</NavLink>
-							</div>
-						)}
-					</div>
+  return (
+    <>
+      <header className="header">
+        <NavLink
+          activeClassName="header__nav-links-active"
+          to="/"
+          className="header__link"
+        >
+          <img src={logo} alt="" className="header__logo" />
+        </NavLink>
+        <nav className="header__nav">
+          {token ? (
+            menuItems.map((item) => (
+              <NavLink
+                className="header__link"
+                activeClassName="header__nav-links-active"
+                to={item.link}
+              >
+                {item.name}
+              </NavLink>
+            ))
+          ) : (
+            <>
+              <NavLink
+                className="header__link"
+                activeClassName="header__nav-links-active"
+                to="/login"
+              >
+                Iniciar sesión
+              </NavLink>
+              <NavLink
+                className="header__link"
+                activeClassName="header__nav-links-active"
+                to="/register"
+              >
+                Registrarse
+              </NavLink>
+            </>
+          )}
+          {isAuthenticated && isAuthenticated.role_id !== 1 && (
+            <>
+              <NavLink
+                className="header__link"
+                activeClassName="header__nav-links-active"
+                to="/contacto"
+              >
+                Contacto
+              </NavLink>
+            </>
+          )}
+          {isAuthenticated && isAuthenticated.role_id === 2 && (
+            <NavLink className="header__link" to="/donate">
+              Donar
+            </NavLink>
+          )}
 
-					{isAuthenticated && !isAuthenticated.role_id === 1 && (
-						<Link to="/donate">Donar</Link>
-					)}
-
-					{/* Atento cuando venga el pull de los estilos que hice, hay que borrar todo lo local, pero pasar el 
-          metodo logout como prop del botton que viene */}
-				</nav>
-			</header>
-		</>
-	);
+          {isAuthenticated && (
+            <button onClick={logout} className="header__logbutton">
+              Cerrar sesión
+            </button>
+          )}
+        </nav>
+      </header>
+    </>
+  );
 };
 export default Header;
