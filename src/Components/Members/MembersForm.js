7c532ad6f523eb979721e49a5b13049afa747301
Mauricio Ojeda <ojeda.mauricio@hotmail.com.ar>
2022-02-25 18:43:29 -0300

    spinner removido

--- src/Components/Members/MembersForm.js
+++ src/Components/Members/MembersForm.js
@@ -14,7 +14,7 @@ import {
 
 const MemberForm = () => {
   const { id } = useParams();
-  const [loading, setLoading] = useState(false);
+
   const [formValues, setFormValues] = useState({
     name: "",
     description: "",
@@ -36,7 +36,6 @@ const MemberForm = () => {
   };
 
   const getDataById = async () => {
-    setLoading(true);
     const response = await getMemberById(id);
     const data = response.data.data;
     setFormValues({
@@ -46,7 +45,6 @@ const MemberForm = () => {
       linkedinUrl: data.linkedinUrl,
       image: data.image,
     });
-    setLoading(false);
   };
 
   useEffect(() => {
@@ -78,109 +76,105 @@ const MemberForm = () => {
   };
   return (
     <div>
-      {loading ? (
-        <Spinner />
-      ) : (
-        <Formik
-          enableReinitialize={true}
-          initialValues={formValues}
-          validationSchema={Yup.object({
-            name: Yup.string()
-              .min(4, "Debe tener por lo menos 4 caracteres.")
-              .required("Campo obligatorio"),
-            description: Yup.string().required("Campo obligatorio"),
-            image: Yup.string().required("Este campo es obligatorio"),
-            facebookUrl: Yup.string()
-              .required("Campo obligatorio")
-              .url("Formato invalido"),
-            linkedinUrl: Yup.string()
-              .required("Campo obligatorio")
-              .url("Formato invalido"),
-          })}
-          onSubmit={(values, { resetForm }) => {
-            handleSubmit(values);
-            resetForm();
-          }}
-        >
-          {({ setFieldValue }) => (
-            <Form className="form__container">
-              <div className="form">
-                <label className="form__label" htmlFor="name">
-                  Nombre
-                </label>
-                <Field name="name" type="text" className="form__input" />
-                <ErrorMessage
-                  name="name"
-                  render={(msg) => <div className="form__error">{msg}</div>}
-                />
+      <Formik
+        enableReinitialize={true}
+        initialValues={formValues}
+        validationSchema={Yup.object({
+          name: Yup.string()
+            .min(4, "Debe tener por lo menos 4 caracteres.")
+            .required("Campo obligatorio"),
+          description: Yup.string().required("Campo obligatorio"),
+          image: Yup.string().required("Este campo es obligatorio"),
+          facebookUrl: Yup.string()
+            .required("Campo obligatorio")
+            .url("Formato invalido"),
+          linkedinUrl: Yup.string()
+            .required("Campo obligatorio")
+            .url("Formato invalido"),
+        })}
+        onSubmit={(values, { resetForm }) => {
+          handleSubmit(values);
+          resetForm();
+        }}
+      >
+        {({ setFieldValue }) => (
+          <Form className="form__container">
+            <div className="form">
+              <label className="form__label" htmlFor="name">
+                Nombre
+              </label>
+              <Field name="name" type="text" className="form__input" />
+              <ErrorMessage
+                name="name"
+                render={(msg) => <div className="form__error">{msg}</div>}
+              />
 
-                <label className="form__label" htmlFor="description">
-                  Descripcion
-                </label>
-                <Field name="description">
-                  {({ field }) => (
-                    <>
-                      <CKEditor
-                        config={{
-                          language: "es",
-                        }}
-                        editor={ClassicEditor}
-                        data={field.value}
-                        onChange={(event, editor) => {
-                          setFieldValue(field.name, editor.getData());
-                        }}
-                      />
-                    </>
-                  )}
-                </Field>
-                <ErrorMessage
-                  name="description"
-                  render={(msg) => <div className="form__error">{msg}</div>}
-                />
+              <label className="form__label" htmlFor="description">
+                Descripcion
+              </label>
+              <Field name="description">
+                {({ field }) => (
+                  <>
+                    <CKEditor
+                      config={{
+                        language: "es",
+                      }}
+                      editor={ClassicEditor}
+                      data={field.value}
+                      onChange={(event, editor) => {
+                        setFieldValue(field.name, editor.getData());
+                      }}
+                    />
+                  </>
+                )}
+              </Field>
+              <ErrorMessage
+                name="description"
+                render={(msg) => <div className="form__error">{msg}</div>}
+              />
 
-                <label className="form__label" htmlFor="image">
-                  {id ? "Cambiar Imagen" : "Cargar Imagen"}
-                </label>
-                <input
-                  name="image"
-                  ref={inputFileRef}
-                  type="file"
-                  accept=".jpg, .png"
-                  onChange={(e) => {
-                    setFieldValue("image", e.currentTarget.files[0]);
-                  }}
-                />
-                <ErrorMessage
-                  name="image"
-                  render={(msg) => <div className="form__error">{msg}</div>}
-                />
+              <label className="form__label" htmlFor="image">
+                {id ? "Cambiar Imagen" : "Cargar Imagen"}
+              </label>
+              <input
+                name="image"
+                ref={inputFileRef}
+                type="file"
+                accept=".jpg, .png"
+                onChange={(e) => {
+                  setFieldValue("image", e.currentTarget.files[0]);
+                }}
+              />
+              <ErrorMessage
+                name="image"
+                render={(msg) => <div className="form__error">{msg}</div>}
+              />
 
-                <label className="form__label" htmlFor="facebookUrl">
-                  Facebook
-                </label>
-                <Field name="facebookUrl" type="text" className="form__input" />
-                <ErrorMessage
-                  name="facebookUrl"
-                  render={(msg) => <div className="form__error">{msg}</div>}
-                />
+              <label className="form__label" htmlFor="facebookUrl">
+                Facebook
+              </label>
+              <Field name="facebookUrl" type="text" className="form__input" />
+              <ErrorMessage
+                name="facebookUrl"
+                render={(msg) => <div className="form__error">{msg}</div>}
+              />
 
-                <label className="form__label" htmlFor="linkedinUrl">
-                  LinkedIn
-                </label>
-                <Field name="linkedinUrl" type="text" className="form__input" />
-                <ErrorMessage
-                  name="linkedinUrl"
-                  render={(msg) => <div className="form__error">{msg}</div>}
-                />
+              <label className="form__label" htmlFor="linkedinUrl">
+                LinkedIn
+              </label>
+              <Field name="linkedinUrl" type="text" className="form__input" />
+              <ErrorMessage
+                name="linkedinUrl"
+                render={(msg) => <div className="form__error">{msg}</div>}
+              />
 
-                <button className="form__button" type="submit">
-                  {id ? "Editar" : "Crear"}
-                </button>
-              </div>
-            </Form>
-          )}
-        </Formik>
-      )}
+              <button className="form__button" type="submit">
+                {id ? "Editar" : "Crear"}
+              </button>
+            </div>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
