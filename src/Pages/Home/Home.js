7a2abdaec7ceba413a584900f5cf8ccb43b3fc4c
matias <solisma42@gmail.com>
2022-02-27 17:29:45 -0300

    agregado hero en home

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,13 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SliderHome from "../../Components/Slides/SliderHome";
+import { useDispatch, useSelector } from "react-redux";
+import { getUs } from "../../Redux/reducers/usSlice";
 import News from "../../Components/News/News";
 
 const Home = () => {
   const [welcomeText, setWelcomeText] = useState("Bienvenido"); //eslint-disable-line
+  const dispatch = useDispatch()
+  const info = useSelector(state => state.usReducer.info)
+  const isAuthenticated = JSON.parse(localStorage.getItem("authenticatedUser"));
+
+  useEffect(() => {
+    dispatch(getUs());
+  }, []);//eslint-disable-line
 
   return (
     <div className="home">
-      <h2 className="home__welcometext">{welcomeText}</h2>
+      <div className="home__hero">
+        <div className="home__hero-container">
+          <p className="home__welcometext">{welcomeText}</p>
+          <p className="home__info">{info.short_description}</p>
+          {isAuthenticated && isAuthenticated.name !== "Admin" ?
+            <div className="home__button-container">
+              <button className="home__button home__button--about">Nosotros</button>
+              <button className="home__button home__button--donate">Donar</button>
+            </div>
+            :
+            null
+          }
+        </div>
+      </div>
       <SliderHome />
       <News />
     </div>
